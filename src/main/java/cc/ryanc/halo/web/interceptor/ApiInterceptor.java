e17e9f15c4c568a2122a7b9fb3f221afbdd93e71
ruibaby <i@ryanc.cc>
2018-10-21 19:54:27 +0800

    :art: APi新增Token验证

--- src/main/java/cc/ryanc/halo/web/interceptor/ApiInterceptor.java
+++ src/main/java/cc/ryanc/halo/web/interceptor/ApiInterceptor.java
@@ -4,12 +4,15 @@ import cc.ryanc.halo.model.dto.HaloConst;
 import cc.ryanc.halo.model.enums.BlogPropertiesEnum;
 import cc.ryanc.halo.model.enums.TrueFalseEnum;
 import cn.hutool.core.util.StrUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <pre>
@@ -25,7 +28,18 @@ public class ApiInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         if (StrUtil.equals(TrueFalseEnum.TRUE.getDesc(), HaloConst.OPTIONS.get(BlogPropertiesEnum.API_STATUS.getProp()))) {
-            return true;
+            if (StrUtil.equals(request.getHeader("token"), HaloConst.OPTIONS.get(BlogPropertiesEnum.API_TOKEN.getProp()))) {
+                return true;
+            } else {
+                response.setCharacterEncoding("UTF-8");
+                response.setContentType("application/json;charset=utf-8");
+                Map<String, Object> map = new HashMap<>(2);
+                ObjectMapper mapper = new ObjectMapper();
+                map.put("code", 400);
+                map.put("msg", "Invalid Token");
+                response.getWriter().write(mapper.writeValueAsString(map));
+                return false;
+            }
         }
         response.sendRedirect("/404");
         return false;
