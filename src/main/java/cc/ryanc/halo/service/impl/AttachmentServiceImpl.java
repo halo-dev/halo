5ba386e37f1fbc1db2575fd7c5526a1e9a4e22ca
Yawn <ywms9521@163.com>
2018-12-04 21:25:50 +0800

    新增七牛和又拍的云存储支持

--- src/main/java/cc/ryanc/halo/service/impl/AttachmentServiceImpl.java
+++ src/main/java/cc/ryanc/halo/service/impl/AttachmentServiceImpl.java
@@ -1,17 +1,45 @@
 package cc.ryanc.halo.service.impl;
 
 import cc.ryanc.halo.model.domain.Attachment;
+import cc.ryanc.halo.model.domain.Options;
+import cc.ryanc.halo.model.dto.QiNiuPutSet;
+import cc.ryanc.halo.model.enums.AttachLocationEnum;
 import cc.ryanc.halo.repository.AttachmentRepository;
+import cc.ryanc.halo.repository.OptionsRepository;
 import cc.ryanc.halo.service.AttachmentService;
+import cc.ryanc.halo.utils.HaloUtils;
+import cc.ryanc.halo.utils.Md5Util;
+import cn.hutool.core.date.DateUtil;
+import com.UpYun;
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import com.qiniu.common.QiniuException;
+import com.qiniu.common.Zone;
+import com.qiniu.http.Response;
+import com.qiniu.storage.BucketManager;
+import com.qiniu.storage.Configuration;
+import com.qiniu.storage.UploadManager;
+import com.qiniu.storage.persistent.FileRecorder;
+import com.qiniu.util.Auth;
+import com.qiniu.util.StringMap;
+import com.upyun.UpException;
+import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-import java.util.Optional;
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * <pre>
@@ -28,6 +56,9 @@ public class AttachmentServiceImpl implements AttachmentService {
 
     @Autowired
     private AttachmentRepository attachmentRepository;
+
+    @Autowired
+    private OptionsRepository optionsRepository;
 
     /**
      * 新增附件信息
@@ -86,5 +117,242 @@ public class AttachmentServiceImpl implements AttachmentService {
         Optional<Attachment> attachment = this.findByAttachId(attachId);
         attachmentRepository.delete(attachment.get());
         return attachment.get();
+    }
+
+    /**
+     * 上传转发
+     * @param file
+     * @param request
+     * @return
+     */
+    @Override
+    public Map<String, String> upload(MultipartFile file, HttpServletRequest request) {
+        Map<String,String> resultMap;
+        Options options = optionsRepository.findOptionsByOptionName("attach_loc");
+        if(options == null){
+            return null;
+        }
+        switch (options.getOptionValue()){
+            case "server":
+                resultMap = this.attachUpload(file,request);
+                break;
+            case "qiniu":
+                resultMap = this.attachQiNiuUpload(file,request);
+                break;
+            case "upyun":
+                resultMap = this.attachUpYunUpload(file,request);
+                break;
+            default:
+                resultMap = this.attachUpload(file,request);
+                break;
+        }
+        return resultMap;
+    }
+
+    /**
+     * 原生服务器上传
+     * @param file
+     * @param request
+     * @return
+     */
+    @Override
+    public Map<String, String> attachUpload(MultipartFile file, HttpServletRequest request) {
+        Map<String,String> resultMap = new HashMap<>(6);
+        try {
+            //用户目录
+            String userPath = System.getProperties().getProperty("user.home") + "/halo";
+            //upload的路径
+            StringBuffer sbMedia = new StringBuffer("upload/");
+            //获取当前年月以创建目录，如果没有该目录则创建
+            sbMedia.append(DateUtil.thisYear()).append("/").append(DateUtil.thisMonth()).append("/");
+            File mediaPath = new File(userPath, sbMedia.toString());
+            if (!mediaPath.exists()) {
+                mediaPath.mkdirs();
+            }
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+            String nameWithOutSuffix = file.getOriginalFilename().substring(0, file.getOriginalFilename().lastIndexOf('.')).replaceAll(" ", "_").replaceAll(",", "") + dateFormat.format(DateUtil.date()) + new Random().nextInt(1000);
+            String fileSuffix = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf('.') + 1);
+            String fileName = nameWithOutSuffix + "." + fileSuffix;
+            file.transferTo(new File(mediaPath.getAbsoluteFile(), fileName));
+            //压缩图片
+            Thumbnails.of(new StringBuffer(mediaPath.getAbsolutePath()).append("/").append(fileName).toString()).size(256, 256).keepAspectRatio(false).toFile(new StringBuffer(mediaPath.getAbsolutePath()).append("/").append(nameWithOutSuffix).append("_small.").append(fileSuffix).toString());
+            String filePath = new StringBuffer("/upload/").append(DateUtil.thisYear()).append("/").append(DateUtil.thisMonth()).append("/").append(fileName).toString();
+            String smallPath = new StringBuffer("/upload/").append(DateUtil.thisYear()).append("/").append(DateUtil.thisMonth()).append("/").append(nameWithOutSuffix).append("_small.").append(fileSuffix).toString();
+            String suffix = new StringBuffer(".").append(fileSuffix).toString();
+            String size = HaloUtils.parseSize(new File(mediaPath, fileName).length());
+            String wh = HaloUtils.getImageWh(new File(mediaPath, fileName));
+            resultMap.put("fileName",fileName);
+            resultMap.put("filePath",filePath);
+            resultMap.put("smallPath",smallPath);
+            resultMap.put("suffix",suffix);
+            resultMap.put("size",size);
+            resultMap.put("wh",wh);
+            resultMap.put("location", AttachLocationEnum.SERVER.getDesc());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return resultMap;
+    }
+
+    /**
+     * 七牛云上传
+     * @param file
+     * @param request
+     * @return
+     */
+    @Override
+    public Map<String, String> attachQiNiuUpload(MultipartFile file, HttpServletRequest request) {
+        Map<String, String> resultMap = new HashMap<>(6);
+        try {
+            Configuration cfg = new Configuration(Zone.zone0());
+            String key = Md5Util.getMD5Checksum(file);
+            Options accessKey = optionsRepository.findOptionsByOptionName("qiniu_access_key");
+            Options secretKey = optionsRepository.findOptionsByOptionName("qiniu_secret_key");
+            Options domain = optionsRepository.findOptionsByOptionName("qiniu_domain");
+            Options bucket = optionsRepository.findOptionsByOptionName("qiniu_bucket");
+            Options smallUrl = optionsRepository.findOptionsByOptionName("qiniu_small_url");
+            if(accessKey == null || secretKey == null || domain == null || bucket == null){
+                return resultMap;
+            }
+            Auth auth = Auth.create(accessKey.getOptionValue(),secretKey.getOptionValue());
+            StringMap putPolicy = new StringMap();
+            putPolicy.put("returnBody", "{\"size\":$(fsize),\"w\":$(imageInfo.width),\"h\":$(imageInfo.height)}");
+            String upToken = auth.uploadToken(bucket.getOptionValue(),null,3600,putPolicy);
+            String localTempDir = Paths.get(System.getenv("java.io.tmpdir"),bucket.getOptionValue()).toString();
+            QiNiuPutSet putSet = new QiNiuPutSet();
+            try {
+                FileRecorder fileRecorder = new FileRecorder(localTempDir);
+                UploadManager uploadManager = new UploadManager(cfg,fileRecorder);
+                Response response = uploadManager.put(file.getInputStream(),key,upToken,null,null);
+                //解析上传成功的结果
+                putSet = new Gson().fromJson(response.bodyString(), QiNiuPutSet.class);
+            } catch (QiniuException e) {
+                Response r = e.response;
+                System.err.println(r.toString());
+                try {
+                    System.err.println(r.bodyString());
+                } catch (QiniuException ex2) {
+                    //ignore
+                }
+            } catch (JsonSyntaxException e) {
+                e.printStackTrace();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            String filePath = domain.getOptionValue().contains("http://")?domain.getOptionValue().trim():("http://"+domain.getOptionValue().trim()) + "/" + key;
+            resultMap.put("fileName",file.getOriginalFilename());
+            resultMap.put("filePath",filePath.trim());
+            resultMap.put("smallPath",smallUrl == null ? filePath.trim():(filePath+smallUrl.getOptionValue()).trim());
+            resultMap.put("suffix",file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf('.')));
+            resultMap.put("size",HaloUtils.parseSize(file.getSize()));
+            resultMap.put("wh",putSet.getW() + "x" + putSet.getH());
+            resultMap.put("location", AttachLocationEnum.QINIU.getDesc());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return resultMap;
+    }
+
+    /**
+     * 又拍云上传
+     * @param file
+     * @param request
+     * @return
+     */
+    @Override
+    public Map<String, String> attachUpYunUpload(MultipartFile file, HttpServletRequest request) {
+        Map<String, String> resultMap = new HashMap<>(6);
+        try {
+            String key = Md5Util.getMD5Checksum(file);
+            Options ossSrc = optionsRepository.findOptionsByOptionName("upyun_oss_src");
+            Options ossPwd = optionsRepository.findOptionsByOptionName("upyun_oss_pwd");
+            Options bucket = optionsRepository.findOptionsByOptionName("upyun_oss_bucket");
+            Options domain = optionsRepository.findOptionsByOptionName("upyun_oss_domain");
+            Options operator = optionsRepository.findOptionsByOptionName("upyun_oss_operator");
+            Options smallUrl = optionsRepository.findOptionsByOptionName("upyun_oss_small");
+            if(ossSrc == null || ossPwd == null || domain == null || bucket == null || operator == null){
+                return resultMap;
+            }
+            String fileName = file.getOriginalFilename();
+            String fileSuffix = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf('.'));
+            UpYun upYun = new UpYun(bucket.getOptionValue(),operator.getOptionValue(),ossPwd.getOptionValue());
+            upYun.setTimeout(60);
+            upYun.setApiDomain(UpYun.ED_AUTO);
+            upYun.setDebug(true);
+            upYun.writeFile(ossSrc.getOptionValue()+key+"."+fileSuffix,file.getBytes(),true,null);
+            String filePath = domain.getOptionValue().contains("http://")?domain.getOptionValue().trim():("http://"+domain.getOptionValue().trim() +ossSrc.getOptionValue() + key + "." + fileSuffix);
+            String smallPath = filePath;
+            if(smallUrl != null){
+                smallPath += smallUrl.getOptionValue();
+            }
+            BufferedImage image = ImageIO.read(file.getInputStream());
+            if (image != null) {
+                resultMap.put("wh",image.getWidth()+"x"+image.getHeight());
+            }
+            resultMap.put("fileName",fileName);
+            resultMap.put("filePath",filePath.trim());
+            resultMap.put("smallPath",smallPath.trim());
+            resultMap.put("suffix",fileSuffix);
+            resultMap.put("size",HaloUtils.parseSize(file.getSize()));
+            resultMap.put("location", AttachLocationEnum.UPYUN.getDesc());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return resultMap;
+    }
+
+    /**
+     * 七牛云删除附件
+     * @param key
+     * @return
+     */
+    @Override
+    public boolean deleteQiNiuAttachment(String key) {
+        boolean flag = true;
+        Configuration cfg = new Configuration(Zone.zone0());
+        Options accessKey = optionsRepository.findOptionsByOptionName("qiniu_access_key");
+        Options secretKey = optionsRepository.findOptionsByOptionName("qiniu_secret_key");
+        Options bucket = optionsRepository.findOptionsByOptionName("qiniu_bucket");
+        if(accessKey == null || secretKey == null || bucket == null){
+            return false;
+        }
+        Auth auth = Auth.create(accessKey.getOptionValue(), secretKey.getOptionValue());
+        BucketManager bucketManager = new BucketManager(auth, cfg);
+        try {
+            bucketManager.delete(bucket.getOptionValue(), key);
+        } catch (QiniuException ex) {
+            System.err.println(ex.code());
+            System.err.println(ex.response.toString());
+            flag = false;
+        }
+        return flag;
+    }
+
+    /**
+     * 又拍云删除附件
+     * @param fileName
+     * @return
+     */
+    @Override
+    public boolean deleteUpYunAttachment(String fileName) {
+        boolean flag = true;
+        Options ossSrc = optionsRepository.findOptionsByOptionName("upyun_oss_src");
+        Options ossPwd = optionsRepository.findOptionsByOptionName("upyun_oss_pwd");
+        Options bucket = optionsRepository.findOptionsByOptionName("upyun_oss_bucket");
+        Options operator = optionsRepository.findOptionsByOptionName("upyun_oss_operator");
+        if(ossSrc == null || ossPwd == null || bucket == null || operator == null){
+            return false;
+        }
+        UpYun upYun = new UpYun(bucket.getOptionValue(),operator.getOptionValue(),ossPwd.getOptionValue());
+        upYun.setApiDomain(UpYun.ED_AUTO);
+        try {
+            flag = upYun.deleteFile(ossSrc.getOptionValue()+fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (UpException e) {
+            e.printStackTrace();
+        }
+        return flag;
     }
 }
