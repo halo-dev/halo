d0167dcc641a7597c11e18d5652928c472bed45c
johnniang <1340692778@qq.com>
2019-02-19 20:50:13 +0800

    Add CrudService interface

---
+++ src/main/java/cc/ryanc/halo/service/base/AbstractService.java
@@ -0,0 +1,11 @@
+package cc.ryanc.halo.service.base;
+
+/**
+ * Abstract service implementation.
+ *
+ * @param <ID>     id type
+ * @param <DOMAIN> domain type
+ */
+public abstract class AbstractService<ID, DOMAIN> implements CrudService<ID, DOMAIN> {
+
+}
