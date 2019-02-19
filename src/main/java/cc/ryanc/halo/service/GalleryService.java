007ba6a8227a761cdf8592b6882daa8863f00f9c
johnniang <1340692778@qq.com>
2019-02-19 22:43:36 +0800

    Refactor GalleryService

--- src/main/java/cc/ryanc/halo/service/GalleryService.java
+++ src/main/java/cc/ryanc/halo/service/GalleryService.java
@@ -1,11 +1,7 @@
 package cc.ryanc.halo.service;
 
 import cc.ryanc.halo.model.domain.Gallery;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-import java.util.List;
-import java.util.Optional;
+import cc.ryanc.halo.service.base.CrudService;
 
 /**
  * <pre>
@@ -15,44 +11,6 @@ import java.util.Optional;
  * @author : RYAN0UP
  * @date : 2018/2/26
  */
-public interface GalleryService {
+public interface GalleryService extends CrudService<Gallery, Long> {
 
-    /**
-     * 保存图片
-     *
-     * @param gallery gallery
-     * @return Gallery
-     */
-    Gallery save(Gallery gallery);
-
-    /**
-     * 根据编号删除图片
-     *
-     * @param galleryId galleryId
-     * @return Gallery
-     */
-    Gallery remove(Long galleryId);
-
-    /**
-     * 查询所有图片 分页
-     *
-     * @param pageable pageable
-     * @return Page
-     */
-    Page<Gallery> findAll(Pageable pageable);
-
-    /**
-     * 查询所有图片 不分页
-     *
-     * @return List
-     */
-    List<Gallery> findAll();
-
-    /**
-     * 根据编号查询图片信息
-     *
-     * @param galleryId galleryId
-     * @return Optional
-     */
-    Optional<Gallery> findByGalleryId(Long galleryId);
 }
