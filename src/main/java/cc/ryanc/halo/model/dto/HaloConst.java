a84f7471112c0c50f0e71039c73313ceb7be579f
ruibaby <i@ryanc.cc>
2019-02-24 16:08:00 +0800

    :sparkles: 编辑器支持网易云音乐短代码[music:id]

--- src/main/java/cc/ryanc/halo/model/dto/HaloConst.java
+++ src/main/java/cc/ryanc/halo/model/dto/HaloConst.java
@@ -39,4 +39,19 @@ public class HaloConst {
      * 文章阅读数缓存
      */
     public static Map<Long, Long> POSTS_VIEWS = new HashMap<>();
+
+    /**
+     * 网易云音乐短代码前缀
+     */
+    public static final String NETEASE_MUSIC_PREFIX = "[music:";
+
+    /**
+     * 网易云音乐 iframe 代码
+     */
+    public static final String NETEASE_MUSIC_IFRAME = "<iframe frameborder=\"no\" border=\"0\" marginwidth=\"0\" marginheight=\"0\" width=330 height=86 src=\"//music.163.com/outchain/player?type=2&id=$1&auto=1&height=66\"></iframe>";
+
+    /**
+     * 网易云音乐短代码正则表达式
+     */
+    public static final String NETEASE_MUSIC_REG_PATTERN = "\\[music:(\\d+)\\]";
 }
