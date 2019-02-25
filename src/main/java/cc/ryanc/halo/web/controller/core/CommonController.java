3812b39a468ca8ad000bc95069c1a5ce45f12265
johnniang <1340692778@qq.com>
2019-02-25 19:37:58 +0800

    Redirect 404 if view render is not found

--- src/main/java/cc/ryanc/halo/web/controller/core/CommonController.java
+++ src/main/java/cc/ryanc/halo/web/controller/core/CommonController.java
@@ -1,9 +1,10 @@
 package cc.ryanc.halo.web.controller.core;
 
+import cc.ryanc.halo.logging.Logger;
 import cc.ryanc.halo.model.enums.CommonParamsEnum;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,9 +17,10 @@ import javax.servlet.http.HttpServletRequest;
  * @author : RYAN0UP
  * @date : 2017/12/26
  */
-@Slf4j
 @Controller
 public class CommonController implements ErrorController {
+
+    private final Logger log = Logger.getLogger(getClass());
 
     private static final String ERROR_PATH = "/error";
 
@@ -31,6 +33,18 @@ public class CommonController implements ErrorController {
     @GetMapping(value = ERROR_PATH)
     public String handleError(HttpServletRequest request) {
         final Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+
+        log.error("Error path: [{}], status: [{}]", request.getRequestURI(), statusCode);
+
+        // Get the exception
+        Throwable throwable = (Throwable) request.getAttribute("javax.servlet.error.exception");
+
+        if (StringUtils.startsWithIgnoreCase(throwable.getMessage(), "Could not resolve view with name '")) {
+            // TODO May cause unreasoned problem
+            // if Ftl was not found then redirect to /404
+            return "redirect:/404";
+        }
+
         if (statusCode.equals(CommonParamsEnum.NOT_FOUND.getValue())) {
             return "redirect:/404";
         } else {
