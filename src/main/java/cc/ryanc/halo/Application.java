4ec6604dcb9b392761e7b58740289f2e864ff0ba
johnniang <johnniang@riseup.net>
2019-03-08 19:31:58 +0800

    Update start information

--- src/main/java/cc/ryanc/halo/Application.java
+++ src/main/java/cc/ryanc/halo/Application.java
@@ -1,14 +1,10 @@
 package cc.ryanc.halo;
 
-import cc.ryanc.halo.config.properties.HaloProperties;
 import cc.ryanc.halo.logging.Logger;
 import cc.ryanc.halo.repository.base.BaseRepositoryImpl;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -33,19 +29,5 @@ public class Application {
     public static void main(String[] args) {
         // Run application
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    CommandLineRunner runner(ApplicationContext context, HaloProperties haloProperties) {
-        return args -> {
-            // Get server port
-            String serverPort = context.getEnvironment().getProperty("server.port");
-
-            LOG.debug("Halo started at    {}:{}", "http://localhost", serverPort);
-
-            if (!haloProperties.getDocDisabled()) {
-                LOG.debug("Halo doc enable at {}:{}/{}", "http://localhost", serverPort, "swagger-ui.html");
-            }
-        };
     }
 }
