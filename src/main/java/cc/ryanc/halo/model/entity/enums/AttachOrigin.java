e81b0b20881984e14b7dcc4996d63de5c8fc77b6
ruibaby <i@ryanc.cc>
2019-03-13 09:11:25 +0800

    modify AttachOrigin enum

--- src/main/java/cc/ryanc/halo/model/entity/enums/AttachOrigin.java
+++ src/main/java/cc/ryanc/halo/model/entity/enums/AttachOrigin.java
@@ -7,9 +7,27 @@ package cc.ryanc.halo.model.entity.enums;
  * @date : 2019-03-12
  */
 public enum AttachOrigin implements ValueEnum<Integer> {
-    ;
+
+    /**
+     * 服务器
+     */
+    SERVER(0),
+
+    /**
+     * 又拍云
+     */
+    UPYUN(1),
+
+    /**
+     * 七牛云
+     */
+    QINIUYUN(2);
 
     private Integer value;
+
+    AttachOrigin(Integer value) {
+        this.value = value;
+    }
 
     /**
      * Get enum value.
@@ -18,6 +36,6 @@ public enum AttachOrigin implements ValueEnum<Integer> {
      */
     @Override
     public Integer getValue() {
-        return null;
+        return value;
     }
 }
