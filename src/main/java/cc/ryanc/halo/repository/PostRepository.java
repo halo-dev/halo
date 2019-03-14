afae5fc38ebd8fd0588f26c14c3548e3ccf15774
ruibaby <i@ryanc.cc>
2019-03-14 16:59:09 +0800

    posts manage

--- src/main/java/cc/ryanc/halo/repository/PostRepository.java
+++ src/main/java/cc/ryanc/halo/repository/PostRepository.java
@@ -2,6 +2,8 @@ package cc.ryanc.halo.repository;
 
 import cc.ryanc.halo.model.entity.Post;
 import cc.ryanc.halo.repository.base.BaseRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 
@@ -9,7 +11,28 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
  * Post repository.
  *
  * @author johnniang
+ * @author RYAN0UP
  */
 public interface PostRepository extends BaseRepository<Post, Integer>, JpaSpecificationExecutor<Post> {
 
+    /**
+     * Find posts by status and type
+     *
+     * @param status   status
+     * @param type     type
+     * @param pageable pageable
+     *
+     * @return Page<Post>
+     */
+    Page<Post> queryAllByStatusAndType(int status, Integer type, Pageable pageable);
+
+    /**
+     * Count posts by status and type
+     *
+     * @param status status
+     * @param type   type
+     *
+     * @return posts count
+     */
+    Long countAllByStatusAndType(int status, Integer type);
 }
--- src/main/java/cc/ryanc/halo/web/controller/admin/PostController.java
+++ src/main/java/cc/ryanc/halo/web/controller/admin/PostController.java
@@ -1,7 +1,21 @@
 package cc.ryanc.halo.web.controller.admin;
 
+import cc.ryanc.halo.model.dto.post.PostSimpleOutputDTO;
+import cc.ryanc.halo.model.enums.PostStatus;
+import cc.ryanc.halo.model.enums.PostType;
+import cc.ryanc.halo.service.PostService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.SortDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import static org.springframework.data.domain.Sort.Direction.DESC;
 
 /**
  * Posts controller
@@ -12,4 +26,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value = "/admin/posts")
 public class PostController {
+
+    private PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    /**
+     * posts manage
+     *
+     * @param model  model
+     * @param status post status
+     * @param page   current page
+     * @param sort   sort
+     *
+     * @return template path: admin/admin_post.ftl
+     */
+    @GetMapping
+    public String posts(Model model,
+                        @RequestParam(value = "status", defaultValue = "0") Integer status,
+                        @RequestParam(value = "page", defaultValue = "0") Integer page,
+                        @SortDefault.SortDefaults({
+                                @SortDefault(sort = "postPriority", direction = DESC),
+                                @SortDefault(sort = "postDate", direction = DESC)
+                        }) Sort sort) {
+        final Pageable pageable = PageRequest.of(page, 10, sort);
+        final Page<PostSimpleOutputDTO> posts = postService.listByStatus(status, PostType.POST.getValue(), pageable);
+        model.addAttribute("posts", posts);
+        model.addAttribute("publishCount", postService.countByStatus(PostStatus.PUBLISHED.getValue(), PostType.POST.getValue()));
+        model.addAttribute("draftCount", postService.countByStatus(PostStatus.DRAFT.getValue(), PostType.POST.getValue()));
+        model.addAttribute("trashCount", postService.countByStatus(PostStatus.RECYCLE.getValue(), PostType.POST.getValue()));
+        model.addAttribute("status", status);
+        return "admin/admin_post";
+    }
 }
