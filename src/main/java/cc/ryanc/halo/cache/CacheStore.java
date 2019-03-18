5771b42c251f727529e13386556fe7ed800dd20b
johnniang <johnniang@riseup.net>
2019-03-18 21:16:19 +0800

    Add custom temporary store support

---
+++ src/main/java/cc/ryanc/halo/cache/CacheStore.java
@@ -0,0 +1,44 @@
+package cc.ryanc.halo.cache;
+
+import org.springframework.lang.NonNull;
+
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Cache store interface.
+ *
+ * @param <K> cache key type
+ * @param <V> cache value type
+ * @author johnniang
+ * *
+ */
+public interface CacheStore<K, V> {
+
+    /**
+     * Gets by cache key.
+     *
+     * @param key must not be null
+     * @return cache value
+     */
+    @NonNull
+    Optional<V> get(@NonNull K key);
+
+    /**
+     * Puts a cache.
+     *
+     * @param key      cache key must not be null
+     * @param value    cache value must not be null
+     * @param timeout  the key expiration must not be less than 0
+     * @param timeUnit timeout unit
+     */
+    void put(@NonNull K key, @NonNull V value, long timeout, @NonNull TimeUnit timeUnit);
+
+    /**
+     * Delete a key.
+     *
+     * @param key cache key must not be null
+     */
+    void delete(@NonNull K key);
+
+}
