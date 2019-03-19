23e95cc2a847f9b5b4814f1504dfa14feefb91b6
johnniang <johnniang@riseup.net>
2019-03-19 18:31:08 +0800

    Registry PageJacksonSerializer manually

--- src/main/java/cc/ryanc/halo/config/WebMvcAutoConfiguration.java
+++ src/main/java/cc/ryanc/halo/config/WebMvcAutoConfiguration.java
@@ -1,16 +1,23 @@
 package cc.ryanc.halo.config;
 
 import cc.ryanc.halo.config.properties.HaloProperties;
-import cc.ryanc.halo.security.resolver.AuthenticationArgumentResolver;
 import cc.ryanc.halo.factory.StringToEnumConverterFactory;
+import cc.ryanc.halo.security.resolver.AuthenticationArgumentResolver;
+import cc.ryanc.halo.web.controller.support.PageJacksonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jackson.JsonComponentModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -38,6 +45,20 @@ public class WebMvcAutoConfiguration implements WebMvcConfigurer {
 
     @Autowired
     private HaloProperties haloProperties;
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.stream()
+                .filter(c -> c instanceof MappingJackson2HttpMessageConverter)
+                .findFirst().ifPresent(converter -> {
+            MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = (MappingJackson2HttpMessageConverter) converter;
+            Jackson2ObjectMapperBuilder builder = Jackson2ObjectMapperBuilder.json();
+            JsonComponentModule module = new JsonComponentModule();
+            module.addSerializer(PageImpl.class, new PageJacksonSerializer());
+            ObjectMapper objectMapper = builder.modules(module).build();
+            mappingJackson2HttpMessageConverter.setObjectMapper(objectMapper);
+        });
+    }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
