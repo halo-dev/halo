1fa38f1171d6105de572aac9d354fac57618cc28
johnniang <johnniang@riseup.net>
2019-03-19 19:42:41 +0800

    Add LogController

--- src/main/java/cc/ryanc/halo/web/controller/admin/api/PostController.java
+++ src/main/java/cc/ryanc/halo/web/controller/admin/api/PostController.java
@@ -2,6 +2,7 @@ package cc.ryanc.halo.web.controller.admin.api;
 
 import cc.ryanc.halo.model.dto.post.PostSimpleOutputDTO;
 import cc.ryanc.halo.service.PostService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 3/19/19
  */
 @RestController
-@RequestMapping("/api/admin/posts")
+@RequestMapping("/admin/api/posts")
 public class PostController {
 
     private final PostService postService;
@@ -25,6 +26,7 @@ public class PostController {
     }
 
     @GetMapping("latest")
+    @ApiOperation("Page latest post")
     public Page<PostSimpleOutputDTO> pageLatest(@RequestParam(name = "top", defaultValue = "10") int top) {
         return postService.pageLatest(top);
     }
