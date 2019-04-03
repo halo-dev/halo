7793737459f64c45e4317cc2a506399244ec1600
johnniang <johnniang@riseup.net>
2019-04-03 17:04:06 +0800

    Complete menu list api

--- src/main/java/cc/ryanc/halo/service/impl/MenuServiceImpl.java
+++ src/main/java/cc/ryanc/halo/service/impl/MenuServiceImpl.java
@@ -1,10 +1,18 @@
 package cc.ryanc.halo.service.impl;
 
+import cc.ryanc.halo.model.dto.MenuOutputDTO;
 import cc.ryanc.halo.model.entity.Menu;
 import cc.ryanc.halo.repository.MenuRepository;
 import cc.ryanc.halo.service.MenuService;
 import cc.ryanc.halo.service.base.AbstractCrudService;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * MenuService implementation class
@@ -20,5 +28,22 @@ public class MenuServiceImpl extends AbstractCrudService<Menu, Integer> implemen
     public MenuServiceImpl(MenuRepository menuRepository) {
         super(menuRepository);
         this.menuRepository = menuRepository;
+    }
+
+    @Override
+    public List<MenuOutputDTO> listDtos(Sort sort) {
+        Assert.notNull(sort, "Sort info must not be null");
+
+        return convertTo(listAll(sort));
+    }
+
+    private List<MenuOutputDTO> convertTo(List<Menu> menus) {
+        if (CollectionUtils.isEmpty(menus)) {
+            return Collections.emptyList();
+        }
+
+        return menus.stream()
+                .map(menu -> new MenuOutputDTO().<MenuOutputDTO>convertFrom(menu))
+                .collect(Collectors.toList());
     }
 }
