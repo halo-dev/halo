c9fadc7823116431dda638750ed663200d9d8598
johnniang <johnniang@riseup.net>
2019-04-03 17:33:41 +0800

    Remove url validation on url property of Menu

--- src/main/java/cc/ryanc/halo/model/params/MenuParam.java
+++ src/main/java/cc/ryanc/halo/model/params/MenuParam.java
@@ -4,7 +4,6 @@ import cc.ryanc.halo.model.dto.base.InputConverter;
 import cc.ryanc.halo.model.entity.Menu;
 import lombok.Data;
 import lombok.ToString;
-import org.hibernate.validator.constraints.URL;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
@@ -26,7 +25,6 @@ public class MenuParam implements InputConverter<Menu> {
 
     @NotBlank(message = "Menu url must not be blank")
     @Size(max = 1023, message = "Length of menu url must not be more than {max}")
-    @URL(message = "Menu url has incorrect url format")
     private String url;
 
     @Min(value = 0, message = "Menu sort must not be less than {value}")
