9d037e2cc312aacfaedc194602c0eb8468b3d7bd
Aanko <952396285@qq.com>
2019-04-09 16:36:12 +0800

    动态切换title

--- src/main.js
+++ src/main.js
@@ -13,6 +13,13 @@ import animated from 'animate.css'
 
 Vue.config.productionTip = false
 
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
+  next()
+})
+
 Vue.use(router)
 Vue.use(animated)
 
