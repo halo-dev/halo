881674ab8759200e23c658b52c52b2ecc239cd3f
ruibaby <i@ryanc.cc>
2019-04-10 20:56:42 +0800

    Refactor PageList

--- src/views/page/PageList.vue
+++ src/views/page/PageList.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="page-header-index-wide">
-    Page list
+    <a-row>
+      <a-col :xl="24" :lg="24" :md="24" :sm="24" :xs="24">
+        <div class="card-container">
+          <a-tabs type="card">
+            <a-tab-pane tab="内置页面" key="internal">
+              内置页面
+            </a-tab-pane>
+            <a-tab-pane tab="自定义页面" key="custom">
+              自定义页面
+            </a-tab-pane>
+          </a-tabs>
+        </div>
+      </a-col>
+    </a-row>
+  </div>
   </div>
 </template>
 
@@ -8,4 +22,34 @@
 export default {}
 </script>
 
-<style scoped></style>
+<style>
+.card-container {
+  background: #f5f5f5;
+}
+.card-container > .ant-tabs-card > .ant-tabs-content {
+  margin-top: -16px;
+}
+
+.card-container > .ant-tabs-card > .ant-tabs-content > .ant-tabs-tabpane {
+  background: #fff;
+  padding: 16px;
+}
+
+.card-container > .ant-tabs-card > .ant-tabs-bar {
+  border-color: #fff;
+}
+
+.card-container > .ant-tabs-card > .ant-tabs-bar .ant-tabs-tab {
+  border-color: transparent;
+  background: transparent;
+}
+
+.card-container > .ant-tabs-card > .ant-tabs-bar .ant-tabs-tab-active {
+  border-color: #fff;
+  background: #fff;
+}
+.ant-form-vertical .ant-form-item {
+  padding-bottom: 0;
+}
+</style>
+
