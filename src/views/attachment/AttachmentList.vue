0d6e3572c734b6afd95323668c111b34f053fa6e
ruibaby <i@ryanc.cc>
2019-04-10 20:32:00 +0800

    JSLint

--- src/views/attachment/AttachmentList.vue
+++ src/views/attachment/AttachmentList.vue
@@ -1,42 +1,90 @@
 <template>
   <page-view>
     <a-row :gutter="12" type="flex" align="middle">
-      <a-col class="attachment-item" :xl="4" :lg="4" :md="12" :sm="12" :xs="24">
+      <a-col
+        class="attachment-item"
+        :xl="4"
+        :lg="4"
+        :md="12"
+        :sm="12"
+        :xs="24">
         <a-card :bodyStyle="{ padding: '14px' }">
           <img :src="src" slot="cover">
         </a-card>
       </a-col>
-      <a-col class="attachment-item" :xl="4" :lg="4" :md="12" :sm="12" :xs="24">
+      <a-col
+        class="attachment-item"
+        :xl="4"
+        :lg="4"
+        :md="12"
+        :sm="12"
+        :xs="24">
         <a-card :bodyStyle="{ padding: '14px' }">
           <img :src="src" slot="cover">
         </a-card>
       </a-col>
-      <a-col class="attachment-item" :xl="4" :lg="4" :md="12" :sm="12" :xs="24">
+      <a-col
+        class="attachment-item"
+        :xl="4"
+        :lg="4"
+        :md="12"
+        :sm="12"
+        :xs="24">
         <a-card :bodyStyle="{ padding: '14px' }">
           <img :src="src" slot="cover">
         </a-card>
       </a-col>
-      <a-col class="attachment-item" :xl="4" :lg="4" :md="12" :sm="12" :xs="24">
+      <a-col
+        class="attachment-item"
+        :xl="4"
+        :lg="4"
+        :md="12"
+        :sm="12"
+        :xs="24">
         <a-card :bodyStyle="{ padding: '14px' }">
           <img :src="src" slot="cover">
         </a-card>
       </a-col>
-      <a-col class="attachment-item" :xl="4" :lg="4" :md="12" :sm="12" :xs="24">
+      <a-col
+        class="attachment-item"
+        :xl="4"
+        :lg="4"
+        :md="12"
+        :sm="12"
+        :xs="24">
         <a-card :bodyStyle="{ padding: '14px' }">
           <img :src="src" slot="cover">
         </a-card>
       </a-col>
-      <a-col class="attachment-item" :xl="4" :lg="4" :md="12" :sm="12" :xs="24">
+      <a-col
+        class="attachment-item"
+        :xl="4"
+        :lg="4"
+        :md="12"
+        :sm="12"
+        :xs="24">
         <a-card :bodyStyle="{ padding: '14px' }">
           <img :src="src" slot="cover">
         </a-card>
       </a-col>
-      <a-col class="attachment-item" :xl="4" :lg="4" :md="12" :sm="12" :xs="24">
+      <a-col
+        class="attachment-item"
+        :xl="4"
+        :lg="4"
+        :md="12"
+        :sm="12"
+        :xs="24">
         <a-card :bodyStyle="{ padding: '14px' }">
           <img :src="src" slot="cover">
         </a-card>
       </a-col>
-      <a-col class="attachment-item" :xl="4" :lg="4" :md="12" :sm="12" :xs="24">
+      <a-col
+        class="attachment-item"
+        :xl="4"
+        :lg="4"
+        :md="12"
+        :sm="12"
+        :xs="24">
         <a-card :bodyStyle="{ padding: '14px' }">
           <img :src="src" slot="cover">
         </a-card>
