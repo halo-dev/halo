cf310fb54da83712a79de3317f023716ae2b11ce
johnniang <johnniang@riseup.net>
2019-04-10 11:53:22 +0800

    Complete yaml theme configuration resolver

--- src/main/java/run/halo/app/handler/theme/ThemeConfigResolver.java
+++ src/main/java/run/halo/app/handler/theme/ThemeConfigResolver.java
@@ -1,12 +1,25 @@
 package run.halo.app.handler.theme;
 
+import org.springframework.lang.NonNull;
+
+import java.io.IOException;
+import java.util.List;
+
 /**
- * Theme configuration resolver.
+ * Theme config resolver interface.
  *
  * @author johnniang
  * @date 4/10/19
  */
-public class ThemeConfigResolver {
+public interface ThemeConfigResolver {
 
-
+    /**
+     * Resolves content as tab.
+     *
+     * @param content content must not be blank
+     * @return a list of tab
+     * @throws IOException throws when content conversion fails
+     */
+    @NonNull
+    List<Tab> resolve(@NonNull String content) throws IOException;
 }
