3c303c0c124e4d3df89381ccbb578d58d28e042a
ruibaby <i@ryanc.cc>
2019-04-17 19:40:24 +0800

    Fixed style error.

--- src/views/page/internal/JournalList.vue
+++ src/views/page/internal/JournalList.vue
@@ -2,27 +2,27 @@
   <div class="page-header-index-wide">
     <a-row>
       <a-col :xl="24" :lg="24" :md="24" :sm="24" :xs="24">
-        <a-list
-          class="demo-loadmore-list"
-          :loading="loading"
-          itemLayout="horizontal"
-          :dataSource="data"
-        >
-          <a-list-item slot="renderItem" slot-scope="item, index" :key="index">
-            <a slot="actions">edit</a>
-            <a slot="actions">more</a>
-            <a-list-item-meta
-              :description="item.url"
-            >
-              <a slot="title" href="https://vue.ant.design/">{{ item.url }}</a>
-              <a-avatar
-                slot="avatar"
-                src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-              />
-            </a-list-item-meta>
-            <div>content</div>
-          </a-list-item>
-        </a-list>
+        <a-card>
+          <a-list
+            class="demo-loadmore-list"
+            :loading="loading"
+            itemLayout="horizontal"
+            :dataSource="data"
+          >
+            <a-list-item slot="renderItem" slot-scope="item, index" :key="index">
+              <a slot="actions">edit</a>
+              <a slot="actions">more</a>
+              <a-list-item-meta :description="item.url">
+                <a slot="title" href="https://vue.ant.design/">{{ item.url }}</a>
+                <a-avatar
+                  slot="avatar"
+                  src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+                />
+              </a-list-item-meta>
+              <div>content</div>
+            </a-list-item>
+          </a-list>
+        </a-card>
       </a-col>
     </a-row>
   </div>
