16476f5a9bf79a8f454d428e1f70c9e137bc2e33
johnniang <johnniang@riseup.net>
2019-04-25 22:09:05 +0800

    Refactor sheet service and contorller

--- src/main/java/run/halo/app/controller/admin/api/SheetController.java
+++ src/main/java/run/halo/app/controller/admin/api/SheetController.java
@@ -5,11 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
-import run.halo.app.model.dto.post.SheetDetailDTO;
-import run.halo.app.model.vo.SheetListVO;
+import run.halo.app.model.dto.post.BasePostDetailDTO;
 import run.halo.app.model.entity.Sheet;
 import run.halo.app.model.enums.PostStatus;
 import run.halo.app.model.params.SheetParam;
+import run.halo.app.model.vo.SheetListVO;
 import run.halo.app.service.SheetService;
 
 import javax.validation.Valid;
@@ -34,28 +34,28 @@ public class SheetController {
 
     @GetMapping("{sheetId:\\d+}")
     @ApiOperation("Gets a sheet")
-    public SheetDetailDTO getBy(@PathVariable("sheetId") Integer sheetId) {
+    public BasePostDetailDTO getBy(@PathVariable("sheetId") Integer sheetId) {
         Sheet sheet = sheetService.getById(sheetId);
-        return sheetService.convertToDetailDto(sheet);
+        return sheetService.convertToDetail(sheet);
     }
 
     @GetMapping
     @ApiOperation("Gets a page of sheet")
     public Page<SheetListVO> pageBy(@PageableDefault(sort = "editTime", direction = DESC) Pageable pageable) {
         Page<Sheet> sheetPage = sheetService.pageBy(pageable);
-        return sheetService.convertToListDto(sheetPage);
+        return sheetService.convertToListVo(sheetPage);
     }
 
     @PostMapping
     @ApiOperation("Creates a sheet")
-    public SheetDetailDTO createBy(@RequestBody @Valid SheetParam sheetParam) {
+    public BasePostDetailDTO createBy(@RequestBody @Valid SheetParam sheetParam) {
         Sheet sheet = sheetService.createBy(sheetParam.convertTo());
-        return sheetService.convertToDetailDto(sheet);
+        return sheetService.convertToDetail(sheet);
     }
 
     @PutMapping("{sheetId:\\d+}")
     @ApiOperation("Updates a sheet")
-    public SheetDetailDTO updateBy(
+    public BasePostDetailDTO updateBy(
             @PathVariable("sheetId") Integer sheetId,
             @RequestBody @Valid SheetParam sheetParam) {
         Sheet sheetToUpdate = sheetService.getById(sheetId);
@@ -64,7 +64,7 @@ public class SheetController {
 
         Sheet sheet = sheetService.updateBy(sheetToUpdate);
 
-        return sheetService.convertToDetailDto(sheet);
+        return sheetService.convertToDetail(sheet);
     }
 
     @PutMapping("{sheetId:\\d+}/{status}")
@@ -82,8 +82,8 @@ public class SheetController {
 
     @DeleteMapping("{sheetId:\\d+}")
     @ApiOperation("Deletes a sheet")
-    public SheetDetailDTO deleteBy(@PathVariable("sheetId") Integer sheetId) {
+    public BasePostDetailDTO deleteBy(@PathVariable("sheetId") Integer sheetId) {
         Sheet sheet = sheetService.removeById(sheetId);
-        return sheetService.convertToDetailDto(sheet);
+        return sheetService.convertToDetail(sheet);
     }
 }
