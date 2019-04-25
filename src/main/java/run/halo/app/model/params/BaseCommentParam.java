261e8c661398735e0438eeea3a10a5e7ed682b86
johnniang <johnniang@riseup.net>
2019-04-25 19:58:08 +0800

    Enhance InputConverter

---
+++ src/main/java/run/halo/app/model/params/BaseCommentParam.java
@@ -0,0 +1,48 @@
+package run.halo.app.model.params;
+
+import lombok.Data;
+import run.halo.app.model.dto.base.InputConverter;
+import run.halo.app.utils.ReflectionUtils;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.lang.reflect.ParameterizedType;
+
+/**
+ * Base Comment param.
+ *
+ * @author johnniang
+ * @date 3/22/19
+ */
+@Data
+public abstract class BaseCommentParam<COMMENT> implements InputConverter<COMMENT> {
+
+    @NotBlank(message = "Comment author name must not be blank")
+    @Size(max = 50, message = "Length of comment author name must not be more than {max}")
+    private String author;
+
+    @NotBlank(message = "Comment email must not be blank")
+    @Email(message = "Comment email's format is incorrect")
+    @Size(max = 255, message = "Length of comment email must not be more than {max}")
+    private String email;
+
+    @Size(max = 127, message = "Length of comment author url must not be more than {max}")
+    private String authorUrl;
+
+    @NotBlank(message = "Comment content must not be blank")
+    @Size(max = 1023, message = "Length of comment content must not be more than {max}")
+    private String content;
+
+    @Min(value = 1, message = "Post id must not be less than {value}")
+    private Integer postId;
+
+    @Min(value = 0, message = "Comment parent id must not be less than {value}")
+    private Long parentId = 0L;
+
+    @Override
+    public ParameterizedType getParameterizedType() {
+        return ReflectionUtils.getParameterizedTypeBySuperClass(BaseCommentParam.class, this.getClass());
+    }
+}
