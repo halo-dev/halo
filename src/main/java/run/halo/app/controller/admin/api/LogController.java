389e9d93cc8632a72d25e06dd5c47fa0c37609f8
ruibaby <i@ryanc.cc>
2019-05-05 23:31:30 +0800

    Create pageBy api for log.

--- src/main/java/run/halo/app/controller/admin/api/LogController.java
+++ src/main/java/run/halo/app/controller/admin/api/LogController.java
@@ -1,14 +1,20 @@
 package run.halo.app.controller.admin.api;
 
 import io.swagger.annotations.ApiOperation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import run.halo.app.model.dto.LogDTO;
+import run.halo.app.model.entity.Log;
 import run.halo.app.service.LogService;
 
 import java.util.List;
+
+import static org.springframework.data.domain.Sort.Direction.DESC;
 
 /**
  * Log controller.
@@ -36,6 +42,12 @@ public class LogController {
     @ApiOperation("Pages latest logs")
     public List<LogDTO> pageLatest(@RequestParam(name = "top", defaultValue = "10") int top) {
         return logService.pageLatest(top).getContent();
+    }
+
+    @GetMapping
+    public Page<LogDTO> pageBy(@PageableDefault(sort = "updateTime", direction = DESC) Pageable pageable){
+        Page<Log> logPage = logService.listAll(pageable);
+        return logPage.map(log -> new LogDTO().convertFrom(log));
     }
 
     /**
