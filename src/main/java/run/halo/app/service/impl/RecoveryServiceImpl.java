c52067aa32d376ac925ff93845a2a9065bbbfb59
johnniang <johnniang@riseup.net>
2019-05-07 20:16:10 +0800

    Fix migration error

--- src/main/java/run/halo/app/service/impl/RecoveryServiceImpl.java
+++ src/main/java/run/halo/app/service/impl/RecoveryServiceImpl.java
@@ -24,6 +24,7 @@ import run.halo.app.utils.ServiceUtils;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -94,7 +95,7 @@ public class RecoveryServiceImpl implements RecoveryService {
     public void migrateFromV0_4_3(MultipartFile file) {
         // Get migration content
         try {
-            String migrationContent = FileCopyUtils.copyToString(new InputStreamReader(file.getInputStream()));
+            String migrationContent = FileCopyUtils.copyToString(new InputStreamReader(file.getInputStream(), StandardCharsets.UTF_8));
 
             Object migrationObject = JsonUtils.jsonToObject(migrationContent, Object.class);
 
