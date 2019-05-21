1fafc67998b8dc4ca0ab30ce915847cd0e0d6e24
John Niang <johnniang@riseup.net>
2019-05-21 16:19:45 +0800

    Update .travis.yml (halo-dev/console#9)

--- scripts/zip_dist.sh
+++ scripts/zip_dist.sh
@@ -2,3 +2,5 @@
 PACKAGE_VERSION=$(sed -nE 's/^\s*"version": "(.*?)",$/\1/p' package.json)
 echo "Halo admin version $PACKAGE_VERSION"
 zip -r dist/halo-admin-$PACKAGE_VERSION.zip dist
+
+export PACKAGE_VERSION
