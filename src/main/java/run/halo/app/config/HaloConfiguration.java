5344101d3d2c7fdbb3641c897299ff26cafbb574
johnniang <johnniang@riseup.net>
2019-05-24 07:48:59 +0800

    Change exclude url pattern for comment related api

--- src/main/java/run/halo/app/config/HaloConfiguration.java
+++ src/main/java/run/halo/app/config/HaloConfiguration.java
@@ -113,7 +113,7 @@ public class HaloConfiguration {
         ApiAuthenticationFilter apiFilter = new ApiAuthenticationFilter(haloProperties, optionService);
         apiFilter.addExcludeUrlPatterns(
                 "/api/content/*/comments",
-                "/api/content/**/comments/*"
+                "/api/content/**/comments/**"
         );
 
         DefaultAuthenticationFailureHandler failureHandler = new DefaultAuthenticationFailureHandler();
