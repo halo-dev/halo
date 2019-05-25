fddf09382b06b89301ae2889d402a401ae85fe6b
ruibaby <i@ryanc.cc>
2019-05-25 13:39:55 +0800

    Complete: #169

--- src/main/java/run/halo/app/controller/content/api/JournalController.java
+++ src/main/java/run/halo/app/controller/content/api/JournalController.java
@@ -8,12 +8,18 @@ import org.springframework.data.web.SortDefault;
 import org.springframework.web.bind.annotation.*;
 import run.halo.app.cache.lock.CacheLock;
 import run.halo.app.model.dto.BaseCommentDTO;
+import run.halo.app.model.entity.JournalComment;
+import run.halo.app.model.entity.PostComment;
+import run.halo.app.model.enums.CommentStatus;
 import run.halo.app.model.params.JournalCommentParam;
 import run.halo.app.model.vo.BaseCommentVO;
 import run.halo.app.model.vo.BaseCommentWithParentVO;
+import run.halo.app.model.vo.CommentWithHasChildrenVO;
 import run.halo.app.service.JournalCommentService;
 import run.halo.app.service.JournalService;
 import run.halo.app.service.OptionService;
+
+import java.util.List;
 
 import static org.springframework.data.domain.Sort.Direction.DESC;
 
@@ -37,6 +43,23 @@ public class JournalController {
         this.journalService = journalService;
         this.journalCommentService = journalCommentService;
         this.optionService = optionService;
+    }
+
+    @GetMapping("{journalId:\\d+}/comments/top_view")
+    public Page<CommentWithHasChildrenVO> listTopComments(@PathVariable("journalId") Integer journalId,
+                                                          @RequestParam(name = "page", required = false, defaultValue = "0") int page,
+                                                          @SortDefault(sort = "createTime", direction = DESC) Sort sort) {
+        return journalCommentService.pageTopCommentsBy(journalId, CommentStatus.PUBLISHED, PageRequest.of(page, optionService.getCommentPageSize(), sort));
+    }
+
+    @GetMapping("{journalId:\\d+}/comments/{commentParentId:\\d+}/children")
+    public List<BaseCommentDTO> listChildrenBy(@PathVariable("journalId") Integer journalId,
+                                               @PathVariable("commentParentId") Long commentParentId,
+                                               @SortDefault(sort = "createTime", direction = DESC) Sort sort) {
+        // Find all children comments
+        List<JournalComment> postComments = journalCommentService.listChildrenBy(journalId, commentParentId, CommentStatus.PUBLISHED, sort);
+        // Convert to base comment dto
+        return journalCommentService.convertTo(postComments);
     }
 
     @GetMapping("{journalId:\\d+}/comments/tree_view")
