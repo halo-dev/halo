109c2e099d1c238680f74d5d3657e8d506671507
ruibaby <i@ryanc.cc>
2019-06-05 21:24:39 +0800

    Support war package.

--- src/main/java/run/halo/app/Application.java
+++ src/main/java/run/halo/app/Application.java
@@ -2,6 +2,8 @@ package run.halo.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -21,7 +23,7 @@ import run.halo.app.repository.base.BaseRepositoryImpl;
 @EnableScheduling
 @EnableAsync
 @EnableJpaRepositories(basePackages = "run.halo.app.repository", repositoryBaseClass = BaseRepositoryImpl.class)
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         // Customize the spring config location
@@ -29,5 +31,11 @@ public class Application {
 
         // Run application
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        System.setProperty("spring.config.additional-location", "file:${user.home}/.halo/,file:${user.home}/halo-dev/");
+        return application.sources(Application.class);
     }
 }
