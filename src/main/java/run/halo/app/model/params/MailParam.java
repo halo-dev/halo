19bc819c555442314048160aa75cdc4cef5bcab0
Tao Wei <twchn@live.com>
2019-06-09 10:46:58 +0800

    Add email address validation

--- src/main/java/run/halo/app/model/params/MailParam.java
+++ src/main/java/run/halo/app/model/params/MailParam.java
@@ -2,6 +2,7 @@ package run.halo.app.model.params;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 /**
@@ -14,6 +15,7 @@ import javax.validation.constraints.NotBlank;
 public class MailParam {
 
     @NotBlank(message = "收件人不能为空")
+    @Email(message = "邮箱格式错误")
     private String to;
 
     @NotBlank(message = "主题不能为空")
