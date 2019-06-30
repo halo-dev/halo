48ce6844fa8162c1171b3f26186368433fd030a8
ruibaby <i@ryanc.cc>
2019-06-30 13:36:51 +0800

    Fixed #28

--- src/views/comment/components/CommentTab.vue
+++ src/views/comment/components/CommentTab.vue
@@ -408,7 +408,11 @@ export default {
       this.selectComment = comment
       this.replyCommentVisible = true
       this.replyComment.parentId = comment.id
-      this.replyComment.postId = comment.post.id
+      if (this.type === 'posts') {
+        this.replyComment.postId = comment.post.id
+      } else {
+        this.replyComment.postId = comment.sheet.id
+      }
     },
     handleCreateClick() {
       commentApi.create(this.type, this.replyComment).then(response => {
