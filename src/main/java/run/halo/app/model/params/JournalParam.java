698bd4c098f5ed68d566ea33e33231723abdcc16
ruibaby <i@ryanc.cc>
2019-08-27 12:32:37 +0800

    Support journal type.

--- src/main/java/run/halo/app/model/params/JournalParam.java
+++ src/main/java/run/halo/app/model/params/JournalParam.java
@@ -3,6 +3,7 @@ package run.halo.app.model.params;
 import lombok.Data;
 import run.halo.app.model.dto.base.InputConverter;
 import run.halo.app.model.entity.Journal;
+import run.halo.app.model.enums.JournalType;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -11,6 +12,7 @@ import javax.validation.constraints.Size;
  * Journal param.
  *
  * @author johnniang
+ * @author ryanwang
  * @date 19-4-25
  */
 @Data
@@ -19,4 +21,6 @@ public class JournalParam implements InputConverter<Journal> {
     @NotBlank(message = "内容不能为空")
     @Size(max = 511, message = "内容的字符长度不能超过 {max}")
     private String content;
+
+    private JournalType type = JournalType.PUBLIC;
 }
