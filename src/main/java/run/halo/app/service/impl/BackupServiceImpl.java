6b6a3b3dc85db27a7a3001ba137ffa0a35a6fb82
bobo <yabo_han@163.com>
2019-11-14 09:47:09 +0800

    Batch export markdown backup in hexo format

--- src/main/java/run/halo/app/service/impl/BackupServiceImpl.java
+++ src/main/java/run/halo/app/service/impl/BackupServiceImpl.java
@@ -1,14 +1,31 @@
 package run.halo.app.service.impl;
 
 import cn.hutool.core.io.IoUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.yaml.snakeyaml.Yaml;
 import run.halo.app.model.dto.post.BasePostDetailDTO;
+import run.halo.app.model.entity.Post;
+import run.halo.app.model.entity.Tag;
 import run.halo.app.service.BackupService;
 import run.halo.app.service.PostService;
+import run.halo.app.service.PostTagService;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Backup service implementation.
@@ -17,13 +34,15 @@ import java.nio.charset.StandardCharsets;
  * @date 2019-04-26
  */
 @Service
+@Slf4j
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class BackupServiceImpl implements BackupService {
 
     private final PostService postService;
 
-    public BackupServiceImpl(PostService postService) {
-        this.postService = postService;
-    }
+    private final PostTagService postTagService;
+
+    public static final String LINE_SEPARATOR = System.getProperty("line.separator");
 
     @Override
     public BasePostDetailDTO importMarkdown(MultipartFile file) throws IOException {
@@ -34,5 +53,80 @@ public class BackupServiceImpl implements BackupService {
         // TODO sheet import
 
         return postService.importMarkdown(markdown, file.getOriginalFilename());
+    }
+
+    @Override
+    public JSONObject exportHexoMDs() {
+        final JSONObject ret = new JSONObject();
+        final List<JSONObject> posts = new ArrayList<>();
+        ret.put("posts", (Object) posts);
+        final List<JSONObject> passwords = new ArrayList<>();
+        ret.put("passwords", (Object) passwords);
+        final List<JSONObject> drafts = new ArrayList<>();
+        ret.put("drafts", (Object) drafts);
+
+        List<Post> postList = postService.listAll();
+        Map<Integer, List<Tag>> talMap = postTagService.listTagListMapBy(postList.stream().map(Post::getId).collect(Collectors.toList()));
+        for (Post post : postList) {
+            final Map<String, Object> front = new LinkedHashMap<>();
+            final String title = post.getTitle();
+            front.put("title", title);
+            final String date = DateFormatUtils.format(post.getCreateTime(), "yyyy-MM-dd HH:mm:ss");
+            front.put("date", date);
+            front.put("updated", DateFormatUtils.format(post.getUpdateTime(), "yyyy-MM-dd HH:mm:ss"));
+            final List<String> tags = talMap.get(post.getId()).stream().map(Tag::getName).collect(Collectors.toList());
+            if (tags.isEmpty()) {
+                tags.add("halo");
+            }
+            front.put("tags", tags);
+            front.put("permalink", "");
+            final JSONObject one = new JSONObject();
+            one.put("front", new Yaml().dump(front));
+            one.put("title", title);
+            one.put("content", post.getOriginalContent());
+            one.put("created", post.getCreateTime().getTime());
+
+            if (StringUtils.isNotBlank(post.getPassword())) {
+                passwords.add(one);
+                continue;
+            } else if (post.getDeleted()) {
+                drafts.add(one);
+                continue;
+            } else {
+                posts.add(one);
+            }
+        }
+
+        return ret;
+    }
+
+    @Override
+    public void exportHexoMd(List<JSONObject> posts, String dirPath) {
+        posts.forEach(post -> {
+            final String filename = sanitizeFilename(post.optString("title")) + ".md";
+            final String text = post.optString("front") + "---" + LINE_SEPARATOR + post.optString("content");
+
+            try {
+                final String date = DateFormatUtils.format(post.optLong("created"), "yyyyMM");
+                final String dir = dirPath + File.separator + date + File.separator;
+                new File(dir).mkdirs();
+                FileUtils.writeStringToFile(new File(dir + filename), text, "UTF-8");
+            } catch (final Exception e) {
+                log.error("Write markdown file failed", e);
+            }
+        });
+    }
+
+    /**
+     * Sanitizes the specified file name.
+     *
+     * @param unsanitized the specified file name
+     * @return sanitized file name
+     */
+    public static String sanitizeFilename(final String unsanitized) {
+        return unsanitized.
+                replaceAll("[^(a-zA-Z0-9\\u4e00-\\u9fa5\\.)]", "").
+                replaceAll("[\\?\\\\/:|<>\\*\\[\\]\\(\\)\\$%\\{\\}@~]", "").
+                replaceAll("\\s", "");
     }
 }
