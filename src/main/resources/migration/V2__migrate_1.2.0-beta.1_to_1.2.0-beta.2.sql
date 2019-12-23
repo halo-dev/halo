90757f6555e6339fb72cdc193e7d2c56f97eab82
ruibaby <i@ryanc.cc>
2019-12-23 17:39:16 +0800

    release: 1.2.0-beta.3.

--- src/main/resources/migration/V2__migrate_1.2.0-beta.1_to_1.2.0-beta.2.sql
+++ src/main/resources/migration/V2__migrate_1.2.0-beta.1_to_1.2.0-beta.2.sql
@@ -2,5 +2,5 @@
 
 -- Migrate journals Table
 alter table journals modify content text not null;
-
+alter table journals add source_content varchar(1023) default '' not null;
 update journals set `source_content`=`content`
