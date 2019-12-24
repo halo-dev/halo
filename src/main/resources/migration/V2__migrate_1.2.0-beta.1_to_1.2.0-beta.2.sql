4cdfc2247c0a081b222da1408835c2e6c773607b
ruibaby <i@ryanc.cc>
2019-12-24 19:48:47 +0800

    fix: migrate database error.

--- src/main/resources/migration/V2__migrate_1.2.0-beta.1_to_1.2.0-beta.2.sql
+++ src/main/resources/migration/V2__migrate_1.2.0-beta.1_to_1.2.0-beta.2.sql
@@ -2,5 +2,4 @@
 
 -- Migrate journals Table
 alter table journals modify content text not null;
-alter table journals add source_content varchar(1023) default '' not null;
 update journals set `source_content`=`content`
