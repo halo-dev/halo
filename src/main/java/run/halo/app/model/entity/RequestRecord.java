e6f6815ba728d0e0b15ee5b858b58360da9bc075
xiongshanghua <xiongshanghua@sz.hitrontech.com>
2020-01-06 18:16:01 +0800

    tmp commit

---
+++ src/main/java/run/halo/app/model/entity/RequestRecord.java
@@ -0,0 +1,58 @@
+package run.halo.app.model.entity;
+
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+/**
+ * RequestRecord entity.
+ *
+ * @author ijkzen
+ */
+@Data
+@Entity
+@Table(name = "record")
+@ToString
+@EqualsAndHashCode(callSuper = true)
+public class RequestRecord extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String operatingSystem;
+
+    @Column
+    private String operatingSystemVersion;
+
+    @Column
+    private String browser;
+
+    @Column
+    private String browserVersion;
+
+    @Column
+    private String device;
+
+    @Column
+    private String ip;
+
+    @Column
+    private String url;
+
+    @Column
+    private String country;
+
+    @Column
+    private String region;
+
+    @Column
+    private String city;
+
+    @Column
+    private String httpMethod;
+}
---
+++ src/main/java/run/halo/app/repository/RequestRecordRepository.java
@@ -0,0 +1,14 @@
+package run.halo.app.repository;
+
+import run.halo.app.model.entity.RequestRecord;
+import run.halo.app.repository.base.BaseRepository;
+
+/**
+ * RequestRecord entity.
+ *
+ * @author ijkzen
+ */
+
+public interface RequestRecordRepository extends BaseRepository<RequestRecord, Long> {
+
+}
