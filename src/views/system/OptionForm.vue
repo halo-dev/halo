96bc90bb2d2925d1f68df03e982235159f488ba5
ruibaby <i@ryanc.cc>
2020-01-07 20:13:17 +0800

    feat: support set path suffix.

--- src/views/system/OptionForm.vue
+++ src/views/system/OptionForm.vue
@@ -589,9 +589,9 @@
               >
                 <a-form-item label="文章固定链接类型：">
                   <template slot="help">
-                    <span v-if="options.post_permalink_type === 'DEFAULT'">{{ options.blog_url }}/archives/${url}</span>
-                    <span v-else-if="options.post_permalink_type === 'DATE'">{{ options.blog_url }}/1970/01/${url}</span>
-                    <span v-else-if="options.post_permalink_type === 'DAY'">{{ options.blog_url }}/1970/01/01/${url}</span>
+                    <span v-if="options.post_permalink_type === 'DEFAULT'">{{ options.blog_url }}/{{ options.archives_prefix }}/${url}{{ options.path_suffix }}</span>
+                    <span v-else-if="options.post_permalink_type === 'DATE'">{{ options.blog_url }}/1970/01/${url}{{ options.path_suffix }}</span>
+                    <span v-else-if="options.post_permalink_type === 'DAY'">{{ options.blog_url }}/1970/01/01/${url}{{ options.path_suffix }}</span>
                     <span v-else-if="options.post_permalink_type === 'ID'">{{ options.blog_url }}/?p=${id}</span>
                   </template>
                   <a-select v-model="options.post_permalink_type">
@@ -604,27 +604,33 @@
                 </a-form-item>
                 <a-form-item label="自定义页面前缀：">
                   <template slot="help">
-                    <span>{{ options.blog_url }}/{{ options.sheet_prefix }}/${url}</span>
+                    <span>{{ options.blog_url }}/{{ options.sheet_prefix }}/${url}{{ options.path_suffix }}</span>
                   </template>
                   <a-input v-model="options.sheet_prefix" />
                 </a-form-item>
                 <a-form-item label="归档前缀：">
                   <template slot="help">
-                    <span>{{ options.blog_url }}/{{ options.archives_prefix }}</span>
+                    <span>{{ options.blog_url }}/{{ options.archives_prefix }}{{ options.path_suffix }}</span>
                   </template>
                   <a-input v-model="options.archives_prefix" />
                 </a-form-item>
                 <a-form-item label="分类前缀：">
                   <template slot="help">
-                    <span>{{ options.blog_url }}/{{ options.categories_prefix }}/${slugName}</span>
+                    <span>{{ options.blog_url }}/{{ options.categories_prefix }}/${slugName}{{ options.path_suffix }}</span>
                   </template>
                   <a-input v-model="options.categories_prefix" />
                 </a-form-item>
                 <a-form-item label="标签前缀：">
                   <template slot="help">
-                    <span>{{ options.blog_url }}/{{ options.tags_prefix }}/${slugName}</span>
+                    <span>{{ options.blog_url }}/{{ options.tags_prefix }}/${slugName}{{ options.path_suffix }}</span>
                   </template>
                   <a-input v-model="options.tags_prefix" />
+                </a-form-item>
+                <a-form-item label="路径后缀：">
+                  <template slot="help">
+                    <span>仅对部分路径有效</span>
+                  </template>
+                  <a-input v-model="options.path_suffix" />
                 </a-form-item>
                 <a-form-item>
                   <a-button
