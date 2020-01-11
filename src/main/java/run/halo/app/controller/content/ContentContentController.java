e2f19d2765ab4cabccf2e012d5b1d4d6bee209a5
ruibaby <i@ryanc.cc>
2020-01-11 20:30:57 +0800

    feat: support set categories and tags mapping prefix.

--- src/main/java/run/halo/app/controller/content/ContentContentController.java
+++ src/main/java/run/halo/app/controller/content/ContentContentController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import run.halo.app.controller.content.model.CategoryModel;
 import run.halo.app.controller.content.model.PostModel;
 import run.halo.app.controller.content.model.SheetModel;
+import run.halo.app.controller.content.model.TagModel;
 import run.halo.app.exception.NotFoundException;
 import run.halo.app.model.entity.Post;
 import run.halo.app.model.entity.Sheet;
@@ -31,6 +33,10 @@ public class ContentContentController {
 
     private final SheetModel sheetModel;
 
+    private final CategoryModel categoryModel;
+
+    private final TagModel tagModel;
+
     private final OptionService optionService;
 
     private final PostService postService;
@@ -39,14 +45,48 @@ public class ContentContentController {
 
     public ContentContentController(PostModel postModel,
                                     SheetModel sheetModel,
+                                    CategoryModel categoryModel,
+                                    TagModel tagModel,
                                     OptionService optionService,
                                     PostService postService,
                                     SheetService sheetService) {
         this.postModel = postModel;
         this.sheetModel = sheetModel;
+        this.categoryModel = categoryModel;
+        this.tagModel = tagModel;
         this.optionService = optionService;
         this.postService = postService;
         this.sheetService = sheetService;
+    }
+
+    @GetMapping("{prefix}")
+    public String content(@PathVariable("prefix") String prefix,
+                          Model model) {
+        String archivesPrefix = optionService.getByPropertyOrDefault(PermalinkProperties.ARCHIVES_PREFIX, String.class, PermalinkProperties.ARCHIVES_PREFIX.defaultValue());
+        String categoriesPrefix = optionService.getByPropertyOrDefault(PermalinkProperties.CATEGORIES_PREFIX, String.class, PermalinkProperties.CATEGORIES_PREFIX.defaultValue());
+        String tagsPrefix = optionService.getByPropertyOrDefault(PermalinkProperties.TAGS_PREFIX, String.class, PermalinkProperties.TAGS_PREFIX.defaultValue());
+
+        if (archivesPrefix.equals(prefix)) {
+            return postModel.list(1, model, "is_archives", "archives");
+        } else if (categoriesPrefix.equals(prefix)) {
+            return categoryModel.list(model);
+        } else if (tagsPrefix.equals(prefix)) {
+            return tagModel.list(model);
+        } else {
+            throw new NotFoundException("Not Found");
+        }
+    }
+
+    @GetMapping("{prefix}/page/{page}")
+    public String content(@PathVariable("prefix") String prefix,
+                          @PathVariable(value = "page") Integer page,
+                          Model model) {
+        String archivesPrefix = optionService.getByPropertyOrDefault(PermalinkProperties.ARCHIVES_PREFIX, String.class, PermalinkProperties.ARCHIVES_PREFIX.defaultValue());
+        if (archivesPrefix.equals(prefix)) {
+            return postModel.list(page, model, "is_archives", "archives");
+        } else {
+            throw new NotFoundException("Not Found");
+        }
     }
 
     @GetMapping("{prefix}/{url}")
@@ -57,6 +97,8 @@ public class ContentContentController {
         PostPermalinkType postPermalinkType = optionService.getPostPermalinkType();
         String archivesPrefix = optionService.getByPropertyOrDefault(PermalinkProperties.ARCHIVES_PREFIX, String.class, PermalinkProperties.ARCHIVES_PREFIX.defaultValue());
         String sheetPrefix = optionService.getByPropertyOrDefault(PermalinkProperties.SHEET_PREFIX, String.class, PermalinkProperties.SHEET_PREFIX.defaultValue());
+        String categoriesPrefix = optionService.getByPropertyOrDefault(PermalinkProperties.CATEGORIES_PREFIX, String.class, PermalinkProperties.CATEGORIES_PREFIX.defaultValue());
+        String tagsPrefix = optionService.getByPropertyOrDefault(PermalinkProperties.TAGS_PREFIX, String.class, PermalinkProperties.TAGS_PREFIX.defaultValue());
 
         if (postPermalinkType.equals(PostPermalinkType.DEFAULT) && archivesPrefix.equals(prefix)) {
             Post post = postService.getByUrl(url);
@@ -64,6 +106,27 @@ public class ContentContentController {
         } else if (sheetPrefix.equals(prefix)) {
             Sheet sheet = sheetService.getByUrl(url);
             return sheetModel.content(sheet, token, model);
+        } else if (categoriesPrefix.equals(prefix)) {
+            return categoryModel.listPost(model, url, 1);
+        } else if (tagsPrefix.equals(prefix)) {
+            return tagModel.listPost(model, url, 1);
+        } else {
+            throw new NotFoundException("Not Found");
+        }
+    }
+
+    @GetMapping("{prefix}/{url}/page/{page}")
+    public String content(@PathVariable("prefix") String prefix,
+                          @PathVariable("url") String url,
+                          @PathVariable("page") Integer page,
+                          Model model) {
+        String categoriesPrefix = optionService.getByPropertyOrDefault(PermalinkProperties.CATEGORIES_PREFIX, String.class, PermalinkProperties.CATEGORIES_PREFIX.defaultValue());
+        String tagsPrefix = optionService.getByPropertyOrDefault(PermalinkProperties.TAGS_PREFIX, String.class, PermalinkProperties.TAGS_PREFIX.defaultValue());
+
+        if (categoriesPrefix.equals(prefix)) {
+            return categoryModel.listPost(model, url, page);
+        } else if (tagsPrefix.equals(prefix)) {
+            return tagModel.listPost(model, url, page);
         } else {
             throw new NotFoundException("Not Found");
         }
