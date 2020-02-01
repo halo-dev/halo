eee5ca587e24bffe38bb01e10ce0ced178038f44
ruibaby <i@ryanc.cc>
2020-02-01 15:17:15 +0800

    fix: wordpress xml converter.

--- src/main/java/run/halo/app/handler/migrate/utils/PropertyMappingTo.java
+++ src/main/java/run/halo/app/handler/migrate/utils/PropertyMappingTo.java
@@ -4,6 +4,7 @@ import java.lang.annotation.*;
 
 /**
  * 该注解用于定义两个对象之间的属性映射关系
+ *
  * @author guqing
  * @date 2020-1-19 13:51
  */
@@ -13,6 +14,7 @@ import java.lang.annotation.*;
 public @interface PropertyMappingTo {
     /**
      * value对应的是目标对象的属性名称
+     *
      * @return 返回源对象属性对应的目标对象的属性名
      */
     String value() default "";
