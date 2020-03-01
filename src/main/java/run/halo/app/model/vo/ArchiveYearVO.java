7a50e1c54a1f9532abd43ab53c7e30df367441e6
ruibaby <i@ryanc.cc>
2020-03-01 13:44:54 +0800

    fix: test case.

--- src/main/java/run/halo/app/model/vo/ArchiveYearVO.java
+++ src/main/java/run/halo/app/model/vo/ArchiveYearVO.java
@@ -3,7 +3,6 @@ package run.halo.app.model.vo;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
-import run.halo.app.model.dto.post.BasePostMinimalDTO;
 
 import java.util.Comparator;
 import java.util.List;
