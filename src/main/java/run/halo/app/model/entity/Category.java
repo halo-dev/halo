9adac3e2f415be5a621adf74767d95c8ba5a2374
wtune <soundofwtune@outlook.com>
2020-03-14 12:12:49 +0800

    Add Index on categories.name

--- src/main/java/run/halo/app/model/entity/Category.java
+++ src/main/java/run/halo/app/model/entity/Category.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
  */
 @Data
 @Entity
-@Table(name = "categories")
+@Table(name = "categories", indexes = {@Index(name = "categories_name", columnList = "name")})
 @ToString
 @EqualsAndHashCode(callSuper = true)
 public class Category extends BaseEntity {
