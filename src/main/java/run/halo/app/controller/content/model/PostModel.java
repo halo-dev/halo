35521a01b32609c11f8ca76da763c9b3d437c265
Tert-butyllithium <lihaonan1105@gmail.com>
2020-05-04 00:31:44 +0800

    Prevent conflict with pull #818

--- src/main/java/run/halo/app/controller/content/model/PostModel.java
+++ src/main/java/run/halo/app/controller/content/model/PostModel.java
@@ -93,7 +93,7 @@ public class PostModel {
                 post.setFormatContent(post.getOriginalContent());
             }
             // visit decrease 1
-            postService.increaseVisit(-1L, post.getId());
+            // postService.increaseVisit(-1L, post.getId());
         }
 
         postService.publishVisitEvent(post.getId());
