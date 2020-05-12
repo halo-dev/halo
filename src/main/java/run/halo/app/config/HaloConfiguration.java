84f9739cd5c65dd3280755726fc6877801bad5b8
Ryan Wang <i@ryanc.cc>
2020-05-12 11:49:40 +0800

    fix: rollback #714 pull request. (#838)

--- src/main/java/run/halo/app/config/HaloConfiguration.java
+++ src/main/java/run/halo/app/config/HaloConfiguration.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.info.BuildProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,10 +16,8 @@ import run.halo.app.cache.InMemoryCacheStore;
 import run.halo.app.cache.LevelCacheStore;
 import run.halo.app.cache.RedisCacheStore;
 import run.halo.app.config.properties.HaloProperties;
-import run.halo.app.model.support.HaloConst;
 import run.halo.app.utils.HttpClientUtils;
 
-import javax.annotation.PostConstruct;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
@@ -37,9 +34,6 @@ public class HaloConfiguration {
 
     @Autowired
     HaloProperties haloProperties;
-
-    @Autowired
-    BuildProperties buildProperties;
 
     @Bean
     public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
@@ -77,10 +71,5 @@ public class HaloConfiguration {
         log.info("halo cache store load impl : [{}]", stringCacheStore.getClass());
         return stringCacheStore;
 
-    }
-
-    @PostConstruct
-    public void init() {
-        HaloConst.HALO_VERSION = buildProperties.getVersion();
     }
 }
