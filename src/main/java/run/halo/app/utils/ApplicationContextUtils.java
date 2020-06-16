c62c56db52b2b1244c5447e54a4d96681be1dbb0
haiku <401952452@qq.com>
2020-06-16 23:07:10 +0800

    fix backup

--- src/main/java/run/halo/app/utils/ApplicationContextUtils.java
+++ src/main/java/run/halo/app/utils/ApplicationContextUtils.java
@@ -6,6 +6,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
+/***
+ * ApplicationContextUtils
+ *
+ * @author SZFHH
+ * @date 2020/06/16
+ */
 @Component
 public class ApplicationContextUtils implements ApplicationContextAware {
     private static ApplicationContext APPLICATION_CONTEXT;
