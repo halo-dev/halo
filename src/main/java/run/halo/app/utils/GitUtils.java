2b99bd599fd20161fd5a5c9a021291ea5ea7fe2b
BigBang019 <46477713+BigBang019@users.noreply.github.com>
2020-07-02 23:34:47 +0800

    Fix issue #515 & #592 (#835)
    * add release access and branch access(#515)
    
    * add release and branch access(#515)
    
    * always update to latest release(#515)
    
    * #515
    
    * #515
    
    * add testcases(#515)
    
    * fix 515
    
    * fix 515
    
    * deal with connection refused
    
    * disable disk operation related test
    
    * fix #592
    
    * fix #515 & #592
    
    * mockito test
    
    * ignored network related test
    
    * resolve conflict

--- src/main/java/run/halo/app/utils/GitUtils.java
+++ src/main/java/run/halo/app/utils/GitUtils.java
@@ -3,13 +3,17 @@ package run.halo.app.utils;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.TransportException;
 import org.eclipse.jgit.errors.RepositoryNotFoundException;
+import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.storage.file.WindowCacheConfig;
 import org.springframework.lang.NonNull;
 import org.springframework.util.Assert;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.*;
 
 /**
  * Git utilities.
@@ -57,6 +61,43 @@ public class GitUtils {
         }
 
         return git;
+    }
+
+    public static void cloneFromGit(@NonNull String repoUrl, @NonNull Path targetPath, @NonNull String branchName) throws GitAPIException {
+        Assert.hasText(repoUrl, "Repository remote url must not be blank");
+        Assert.notNull(targetPath, "Target path must not be null");
+
+        Git git = null;
+        try {
+            git = Git.cloneRepository()
+                    .setURI(repoUrl)
+                    .setDirectory(targetPath.toFile())
+                    .setBranchesToClone(Arrays.asList("refs/heads/" + branchName))
+                    .setBranch("refs/heads/" + branchName)
+                    .call();
+        } finally {
+            closeQuietly(git);
+        }
+    }
+
+    public static List<String> getAllBranches(@NonNull String repoUrl) {
+        List<String> branches = new ArrayList<String>();
+        try {
+            Collection<Ref> refs = Git.lsRemoteRepository()
+                    .setHeads(true)
+                    .setRemote(repoUrl)
+                    .call();
+            for (Ref ref : refs) {
+                branches.add(ref.getName().substring(ref.getName().lastIndexOf("/") + 1, ref.getName().length()));
+            }
+        } catch (InvalidRemoteException e) {
+            log.warn("Git url is not valid", e);
+        } catch (TransportException e) {
+            log.warn("Transport exception", e);
+        } catch (GitAPIException e) {
+            log.warn("Git api exception", e);
+        }
+        return branches;
     }
 
     public static void closeQuietly(Git git) {
