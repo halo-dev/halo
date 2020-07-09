4a380d80ef7c352788e93231dd3ec81ae2cb5113
Sheiy <35429674@qq.com>
2020-07-09 15:19:50 +0800

    如果系统设置了http代理则创建httpClient时设置代理

--- src/main/java/run/halo/app/utils/HttpClientUtils.java
+++ src/main/java/run/halo/app/utils/HttpClientUtils.java
@@ -2,8 +2,12 @@ package run.halo.app.utils;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
@@ -12,6 +16,9 @@ import org.springframework.core.io.ByteArrayResource;
 import org.springframework.lang.NonNull;
 
 import javax.net.ssl.SSLContext;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
@@ -44,20 +51,61 @@ public class HttpClientUtils {
     @NonNull
     public static CloseableHttpClient createHttpsClient(int timeout) throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
         SSLContext sslContext = new SSLContextBuilder()
-            .loadTrustMaterial(null, (certificate, authType) -> true)
-            .build();
+                .loadTrustMaterial(null, (certificate, authType) -> true)
+                .build();
 
-        final HttpClientBuilder httpClientBuilder = HttpClients.custom();
+        return resolveProxySetting(HttpClients.custom())
+                .setSSLContext(sslContext)
+                .setSSLHostnameVerifier(new NoopHostnameVerifier())
+                .setDefaultRequestConfig(getRequestConfig(timeout))
+                .build();
+    }
+
+    private static HttpClientBuilder resolveProxySetting(final HttpClientBuilder httpClientBuilder) {
         final String httpProxy = System.getenv("http_proxy");
         if (StringUtils.isNotBlank(httpProxy)) {
-            httpClientBuilder.setProxy(HttpHost.create(httpProxy));
-        }
+            final HttpHost httpHost;
+            if (httpProxy.contains("@")) {
+                //with username and password
+                final String[] usernamePasswordHost = httpProxy.split("@");
+                if (usernamePasswordHost.length != 2) {
+                    throw new IllegalArgumentException("illegal http_proxy format the supported format is username:password@scheme://proxy_hostname:proxy_port or scheme://proxy_hostname:proxy_port");
+                }
+                final String[] usernamePassword = usernamePasswordHost[0].split(":");
+                if (usernamePassword.length != 2) {
+                    throw new IllegalArgumentException("illegal http_proxy format the supported format is username:password@scheme://proxy_hostname:proxy_port or scheme://proxy_hostname:proxy_port");
+                }
+                final String host = usernamePasswordHost[1];
+                httpHost = HttpHost.create(host);
+                final String username;
+                final String password;
+                try {
+                    username = URLDecoder.decode(usernamePassword[0], StandardCharsets.UTF_8.displayName());
+                    password = URLDecoder.decode(usernamePassword[1], StandardCharsets.UTF_8.displayName());
+                } catch (UnsupportedEncodingException e) {
+                    //this will not be happen
+                    throw new Error("panic");
+                }
+                int port = httpHost.getPort();
+                if (port == -1) {
+                    if (host.startsWith("http://")) {
+                        port = 80;
+                    }
+                    if (host.startsWith("https://")) {
+                        port = 443;
+                    }
+                }
 
-        return httpClientBuilder
-            .setSSLContext(sslContext)
-            .setSSLHostnameVerifier(new NoopHostnameVerifier())
-            .setDefaultRequestConfig(getRequestConfig(timeout))
-            .build();
+                final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+                credentialsProvider.setCredentials(new AuthScope(httpHost.getHostName(), port),
+                        new UsernamePasswordCredentials(username, password));
+                httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+            } else {
+                httpHost = HttpHost.create(httpProxy);
+            }
+            httpClientBuilder.setProxy(httpHost);
+        }
+        return httpClientBuilder;
     }
 
     /**
@@ -68,10 +116,10 @@ public class HttpClientUtils {
      */
     private static RequestConfig getRequestConfig(int timeout) {
         return RequestConfig.custom()
-            .setConnectTimeout(timeout)
-            .setConnectionRequestTimeout(timeout)
-            .setSocketTimeout(timeout)
-            .build();
+                .setConnectTimeout(timeout)
+                .setConnectionRequestTimeout(timeout)
+                .setSocketTimeout(timeout)
+                .build();
     }
 
 
