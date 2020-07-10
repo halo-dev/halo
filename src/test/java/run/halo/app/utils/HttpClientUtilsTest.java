1a0a3cf8cac7be3fe226a3666fe32781ba9f43f0
sheiy <xlz35429674@foxmail.com>
2020-07-10 12:04:43 +0800

    pref: 如果系统设置了http代理则创建httpClient时设置代理 (#956)
    * 修改缓存时长
    
    * 如果系统设置了http代理则创建httpClient时设置代理
    
    * 如果系统设置了http代理则创建httpClient时设置代理
    
    * 如果系统设置了http代理则创建httpClient时设置代理
    
    * 如果系统设置了http代理则创建httpClient时设置代理
    
    * 如果系统设置了http代理则创建httpClient时设置代理
    添加注释
    
    * 添加单元测试
    没有useJUnitPlatform()单元测试跑不起来
    
    * 修改单元测试，删除zxing的依赖
    
    * 修正错误的http配置格式
    
    Co-authored-by: Sheiy <35429674@qq.com>

---
+++ src/test/java/run/halo/app/utils/HttpClientUtilsTest.java
@@ -0,0 +1,34 @@
+package run.halo.app.utils;
+
+import cn.hutool.core.lang.Tuple;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HttpClientUtilsTest {
+
+    @Test
+    void resolveHttpProxyTest() throws Exception {
+        final Method resolveHttpProxy = HttpClientUtils.class.getDeclaredMethod("resolveHttpProxy", String.class);
+        resolveHttpProxy.setAccessible(true);
+
+        Tuple result = (Tuple) resolveHttpProxy.invoke(null, "http://127.0.0.1");
+        assertEquals(result.get(0), "http://127.0.0.1:80");
+
+        result = (Tuple) resolveHttpProxy.invoke(null, "https://127.0.0.1");
+        assertEquals(result.get(0), "https://127.0.0.1:443");
+
+        result = (Tuple) resolveHttpProxy.invoke(null, "https://127.0.0.1:123");
+        assertEquals(result.get(0), "https://127.0.0.1:123");
+
+        result = (Tuple) resolveHttpProxy.invoke(null, "https://u:p@127.0.0.1:123");
+        assertArrayEquals(result.getMembers(), new Object[]{"https://127.0.0.1:123", "u", "p"});
+
+        result = (Tuple) resolveHttpProxy.invoke(null, "https://u@127.0.0.1");
+        assertArrayEquals(result.getMembers(), new Object[]{"https://127.0.0.1:443", "u", null});
+    }
+
+}
