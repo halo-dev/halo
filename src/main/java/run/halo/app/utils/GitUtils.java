50b19f8d3af259b0a24f5e6e761fdadac39e2231
John Niang <johnniang@riseup.net>
2020-07-14 00:57:24 +0800

    pref: make some unnecessary error detail hide (#962)

--- src/main/java/run/halo/app/utils/GitUtils.java
+++ src/main/java/run/halo/app/utils/GitUtils.java
@@ -56,7 +56,7 @@ public class GitUtils {
         try {
             git = Git.open(repoPath.toFile());
         } catch (RepositoryNotFoundException e) {
-            log.warn("Git repository may not exist, we will try to initialize an empty repository", e);
+            log.warn("Git repository may not exist, we will try to initialize an empty repository: [{}]", e.getMessage());
             git = Git.init().setDirectory(repoPath.toFile()).call();
         }
 
@@ -72,7 +72,7 @@ public class GitUtils {
             git = Git.cloneRepository()
                     .setURI(repoUrl)
                     .setDirectory(targetPath.toFile())
-                    .setBranchesToClone(Arrays.asList("refs/heads/" + branchName))
+                    .setBranchesToClone(Collections.singletonList("refs/heads/" + branchName))
                     .setBranch("refs/heads/" + branchName)
                     .call();
         } finally {
@@ -81,21 +81,21 @@ public class GitUtils {
     }
 
     public static List<String> getAllBranches(@NonNull String repoUrl) {
-        List<String> branches = new ArrayList<String>();
+        List<String> branches = new ArrayList<>();
         try {
             Collection<Ref> refs = Git.lsRemoteRepository()
                     .setHeads(true)
                     .setRemote(repoUrl)
                     .call();
             for (Ref ref : refs) {
-                branches.add(ref.getName().substring(ref.getName().lastIndexOf("/") + 1, ref.getName().length()));
+                branches.add(ref.getName().substring(ref.getName().lastIndexOf("/") + 1));
             }
         } catch (InvalidRemoteException e) {
-            log.warn("Git url is not valid", e);
+            log.warn("Git url is not valid: [{}]", e.getMessage());
         } catch (TransportException e) {
-            log.warn("Transport exception", e);
+            log.warn("Transport exception: [{}]", e.getMessage());
         } catch (GitAPIException e) {
-            log.warn("Git api exception", e);
+            log.warn("Git api exception: [{}]", e.getMessage());
         }
         return branches;
     }
