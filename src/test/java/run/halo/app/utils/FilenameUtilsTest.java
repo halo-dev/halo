4cb988c1de0c4acb20032c833cbf64c5364fb56e
王存露 <1137738840@qq.com>
2020-09-14 13:14:59 +0800

    fix: #897 && fix: halo-dev/halo-admin#236 (#1057)
    * fix: #897
    
    * fix: 网站备份时，上级目录不存在的问题 (#1056)
    
    * 增加网站备份，临时文件不存在时创建
    
    * 增加数据导出，临时文件不存在时创建
    
    * 修改整站备份,数据导出，临时文件不存在时创建
    
    * add: file upload unit test
    
    * change : FileHandlersTest.java file format
    
    * feat: #1065 (#1066)
    
    * feat: #1065
    
    * feat: #1065
    
    * fix: #897
    
    * add: file upload unit test
    
    * change : FileHandlersTest.java file format
    
    * fix: https://github.com/halo-dev/halo-admin/issues/236
    
    * delete : FileHandlersTest.java
    change : FilenameUtilsTest.java add file name double suffix test
    
    Co-authored-by: bruin-sxk <45200590+bruin-sxk@users.noreply.github.com>
    Co-authored-by: Ryan Wang <i@ryanc.cc>

--- src/test/java/run/halo/app/utils/FilenameUtilsTest.java
+++ src/test/java/run/halo/app/utils/FilenameUtilsTest.java
@@ -16,18 +16,24 @@ class FilenameUtilsTest {
     // a.txt     --> a.txt
     // a/b/c     --> c
     // a/b/c/    --> ""
+    // he/ll/o.tar.gz --> "o"
+    // h/i.tar.bz2 --> "i"
     @Test
     void getBasename() {
         assertEquals("c", FilenameUtils.getBasename("a/b/c.txt"));
         assertEquals("a", FilenameUtils.getBasename("a.txt"));
         assertEquals("c", FilenameUtils.getBasename("a/b/c"));
         assertEquals("", FilenameUtils.getBasename("a/b/c/"));
+        assertEquals("o", FilenameUtils.getBasename("he/ll/o.tar.gz"));
+        assertEquals("i", FilenameUtils.getBasename("h/i.tar.bz2"));
     }
 
     // foo.txt      --> "txt"
     // a/b/c.jpg    --> "jpg"
     // a/b.txt/c    --> ""
     // a/b/c        --> ""
+    // he/ll/o.tar.gz --> "tar.gz"
+    // he/ll/o.tar.bz2 --> "tar.bz2"
     @Test
     void getExtension() {
         assertEquals("txt", FilenameUtils.getExtension("foo.txt"));
@@ -35,5 +41,7 @@ class FilenameUtilsTest {
         assertEquals("", FilenameUtils.getExtension("a/b.txt/c"));
         assertEquals("", FilenameUtils.getExtension("a/b/c"));
         assertEquals("", FilenameUtils.getExtension("a/b/c/"));
+        assertEquals("tar.gz", FilenameUtils.getExtension("he/ll/o.tar.gz"));
+        assertEquals("tar.bz2", FilenameUtils.getExtension("he/ll/o.tar.bz2"));
     }
 }
