b17b9acdbcc48cd5fdb8bbd2445063d15fa8af06
xcp <saber3555@outlook.com>
2020-11-09 15:12:22 +0800

    Add AttachmentHandlerCovertService

--- src/main/java/run/halo/app/service/impl/AttachmentHandlerCovertImpl.java
+++ src/main/java/run/halo/app/service/impl/AttachmentHandlerCovertImpl.java
@@ -12,26 +12,26 @@ import run.halo.app.service.AttachmentHandlerCovertService;
 import run.halo.app.service.AttachmentService;
 import run.halo.app.service.PostService;
 import run.halo.app.utils.AttachmentUtils;
+import run.halo.app.utils.FilenameUtils;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URLDecoder;
 import java.util.List;
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Slf4j
 @Service
 public class AttachmentHandlerCovertImpl implements AttachmentHandlerCovertService {
+
     private final AttachmentService attachmentService;
 
     private final PostService postService;
 
     private final String workDir;
 
-    private static final String ATTACHMENTTMPDIR = "attachment_tmp/";
+    private static final String ATTACHMENT_TMP_DIR = "attachment_tmp/";
 
-    private static final Pattern r = Pattern.compile("(?<=!\\[.*]\\()(.+)(?=\\))");
 
     public AttachmentHandlerCovertImpl(PostService postService, AttachmentService attachmentService, HaloProperties haloProperties) {
         this.postService = postService;
@@ -40,57 +40,39 @@ public class AttachmentHandlerCovertImpl implements AttachmentHandlerCovertServi
     }
 
     public void covertByAttachment() {
-        Matcher m;
         List<Attachment> oldAttachments = attachmentService.listAll();
         List<Post> posts = postService.listAll();
-        StringBuilder strBuilder = new StringBuilder();
 
         for (Attachment oldAttachment : oldAttachments) {
             String oldAttachmentPath = oldAttachment.getPath();
-            log.info("Old Attachment Path: {}", oldAttachmentPath);
             File tmpAttachment = new File(getTmpAttachmentPath(oldAttachmentPath, oldAttachment.getName()));
             if (tmpAttachment.exists()) {
                 try {
                     String newAttachmentPath = uploadFile(tmpAttachment);
-                    log.info("New Attachment Path: {}", newAttachmentPath);
                     for (Post post : posts) {
-                        strBuilder.append(post.getOriginalContent());
-                        m = r.matcher(post.getOriginalContent());
-                        while (m.find()) {
-                            if (m.group().contains(oldAttachmentPath)) {
-                                int index = strBuilder.indexOf(m.group());
-                                while (index != -1) {
-                                    strBuilder.replace(index, index + m.group().length(), newAttachmentPath);
-                                    index += newAttachmentPath.length();
-                                    index = strBuilder.indexOf(m.group(), index);
-                                }
-                            }
-                        }
-                        post.setOriginalContent(strBuilder.toString());
-                        strBuilder.delete(0, strBuilder.length());
-                        postService.update(post);
+                        postService.update(AttachmentUtils.replacePostContent(post, oldAttachmentPath, newAttachmentPath));
                     }
                     attachmentService.remove(oldAttachment);
                 } catch (IOException e) {
                     e.printStackTrace();
                 } finally {
                     if (!tmpAttachment.delete()) {
-                        log.info("Failed to delete temporary files: {} ", tmpAttachment.getPath());
+                        log.warn("Failed to delete temporary files: {} ", tmpAttachment.getPath());
                     }
                 }
+            } else {
+                log.warn("Can not download or copy file: {}", oldAttachmentPath);
             }
         }
     }
 
     public void covertByPost() {
         List<Post> posts = postService.listAll();
-
         for (Post post : posts) {
             String oc = post.getOriginalContent();
-            Matcher m = r.matcher(oc);
+            Matcher m = AttachmentUtils.getPattern().matcher(oc);
             while (m.find()) {
-                String fileName = m.group().split("/")[m.group().split("/").length - 1];
-                File tmpAttachment = new File(getTmpAttachmentPath(m.group(), fileName));
+                File tmpAttachment = new File(getTmpAttachmentPath(m.group(), AttachmentUtils.getBaseNameFromUrl(m.group())));
                 if (tmpAttachment.exists()) {
                     try {
                         String newAttachmentPath = uploadFile(tmpAttachment);
@@ -99,10 +81,11 @@ public class AttachmentHandlerCovertImpl implements AttachmentHandlerCovertServi
                         e.printStackTrace();
                     } finally {
                         if (!tmpAttachment.delete()) {
-                            log.info("Failed to delete temporary files: {} ", tmpAttachment.getPath());
+                            log.warn("Failed to delete temporary files: {} ", tmpAttachment.getPath());
                         }
                     }
-
+                } else {
+                    log.warn("Can not download file: {}", m.group());
                 }
             }
             postService.update(post);
@@ -110,11 +93,13 @@ public class AttachmentHandlerCovertImpl implements AttachmentHandlerCovertServi
     }
 
     private String getTmpAttachmentPath(String urlStr, String fileBaseName) {
-        String tmpAttachmentPath = workDir + ATTACHMENTTMPDIR + fileBaseName;
-        if (tmpAttachmentPath.split("\\.").length < 2) {
-            tmpAttachmentPath = tmpAttachmentPath + "." + urlStr.split("\\.")[urlStr.split("\\.").length - 1];
-        }
+
         try {
+            if (fileBaseName.split("\\.").length < 2) {
+                fileBaseName = fileBaseName + "." + FilenameUtils.getExtension(urlStr);
+            }
+            String tmpAttachmentPath = workDir + ATTACHMENT_TMP_DIR + fileBaseName;
+
             if (urlStr.startsWith("http")) {
                 AttachmentUtils.downloadFile(urlStr, tmpAttachmentPath);
             } else {
@@ -124,11 +109,14 @@ public class AttachmentHandlerCovertImpl implements AttachmentHandlerCovertServi
                     FileUtils.copyFile(oldFile, new File(tmpAttachmentPath));
                 }
             }
+            return tmpAttachmentPath;
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return tmpAttachmentPath;
+
+        return "";
     }
+
 
     private String uploadFile(File tmpAttachment) throws IOException {
         MultipartFile multipartFile = AttachmentUtils.getMultipartFile(tmpAttachment);
--- src/main/java/run/halo/app/utils/AttachmentUtils.java
+++ src/main/java/run/halo/app/utils/AttachmentUtils.java
@@ -4,16 +4,28 @@ import org.apache.commons.fileupload.disk.DiskFileItem;
 import org.apache.commons.io.FileUtils;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import run.halo.app.model.entity.Post;
 
 import javax.activation.MimetypesFileTypeMap;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLDecoder;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class AttachmentUtils {
 
+    private static final int FILE_NAME_LIMIT = 64;
+
+    private static final Pattern r = Pattern.compile("(?<=!\\[.*]\\()(.+)(?=\\))");
+
     private AttachmentUtils() {
 
+    }
+
+    public static Pattern getPattern() {
+        return r;
     }
 
     /**
@@ -23,7 +35,6 @@ public class AttachmentUtils {
      * @return multipartFile new multipartFile
      * @throws IOException covert failed
      */
-
     public static MultipartFile getMultipartFile(File file) throws IOException {
         MimetypesFileTypeMap mimeTypesMap = new MimetypesFileTypeMap();
         DiskFileItem item = new DiskFileItem(
@@ -74,4 +85,58 @@ public class AttachmentUtils {
         }
         conn.disconnect();
     }
+
+    /**
+     * Extract the file_base_name from Url,
+     * it will be truncated when the length of the file_base_name is greater than FILE_NAME_LIMIT.
+     * <p>
+     * http://test.com/test.png
+     * return test
+     * <p>
+     * http://test.com/test.png
+     * return tes (When FILE_NAME_LIMIT=3, default 64)
+     * <p>
+     * http://test.com/你好你好.png
+     * return 你好你 (When FILE_NAME_LIMIT=3, default 64)
+     * <p>
+     * http://test.com/%E4%BD%A0%E5%A5%BD%E4%BD%A0%E5%A5%BD.png
+     * return 你好你 (When FILE_NAME_LIMIT=3, default 64)
+     *
+     * @param url Extracted url
+     * @return FileBaseName
+     */
+    public static String getBaseNameFromUrl(String url) {
+        String fileBaseName = FilenameUtils.getBasename(url);
+        try {
+            fileBaseName = URLDecoder.decode(fileBaseName, "utf-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        if (fileBaseName.length() > FILE_NAME_LIMIT) {
+            fileBaseName = fileBaseName.substring(0, FILE_NAME_LIMIT);
+        }
+        return fileBaseName;
+    }
+
+
+    public static Post replacePostContent(Post post, String oldAttachmentPath, String newAttachmentPath) {
+        Matcher m = r.matcher(post.getOriginalContent());
+        StringBuilder strBuilder = new StringBuilder();
+        strBuilder.append(post.getOriginalContent());
+        while (m.find()) {
+            if (m.group().contains(oldAttachmentPath)) {
+                int index = strBuilder.indexOf(m.group());
+                while (index != -1) {
+                    strBuilder.replace(index, index + m.group().length(), newAttachmentPath);
+                    index += newAttachmentPath.length();
+                    index = strBuilder.indexOf(m.group(), index);
+                }
+            }
+        }
+        post.setOriginalContent(strBuilder.toString());
+        strBuilder.delete(0, strBuilder.length());
+        return post;
+    }
+
+
 }
