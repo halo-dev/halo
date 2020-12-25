6b8cf2f57e5671544edbcea23f50906cba833462
Ryan Wang <i@ryanc.cc>
2020-12-25 21:40:04 +0800

    feat: #1160. (#1203)

--- src/main/java/run/halo/app/model/params/JournalParam.java
+++ src/main/java/run/halo/app/model/params/JournalParam.java
@@ -6,7 +6,6 @@ import run.halo.app.model.entity.Journal;
 import run.halo.app.model.enums.JournalType;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 
 /**
  * Journal param.
@@ -19,7 +18,6 @@ import javax.validation.constraints.Size;
 public class JournalParam implements InputConverter<Journal> {
 
     @NotBlank(message = "内容不能为空")
-    @Size(max = 511, message = "内容的字符长度不能超过 {max}")
     private String sourceContent;
 
     private JournalType type = JournalType.PUBLIC;
