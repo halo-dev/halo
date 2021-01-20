3bc403879cc51072f6eb0b814407148c360fcd55
johnniang <it@johnniang.me>
2021-01-21 00:34:24 +0800

    Add merge two local repo test

--- src/test/java/run/halo/app/utils/GitTest.java
+++ src/test/java/run/halo/app/utils/GitTest.java
@@ -1,5 +1,15 @@
 package run.halo.app.utils;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.Status;
@@ -9,16 +19,7 @@ import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.transport.RemoteConfig;
 import org.eclipse.jgit.transport.URIish;
 import org.junit.jupiter.api.*;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,9 +27,10 @@ import static org.junit.jupiter.api.Assertions.*;
  * Git test.
  *
  * @author johnniang
- * @date 19-5-21
+ * @date 2020.01.21
  */
 @Slf4j
+@ActiveProfiles("test")
 class GitTest {
 
     Path tempPath;
@@ -128,6 +130,65 @@ class GitTest {
         refMap.forEach((name, ref) -> {
             log.debug("name: [{}], ref: [{}]", name, ref);
         });
+    }
+
+    @Test
+    void mergeTwoLocalRepo() throws GitAPIException, IOException, URISyntaxException, InterruptedException {
+        final var oldRepoPath = tempPath.resolve("old-repo");
+        final var newRepoPath = tempPath.resolve("new-repo");
+
+        // prepare one local repo
+        final var oldGit = Git.init()
+                .setDirectory(oldRepoPath.toFile())
+                .call();
+        final var testTextPath = oldRepoPath.resolve("test.txt");
+        Files.writeString(testTextPath, "hello old git");
+        oldGit.commit()
+                .setSign(false)
+                .setMessage("commit test.txt at old repo")
+                .call();
+
+        printAllLog(oldGit);
+
+        // copy old repo path to new repo path
+        FileUtils.copyFolder(oldRepoPath, newRepoPath);
+
+        final var newGit = Git.init()
+                .setDirectory(newRepoPath.toFile())
+                .call();
+
+        Files.writeString(newRepoPath.resolve("test.txt"), "hello old git\nhello new git");
+        TimeUnit.SECONDS.sleep(1);
+        newGit.commit()
+                .setSign(false)
+                .setMessage("commit test.txt at new repo")
+                .call();
+
+        printAllLog(newGit);
+
+        // add new repo as old repo remote
+        oldGit.remoteAdd().setName("newRepo")
+                .setUri(new URIish(newRepoPath.toString()))
+                .call();
+
+        oldGit.fetch()
+                .setRemote("newRepo")
+                .call();
+        final var rebaseResult = oldGit.rebase()
+                .setUpstream("master")
+                .setUpstreamName("newRepo")
+                .call();
+
+        log.debug("{} | {} | {}", rebaseResult.getCurrentCommit(), rebaseResult.getConflicts(), rebaseResult.getStatus());
+
+        printAllLog(oldGit);
+    }
+
+    void printAllLog(Git git) throws IOException, GitAPIException {
+        var commits = git.log().all().call();
+        for (var commit : commits) {
+            log.debug("{}: {} {}", git.toString(), commit, commit.getFullMessage());
+        }
     }
 
     Git cloneRepository() throws GitAPIException {
