eda523c799b576ef03dfae411d9d9d61113f1999
johnniang <johnniang@foxmail.com>
2021-01-22 09:01:08 +0800

    Add more test entry point in GitTest

--- src/test/java/run/halo/app/utils/GitTest.java
+++ src/test/java/run/halo/app/utils/GitTest.java
@@ -1,5 +1,9 @@
 package run.halo.app.utils;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
@@ -8,7 +12,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jgit.api.Git;
@@ -17,12 +20,15 @@ import org.eclipse.jgit.api.Status;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.errors.RepositoryNotFoundException;
 import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.merge.MergeStrategy;
 import org.eclipse.jgit.transport.RemoteConfig;
 import org.eclipse.jgit.transport.URIish;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ActiveProfiles;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Git test.
@@ -48,7 +54,8 @@ class GitTest {
 
     @Test
     void openFailureTest() {
-        Assertions.assertThrows(RepositoryNotFoundException.class, () -> Git.open(tempPath.toFile()));
+        Assertions
+                .assertThrows(RepositoryNotFoundException.class, () -> Git.open(tempPath.toFile()));
     }
 
     @Test
@@ -67,9 +74,11 @@ class GitTest {
     void remoteAddTest() throws GitAPIException, URISyntaxException {
         Git git = Git.init().setDirectory(tempPath.toFile()).call();
         git.remoteRemove().setRemoteName("theme-provider").call();
-        git.remoteAdd().setName("theme-provider").setUri(new URIish("https://github.com/halo-dev/halo-theme-pinghsu.git")).call();
+        git.remoteAdd().setName("theme-provider")
+                .setUri(new URIish("https://github.com/halo-dev/halo-theme-pinghsu.git")).call();
         List<RemoteConfig> remoteConfigs = git.remoteList().call();
-        remoteConfigs.forEach(remoteConfig -> log.debug("name: [{}], url: [{}]", remoteConfig.getName(), remoteConfig.getURIs()));
+        remoteConfigs.forEach(remoteConfig -> log
+                .debug("name: [{}], url: [{}]", remoteConfig.getName(), remoteConfig.getURIs()));
     }
 
     @Test
@@ -90,35 +99,37 @@ class GitTest {
     @Test
     @Disabled("Due to time-consumption fetching")
     void getAllBranchesFromRemote() {
-        List<String> branches = GitUtils.getAllBranchesFromRemote("https://github.com/halo-dev/halo-theme-hux.git");
+        List<String> branches =
+                GitUtils.getAllBranchesFromRemote("https://github.com/halo-dev/halo-theme-hux.git");
         assertNotNull(branches);
     }
 
     @Test
     @Disabled("Due to time-consumption fetching")
     void getAllBranchesWithInvalidURL() {
-        List<String> branches = GitUtils.getAllBranchesFromRemote("https://github.com/halo-dev/halo-theme.git");
+        List<String> branches =
+                GitUtils.getAllBranchesFromRemote("https://github.com/halo-dev/halo-theme.git");
         assertNotNull(branches);
         assertEquals(0, branches.size());
     }
 
     @Test
-    void getAllBranchesTest() throws GitAPIException, IOException {
-        Git git = Git.init().setDirectory(tempPath.toFile()).call();
+    void getAllBranchesTest() throws GitAPIException {
+        try (Git git = Git.init().setDirectory(tempPath.toFile()).call()) {
+            git.add().addFilepattern(".").call();
+            git.commit().setAllowEmpty(true).setSign(false).setMessage("Empty commit").call();
 
-        git.add().addFilepattern(".").call();
-        git.commit().setAllowEmpty(true).setSign(false).setMessage("Empty commit").call();
-
-        git.branchCreate().setName("main").call();
-        git.branchCreate().setName("dev").call();
-        Set<String> branches = git.branchList()
-                .call()
-                .stream()
-                .map(ref -> {
-                    String refName = ref.getName();
-                    return refName.substring(refName.lastIndexOf('/') + 1);
-                }).collect(Collectors.toSet());
-        assertTrue(branches.containsAll(Arrays.asList("main", "dev")));
+            git.branchCreate().setName("main").call();
+            git.branchCreate().setName("dev").call();
+            Set<String> branches = git.branchList()
+                    .call()
+                    .stream()
+                    .map(ref -> {
+                        String refName = ref.getName();
+                        return refName.substring(refName.lastIndexOf('/') + 1);
+                    }).collect(Collectors.toSet());
+            assertTrue(branches.containsAll(Arrays.asList("main", "dev")));
+        }
     }
 
     @Test
@@ -134,55 +145,81 @@ class GitTest {
     }
 
     @Test
-    void mergeTwoLocalRepo() throws GitAPIException, IOException, URISyntaxException, InterruptedException {
+    void mergeTwoLocalRepo() throws GitAPIException, IOException, URISyntaxException {
         final var oldRepoPath = tempPath.resolve("old-repo");
         final var newRepoPath = tempPath.resolve("new-repo");
 
         // prepare one local repo
-        final var oldGit = Git.init()
+        try (final var oldGit = Git.init()
                 .setDirectory(oldRepoPath.toFile())
-                .call();
-        final var testTextPath = oldRepoPath.resolve("test.txt");
-        Files.writeString(testTextPath, "hello old git");
-        oldGit.commit()
-                .setSign(false)
-                .setMessage("commit test.txt at old repo")
-                .call();
+                .call()) {
 
-        printAllLog(oldGit);
+            final var testTextInOldRepoPath = oldRepoPath.resolve("test.txt");
+            Files.writeString(testTextInOldRepoPath, "hello old git");
+            oldGit.add().addFilepattern(".").call();
+            oldGit.commit()
+                    .setSign(false)
+                    .setMessage("commit test.txt at old repo")
+                    .call();
 
-        // copy old repo path to new repo path
-        FileUtils.copyFolder(oldRepoPath, newRepoPath);
+            printAllLog(oldGit);
 
-        final var newGit = Git.init()
-                .setDirectory(newRepoPath.toFile())
-                .call();
+            // copy old repo path to new repo path
+            FileUtils.copyFolder(oldRepoPath, newRepoPath);
 
-        Files.writeString(newRepoPath.resolve("test.txt"), "hello old git\nhello new git");
-        TimeUnit.SECONDS.sleep(1);
-        newGit.commit()
-                .setSign(false)
-                .setMessage("commit test.txt at new repo")
-                .call();
+            try (final var newGit = Git.init()
+                    .setDirectory(newRepoPath.toFile())
+                    .call()) {
 
-        printAllLog(newGit);
+                Files.writeString(newRepoPath.resolve("test.txt"), "hello old git\nhello new git");
+                newGit.add().addFilepattern(".").call();
+                newGit.commit()
+                        .setSign(false)
+                        .setMessage("commit test.txt at new repo")
+                        .call();
 
-        // add new repo as old repo remote
-        oldGit.remoteAdd().setName("newRepo")
-                .setUri(new URIish(newRepoPath.toString()))
-                .call();
+                final var refs = newGit.branchList()
+                        .setListMode(ListBranchCommand.ListMode.ALL)
+                        .call();
+                refs.forEach(ref -> {
+                    log.debug("Ref in new repo: {}", ref);
+                });
 
-        oldGit.fetch()
-                .setRemote("newRepo")
-                .call();
-        final var rebaseResult = oldGit.rebase()
-                .setUpstream("master")
-                .setUpstreamName("newRepo")
-                .call();
+                printAllLog(newGit);
+            }
 
-        log.debug("{} | {} | {}", rebaseResult.getCurrentCommit(), rebaseResult.getConflicts(), rebaseResult.getStatus());
+            // add new repo as old repo remote
+            oldGit.remoteAdd().setName("newRepo")
+                    .setUri(new URIish(newRepoPath.toString()))
+                    .call();
 
-        printAllLog(oldGit);
+            oldGit.fetch()
+                    .setRemote("newRepo")
+                    .call();
+
+            final var refs = oldGit.branchList()
+                    .setListMode(ListBranchCommand.ListMode.ALL)
+                    .call();
+
+            refs.forEach(ref -> log.debug("Ref in old repo: {}", ref));
+
+            final var testTextInOldRepo = Files.readString(testTextInOldRepoPath);
+            Assertions.assertEquals("hello old git", testTextInOldRepo);
+
+            final var rebaseResult = oldGit.rebase()
+                    .setUpstream("newRepo/master")
+                    .setStrategy(MergeStrategy.THEIRS)
+                    .call();
+
+            log.debug("{} | {} | {}", rebaseResult.getCurrentCommit(), rebaseResult.getConflicts(),
+                    rebaseResult.getStatus());
+            Assertions.assertTrue(rebaseResult.getStatus().isSuccessful());
+
+            final var testTextAfterRebase = Files.readString(testTextInOldRepoPath);
+            Assertions.assertEquals("hello old git\nhello new git", testTextAfterRebase);
+
+            printAllLog(oldGit);
+        }
     }
 
     @Test
