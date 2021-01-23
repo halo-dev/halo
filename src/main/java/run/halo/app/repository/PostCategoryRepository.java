c9d9cf091f987a1b67d88bea21dd81e5f134bdbf
zhixiang.yuan <goog1e491488323@gmail.com>
2021-01-24 00:24:21 +0800

    fix: fix jpa error

--- src/main/java/run/halo/app/repository/PostCategoryRepository.java
+++ src/main/java/run/halo/app/repository/PostCategoryRepository.java
@@ -61,7 +61,7 @@ public interface PostCategoryRepository extends BaseRepository<PostCategory, Int
      * @return a set of post id
      */
     @NonNull
-    @Query("select postCategory.postId from PostCategory postCategory, Post post where postCategory.categoryId = :categoryId and post.id = postCategory.postId and post.status in (:status)")
+    @Query("select postCategory.postId from PostCategory postCategory, Post post where postCategory.categoryId = ?1 and post.id = postCategory.postId and post.status in (?2)")
     Set<Integer> findAllPostIdsByCategoryId(@NonNull Integer categoryId, @NonNull Set<PostStatus> status);
 
     /**
@@ -119,7 +119,7 @@ public interface PostCategoryRepository extends BaseRepository<PostCategory, Int
      * @param categoryIdList category id list must not be empty
      * @return a list of post category
      */
-    @Query("select pc from PostCategory pc where pc.categoryId in (:categoryIdList)")
+    @Query("select pc from PostCategory pc where pc.categoryId in (?1)")
     @NonNull
-    List<PostCategory> findAllByCategoryIdList(@Param("categoryIdList") List<Integer> categoryIdList);
+    List<PostCategory> findAllByCategoryIdList( List<Integer> categoryIdList);
 }
