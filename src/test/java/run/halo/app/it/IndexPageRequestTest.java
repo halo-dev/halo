ccde024f0373db597778ab27f7028320c5792c15
johnniang <johnniang@fastmail.com>
2021-03-07 15:42:02 +0800

    Add test for first page request

--- src/test/java/run/halo/app/it/IndexPageRequestTest.java
+++ src/test/java/run/halo/app/it/IndexPageRequestTest.java
@@ -1,47 +1,39 @@
 package run.halo.app.it;
 
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.web.client.RestTemplate;
-import run.halo.app.model.entity.Category;
-import run.halo.app.service.CategoryService;
 
 /**
  * Index page request test.
  *
  * @author johnniang
  */
-@SpringBootTest(webEnvironment = RANDOM_PORT)
-@ActiveProfiles("test")
 @Slf4j
-class IndexPageRequestTest {
-
-    @Autowired
-    RestTemplate restTemplate;
-
-    @LocalServerPort
-    int port;
-
-    @Autowired
-    CategoryService categoryService;
+class IndexPageRequestTest extends BaseApiTest {
 
     @Test
-    void contextLoad() {
-    }
+    void indexPage() throws IOException {
+        installBlog();
+        // validate atom.xml link
+        Document document = Jsoup.connect(blogUrl).get();
+        Element atomLink = document.head().getElementsByAttributeValue("title", "atom 1.0").get(0);
+        assertEquals(blogUrl + "/atom.xml", atomLink.attr("href"));
 
-    @Test
-    void indexPage() {
-        Category category = new Category();
-        category.setName("a category");
-        category.setSlug("a slug");
-        categoryService.create(category);
-        String response = restTemplate.getForObject("http://localhost:" + port + "/", String.class);
-        log.info("{}", response);
+        // validate title link
+        Element titleLink = document.body().selectFirst(".logo-title > .title > h3 > a");
+        assertEquals(blogUrl, titleLink.attr("href"));
+        assertEquals("Test's Blog", titleLink.text());
+
+        // validate post link
+        Element postTitleLink =
+            document.body().selectFirst(".content > .post > .post-title > h3 > a");
+        assertEquals(blogUrl + "/archives/hello-halo", postTitleLink.attr("href"));
+        assertEquals("Hello Halo", postTitleLink.text());
     }
 }
