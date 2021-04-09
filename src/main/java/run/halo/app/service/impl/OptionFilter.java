03b5d5d79ac68f1739dd3f6f01ea84ca94a72cfe
Ryan Wang <i@ryanc.cc>
2021-04-09 10:22:33 +0800

    feat: complete private option keys.

--- src/main/java/run/halo/app/service/impl/OptionFilter.java
+++ src/main/java/run/halo/app/service/impl/OptionFilter.java
@@ -10,6 +10,14 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 import run.halo.app.model.properties.AliOssProperties;
 import run.halo.app.model.properties.ApiProperties;
+import run.halo.app.model.properties.BaiduBosProperties;
+import run.halo.app.model.properties.EmailProperties;
+import run.halo.app.model.properties.HuaweiObsProperties;
+import run.halo.app.model.properties.MinioProperties;
+import run.halo.app.model.properties.QiniuOssProperties;
+import run.halo.app.model.properties.SmmsProperties;
+import run.halo.app.model.properties.TencentCosProperties;
+import run.halo.app.model.properties.UpOssProperties;
 import run.halo.app.model.support.HaloConst;
 import run.halo.app.service.OptionService;
 
@@ -32,9 +40,56 @@ public class OptionFilter {
 
     private Set<String> getDefaultPrivateOptionKeys() {
         return Set.of(
+            AliOssProperties.OSS_DOMAIN.getValue(),
+            AliOssProperties.OSS_BUCKET_NAME.getValue(),
             AliOssProperties.OSS_ACCESS_KEY.getValue(),
             AliOssProperties.OSS_ACCESS_SECRET.getValue(),
-            ApiProperties.API_ACCESS_KEY.getValue()
+
+            ApiProperties.API_ACCESS_KEY.getValue(),
+
+            BaiduBosProperties.BOS_DOMAIN.getValue(),
+            BaiduBosProperties.BOS_ENDPOINT.getValue(),
+            BaiduBosProperties.BOS_BUCKET_NAME.getValue(),
+            BaiduBosProperties.BOS_ACCESS_KEY.getValue(),
+            BaiduBosProperties.BOS_SECRET_KEY.getValue(),
+
+            EmailProperties.HOST.getValue(),
+            EmailProperties.PROTOCOL.getValue(),
+            EmailProperties.SSL_PORT.getValue(),
+            EmailProperties.USERNAME.getValue(),
+            EmailProperties.PASSWORD.getValue(),
+            EmailProperties.FROM_NAME.getValue(),
+            EmailProperties.ENABLED.getValue(),
+
+            HuaweiObsProperties.OSS_DOMAIN.getValue(),
+            HuaweiObsProperties.OSS_ENDPOINT.getValue(),
+            HuaweiObsProperties.OSS_BUCKET_NAME.getValue(),
+            HuaweiObsProperties.OSS_ACCESS_KEY.getValue(),
+            HuaweiObsProperties.OSS_ACCESS_SECRET.getValue(),
+
+            MinioProperties.ENDPOINT.getValue(),
+            MinioProperties.BUCKET_NAME.getValue(),
+            MinioProperties.ACCESS_KEY.getValue(),
+            MinioProperties.ACCESS_SECRET.getValue(),
+
+            QiniuOssProperties.OSS_ZONE.getValue(),
+            QiniuOssProperties.OSS_ACCESS_KEY.getValue(),
+            QiniuOssProperties.OSS_SECRET_KEY.getValue(),
+            QiniuOssProperties.OSS_DOMAIN.getValue(),
+            QiniuOssProperties.OSS_BUCKET.getValue(),
+
+            SmmsProperties.SMMS_API_SECRET_TOKEN.getValue(),
+
+            TencentCosProperties.COS_DOMAIN.getValue(),
+            TencentCosProperties.COS_REGION.getValue(),
+            TencentCosProperties.COS_BUCKET_NAME.getValue(),
+            TencentCosProperties.COS_SECRET_ID.getValue(),
+            TencentCosProperties.COS_SECRET_KEY.getValue(),
+
+            UpOssProperties.OSS_PASSWORD.getValue(),
+            UpOssProperties.OSS_BUCKET.getValue(),
+            UpOssProperties.OSS_DOMAIN.getValue(),
+            UpOssProperties.OSS_OPERATOR.getValue()
         );
     }
 
