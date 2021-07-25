82d3b90bbac0ffd118a3d3f82978d5faeefc0894
Ryan Wang <i@ryanc.cc>
2021-07-25 17:23:28 +0800

    feat: add region option for minio. (halo-dev/console#346)

--- src/views/system/optiontabs/AttachmentTab.vue
+++ src/views/system/optiontabs/AttachmentTab.vue
@@ -243,6 +243,9 @@
         <a-form-model-item label="Bucket（桶名称）：">
           <a-input v-model="options.minio_bucket_name" placeholder="桶名称" />
         </a-form-model-item>
+        <a-form-model-item label="Region（区域）：">
+          <a-input v-model="options.minio_region" placeholder="区域" />
+        </a-form-model-item>
         <a-form-model-item label="Access Key：">
           <a-input-password v-model="options.minio_access_key" autocomplete="new-password" />
         </a-form-model-item>
