d965b6c3e785d5d850e27d949e150943e0015812
guqing <38999863+guqing@users.noreply.github.com>
2021-11-22 09:10:59 +0800

    fix: not clear access rights when the password of private posts or categories was changed (#1540)
    * fix: 1518
    
    * feat: add test case
    
    * fix: remove unused import clause
    
    * fix: unit test case

--- src/main/java/run/halo/app/service/impl/AuthorizationServiceImpl.java
+++ src/main/java/run/halo/app/service/impl/AuthorizationServiceImpl.java
@@ -1,22 +1,31 @@
 package run.halo.app.service.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import run.halo.app.cache.AbstractStringCacheStore;
 import run.halo.app.service.AuthorizationService;
+import run.halo.app.utils.JsonUtils;
 
 /**
  * @author ZhiXiang Yuan
+ * @author guqing
  * @date 2021/01/21 11:28
  */
+@Slf4j
 @Service
 public class AuthorizationServiceImpl implements AuthorizationService {
-
+    private static final String ACCESS_PERMISSION_PREFIX = "ACCESS_PERMISSION: ";
     private final AbstractStringCacheStore cacheStore;
 
     public AuthorizationServiceImpl(AbstractStringCacheStore cacheStore) {
@@ -54,6 +63,29 @@ public class AuthorizationServiceImpl implements AuthorizationService {
         accessStore.remove(value);
 
         cacheStore.putAny(buildAccessPermissionKey(), accessStore, 1, TimeUnit.DAYS);
+
+        for (Entry<String, String> entry : cacheStore.toMap().entrySet()) {
+            String key = entry.getKey();
+            if (!key.startsWith(ACCESS_PERMISSION_PREFIX)) {
+                continue;
+            }
+            Set<String> valueSet = jsonToValueSet(entry.getValue());
+            if (valueSet.contains(value)) {
+                valueSet.remove(value);
+                cacheStore.putAny(key, valueSet, 1, TimeUnit.DAYS);
+            }
+        }
+    }
+
+    private Set<String> jsonToValueSet(String json) {
+        try {
+            return JsonUtils.DEFAULT_JSON_MAPPER.readValue(json,
+                new TypeReference<LinkedHashSet<String>>() {
+                });
+        } catch (JsonProcessingException e) {
+            log.warn("Failed to convert json to authorization cache value set: [{}]", json, e);
+        }
+        return Collections.emptySet();
     }
 
     private void doAuthorization(String value) {
@@ -70,7 +102,7 @@ public class AuthorizationServiceImpl implements AuthorizationService {
 
         HttpServletRequest request = requestAttributes.getRequest();
 
-        return "ACCESS_PERMISSION: " + request.getSession().getId();
+        return ACCESS_PERMISSION_PREFIX + request.getSession().getId();
     }
 
 }
