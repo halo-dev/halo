641264ba5cb8f350e5e1e80fde6de9902bf02f2c
Ryan Wang <i@ryanc.cc>
2022-03-03 11:34:42 +0800

    chore: clean code for next major version
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- src/main/java/run/halo/app/utils/footnotes/FootnoteVisitor.java
+++
@@ -1,7 +0,0 @@
-package run.halo.app.utils.footnotes;
-
-public interface FootnoteVisitor {
-    void visit(FootnoteBlock node);
-
-    void visit(Footnote node);
-}
--- src/main/resources/migration/V5__migrate_remove_notnull_for_email_in_comments_table.sql
+++
@@ -1,4 +0,0 @@
--- Remove notnull for email in comments table
-
--- Migrate comments Table
-alter table comments modify email VARCHAR(255);
