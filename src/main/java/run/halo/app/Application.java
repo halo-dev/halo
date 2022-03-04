e2843adbe7e885c1dad09ad1d23d7ada8c7a5552
guqing <38999863+guqing@users.noreply.github.com>
2022-03-04 15:04:11 +0800

    Create basic project structure for halo 2.0 (#1699)
    * feat: Add project structure
    
    * feat: Add config
    
    * feat: Add flyway migrate config for h2 and mysql and postgresql
    
    * fix: checkstyle config
    
    * chore: Upgrade springboot 2.6 to 3.0.0-m1
    
    * feat: Add spring snapshot url for repositories
    
    * refactor: Change jdk11 to jdk17 for github ci config
    
    * refactor: update build.gradle config

---
+++ src/main/java/run/halo/app/Application.java
@@ -0,0 +1,21 @@
+package run.halo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Halo main class.
+ *
+ * @author ryanwang
+ * @author JohnNiang
+ * @author guqing
+ * @date 2017-11-14
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
