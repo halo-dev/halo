57854f78eb13883d244e5b4ef92e21d1352c77f4
guqing <1484563614@qq.com>
2022-04-13 14:39:07 +0800

    refactor: use EnableWebSecurity annotation

--- src/main/java/run/halo/app/config/WebSecurityConfig.java
+++ src/main/java/run/halo/app/config/WebSecurityConfig.java
@@ -11,9 +11,9 @@ import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
@@ -35,7 +35,7 @@ import run.halo.app.infra.properties.JwtProperties;
  * @author guqing
  * @date 2022-04-12
  */
-@Configuration
+@EnableWebSecurity
 @EnableConfigurationProperties(JwtProperties.class)
 public class WebSecurityConfig {
 
