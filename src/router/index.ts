334e6ad09f8160bdb82490d675b8cf42d8af7f06
Ryan Wang <i@ryanc.cc>
2022-04-13 17:34:14 +0800

    feat: router use hash history
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import routesConfig from "@/router/routes.config";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: routesConfig,
   scrollBehavior: () => ({ left: 0, top: 0 }),
 });
