ee0fa26c8bdcc1f62a53fb6255c93f77cca49414
Ryan Wang <i@ryanc.cc>
2022-04-19 12:09:24 +0800

    test: add input component test case
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- src/components/base/input/__tests__/Input.spec.ts
+++ src/components/base/input/__tests__/Input.spec.ts
@@ -1,8 +1,20 @@
 import { describe, expect, it } from "vitest";
 import { VInput } from "../index";
+import { mount } from "@vue/test-utils";
 
 describe("Input", () => {
   it("should render", () => {
     expect(VInput).toBeDefined();
+    expect(mount(VInput).html()).toMatchSnapshot();
+  });
+
+  it("should work with size prop", function () {
+    ["lg", "md", "sm", "xs"].forEach((size) => {
+      const input = mount(VInput, { props: { size } });
+
+      expect(input.html()).toMatchSnapshot();
+      expect(input.find("input").classes()).toContain(`input-${size}`);
+      input.unmount();
+    });
   });
 });
