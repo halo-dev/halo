1677135160c5911d61e13331bd3e13466e9ca656
yhc <876680538@qq.com>
2022-04-23 14:01:54 +0800

    do not encode field content

--- src/main/java/run/halo/app/config/serializer/XssJacksonDeserializer.java
+++ src/main/java/run/halo/app/config/serializer/XssJacksonDeserializer.java
@@ -4,14 +4,22 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.text.StringEscapeUtils;
 import java.io.IOException;
 
 
+@Slf4j
 public class XssJacksonDeserializer extends JsonDeserializer<String> {
 
     @Override
-    public String deserialize(JsonParser jp, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
-        return StringEscapeUtils.escapeHtml4(jp.getText());
+    public String deserialize(JsonParser jp, DeserializationContext deserializationContext)
+        throws IOException, JsonProcessingException {
+        String currentName = jp.getParsingContext().getCurrentName();
+        if (!"content".equals(currentName)) {
+            return StringEscapeUtils.escapeHtml4(jp.getText());
+        } else {
+            return jp.getText();
+        }
     }
 }
