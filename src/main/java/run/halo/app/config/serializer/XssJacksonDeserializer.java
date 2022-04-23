261b2b66f8155534042ffc5dc2d2441b9f15f95d
yhc <876680538@qq.com>
2022-04-23 14:02:29 +0800

    Update XssJacksonDeserializer.java

--- src/main/java/run/halo/app/config/serializer/XssJacksonDeserializer.java
+++ src/main/java/run/halo/app/config/serializer/XssJacksonDeserializer.java
@@ -4,12 +4,10 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.text.StringEscapeUtils;
 import java.io.IOException;
 
 
-@Slf4j
 public class XssJacksonDeserializer extends JsonDeserializer<String> {
 
     @Override
