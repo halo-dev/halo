4ca8071c8e0a81e4ce7a099db9e64290cf9e5f5b
Ryan Wang <i@ryanc.cc>
2022-04-24 15:14:03 +0800

    chore: add gzip compression vite plugin
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import icons from "unplugin-icons/vite";
+import Compression from "vite-compression-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), icons()],
+  plugins: [vue(), vueJsx(), icons(), Compression()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
