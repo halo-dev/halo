2b35d67c4e9a9676ce083bde5a6563b9d019b437
Ryan Wang <i@ryanc.cc>
2022-04-27 15:28:13 +0800

    feat: add tooltip component
    Signed-off-by: Ryan Wang <i@ryanc.cc>

---
+++ src/components/base/tooltip/Tooltip.story.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import { VTooltip } from "@/components/base/tooltip";
+import { VButton } from "@/components/base/button";
+</script>
+<template>
+  <Story title="Tooltip">
+    <template #default>
+      <VTooltip content="Hello Halo">
+        <VButton>Hello</VButton>
+      </VTooltip>
+      <VTooltip content="Hello Halo">
+        <div class="inline-flex">Halo</div>
+      </VTooltip>
+    </template>
+  </Story>
+</template>
---
+++ src/components/base/tooltip/index.ts
@@ -0,0 +1,1 @@
+export { default as VTooltip } from "./Tooltip.vue";
