d575f8c9ecffa9a0265511f0643564cc58dfd011
Ryan Wang <i@ryanc.cc>
2022-05-11 19:22:10 +0800

    feat: add pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,23 @@ import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import icons from "unplugin-icons/vite";
 import Compression from "vite-compression-plugin";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), icons(), Compression()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    icons(),
+    Compression(),
+    VitePWA({
+      manifest: {
+        name: "Halo",
+        short_name: "Halo",
+        description: "Web Client For Halo",
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
