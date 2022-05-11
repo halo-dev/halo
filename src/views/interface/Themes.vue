bf4dbc7888d36d62836ac04bc6be8008f7c90469
Ryan Wang <i@ryanc.cc>
2022-05-11 17:47:42 +0800

    feat: add themes page component
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- src/views/interface/Themes.vue
+++ src/views/interface/Themes.vue
@@ -2,7 +2,9 @@
 import { FilledLayout } from "@/layouts";
 import { VPageHeader } from "@/components/base/header";
 import { VButton } from "@/components/base/button";
+import { VCard } from "@/components/base/card";
 import { IconPalette } from "@/core/icons";
+import { themes } from "@/views/interface/themes-mock";
 </script>
 <template>
   <FilledLayout>
@@ -14,5 +16,31 @@ import { IconPalette } from "@/core/icons";
         <VButton type="secondary">安装</VButton>
       </template>
     </VPageHeader>
+
+    <div class="m-4">
+      <ul
+        class="grid grid-cols-1 gap-x-2 gap-y-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6"
+        role="list"
+      >
+        <li v-for="(theme, index) in themes" :key="index" class="relative">
+          <VCard :body-class="['!p-0']">
+            <div
+              class="group block w-full aspect-w-10 aspect-h-7 bg-gray-100 overflow-hidden cursor-pointer"
+            >
+              <img
+                :src="theme.screenshots"
+                alt=""
+                class="object-cover pointer-events-none group-hover:opacity-75"
+              />
+            </div>
+            <p
+              class="block text-sm font-medium text-gray-700 truncate pointer-events-none px-2 py-1"
+            >
+              {{ theme.name }}
+            </p>
+          </VCard>
+        </li>
+      </ul>
+    </div>
   </FilledLayout>
 </template>
