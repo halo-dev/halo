5272c71bd7591375cf8b71288f75b85330c3ea9c
Camsyn <65994555+Camsyn@users.noreply.github.com>
2022-05-23 21:29:42 +0800

    Update src/main/java/run/halo/app/repository/LinkRepository.java
    Co-authored-by: John Niang <johnniang@fastmail.com>

--- src/main/java/run/halo/app/repository/LinkRepository.java
+++ src/main/java/run/halo/app/repository/LinkRepository.java
@@ -17,7 +17,7 @@ public interface LinkRepository extends BaseRepository<Link, Integer> {
      *
      * @return a list of teams
      */
-    @Query(value = "select a.team from Link a group by a.team order by max(a.priority) DESC ")
+    @Query(value = "select a.team from Link a group by a.team order by max(a.priority) DESC")
     List<String> findAllTeams();
 
     boolean existsByNameAndIdNot(String name, Integer id);
