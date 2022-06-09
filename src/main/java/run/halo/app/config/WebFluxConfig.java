728b39dfa2cfa4326f12622d3d656d1f1490e4a0
johnniang <johnniang@fastmail.com>
2022-06-09 15:31:45 +0800

    Overwrite Jackson2Json decoder and encoder

--- src/main/java/run/halo/app/config/WebFluxConfig.java
+++ src/main/java/run/halo/app/config/WebFluxConfig.java
@@ -1,10 +1,14 @@
 package run.halo.app.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.codec.CodecConfigurer;
 import org.springframework.http.codec.HttpMessageWriter;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
 import org.springframework.lang.NonNull;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
@@ -15,6 +19,12 @@ import org.springframework.web.reactive.result.view.ViewResolver;
 @Configuration
 @EnableWebFlux
 public class WebFluxConfig implements WebFluxConfigurer {
+
+    final ObjectMapper objectMapper;
+
+    public WebFluxConfig(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
 
     @Bean
     ServerResponse.Context context(CodecConfigurer codec,
@@ -32,5 +42,14 @@ public class WebFluxConfig implements WebFluxConfigurer {
                 return resultHandler.getViewResolvers();
             }
         };
+    }
+
+    @Override
+    public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
+        // we need to customize the Jackson2Json[Decoder][Encoder] here to serialize and
+        // deserialize special types, e.g.: Instant, LocalDateTime. So we use ObjectMapper
+        // created by outside.
+        configurer.defaultCodecs().jackson2JsonDecoder(new Jackson2JsonDecoder(objectMapper));
+        configurer.defaultCodecs().jackson2JsonEncoder(new Jackson2JsonEncoder(objectMapper));
     }
 }
