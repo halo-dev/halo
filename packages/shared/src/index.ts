3f7958158ffc6defc37b19102d33924af18b85b5
Ryan Wang <i@ryanc.cc>
2022-06-16 18:42:13 +0800

    feat: create admin-shared package (halo-dev/console#581)
    Signed-off-by: Ryan Wang <i@ryanc.cc>

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,2 @@
+export * from "./types/plugin";
+export * from "./types/menus";
---
+++ packages/shared/src/types/plugin.ts
@@ -0,0 +1,40 @@
+import type { Component } from "vue";
+import type { RouteRecordRaw } from "vue-router";
+import type { MenuGroupType } from "./menus";
+
+export type ExtensionPointName =
+  | "POSTS"
+  | "POST_EDITOR"
+  | "DASHBOARD"
+  | "USER_SETTINGS";
+
+// TODO define extension point state
+export type ExtensionPointState = Record<string, unknown>;
+
+export interface Plugin {
+  name: string;
+
+  /**
+   * This components will be registered when plugin is activated.
+   */
+  components?: Component[];
+
+  /**
+   * Activate hook will be called when plugin is activated.
+   */
+  activated?: () => void;
+
+  /**
+   * Deactivate hook will be called when plugin is deactivated.
+   */
+  deactivated?: () => void;
+
+  routes?: RouteRecordRaw[];
+
+  menus?: MenuGroupType[];
+
+  extensionPoints?: Record<
+    ExtensionPointName,
+    (state: ExtensionPointState) => void
+  >;
+}
