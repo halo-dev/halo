2f4721c62b864aab6be9f81954cb5ad5069ec9d8
Ryan Wang <i@ryanc.cc>
2022-06-21 16:36:41 +0800

    refactor(components): remove tooltip component
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- packages/components/src/components.ts
+++ packages/components/src/components.ts
@@ -12,4 +12,3 @@ export * from "./components/space";
 export * from "./components/tabs";
 export * from "./components/tag";
 export * from "./components/textarea";
-export * from "./components/tooltip";
--- packages/components/src/components/tooltip/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as VTooltip } from "./Tooltip.vue";
