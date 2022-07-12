68a788f1c85d9d5a38d7e402c9fdf926f43fc470
guqing <i@guqing.email>
2022-07-12 15:07:00 +0800

    refactor: tostring

--- src/main/java/run/halo/app/extension/ConfigMap.java
+++ src/main/java/run/halo/app/extension/ConfigMap.java
@@ -4,7 +4,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import org.springframework.lang.Nullable;
+import lombok.ToString;
 
 /**
  * <p>ConfigMap holds configuration data to consume.</p>
@@ -13,6 +13,7 @@ import org.springframework.lang.Nullable;
  * @since 2.0.0
  */
 @Data
+@ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 @GVK(group = "", version = "v1alpha1", kind = "ConfigMap", plural = "configmaps",
     singular = "configmap")
@@ -26,31 +27,5 @@ public class ConfigMap extends AbstractExtension {
         }
         this.data.put(key, dataItem);
         return this;
-    }
-
-    @Nullable
-    public Map<String, String> getData() {
-        return data;
-    }
-
-    @Override
-    public String toString() {
-        return "class ConfigMap {\n"
-            + "    apiVersion: " + toIndentedString(getApiVersion()) + "\n"
-            + "    data: " + toIndentedString(data) + "\n"
-            + "    kind: " + toIndentedString(getKind()) + "\n"
-            + "    metadata: " + toIndentedString(getMetadata()) + "\n"
-            + "}";
-    }
-
-    /**
-     * Convert the given object to string with each line indented by 4 spaces (except the first
-     * line).
-     */
-    private String toIndentedString(Object o) {
-        if (o == null) {
-            return "null";
-        }
-        return o.toString().replace("\n", "\n    ");
     }
 }
