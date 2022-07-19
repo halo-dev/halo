05aec43bbd7e42f12195a086df2c2a0f6fe9942b
guqing <i@guqing.email>
2022-07-19 11:06:45 +0800

    refactor: the way of plugin extension update

--- src/main/java/run/halo/app/plugin/PluginStartedListener.java
+++ src/main/java/run/halo/app/plugin/PluginStartedListener.java
@@ -1,8 +1,22 @@
 package run.halo.app.plugin;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.jar.JarFile;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.zip.ZipEntry;
+import lombok.extern.slf4j.Slf4j;
+import org.pf4j.DevelopmentPluginClasspath;
+import org.pf4j.PluginRuntimeException;
 import org.pf4j.PluginWrapper;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.io.DefaultResourceLoader;
@@ -56,5 +70,55 @@ public class PluginStartedListener implements ApplicationListener<HaloPluginStar
                         extensionClient.update(unstructured);
                     }, () -> extensionClient.create(unstructured));
             });
+    }
+
+    @Slf4j
+    static class PluginExtensionLoaderUtils {
+        static final String EXTENSION_LOCATION = "extensions";
+        static final DevelopmentPluginClasspath pluginClasspath = new DevelopmentPluginClasspath();
+
+        static Set<String> lookupFromClasses(Path pluginPath) {
+            Set<String> result = new HashSet<>();
+            for (String directory : pluginClasspath.getClassesDirectories()) {
+                File file = pluginPath.resolve(directory).resolve(EXTENSION_LOCATION).toFile();
+                if (file.exists() && file.isDirectory()) {
+                    result.addAll(walkExtensionFiles(file.toPath()));
+                }
+            }
+            return result;
+        }
+
+        private static Set<String> walkExtensionFiles(Path location) {
+            try (Stream<Path> stream = Files.walk(location)) {
+                return stream.map(Path::normalize)
+                    .filter(Files::isRegularFile)
+                    .filter(path -> isYamlFile(path.getFileName().toString()))
+                    .map(path -> location.getParent().relativize(path).toString())
+                    .collect(Collectors.toSet());
+            } catch (IOException e) {
+                log.debug("Failed to walk extension files from [{}]", location);
+                return Collections.emptySet();
+            }
+        }
+
+        static boolean isYamlFile(String path) {
+            return path.endsWith(".yaml") || path.endsWith(".yml");
+        }
+
+        static Set<String> lookupFromJar(Path pluginJarPath) {
+            try (JarFile jarFile = new JarFile(pluginJarPath.toFile())) {
+                return jarFile.stream()
+                    .filter(jarEntry -> {
+                        String name = jarEntry.getName();
+                        return name.startsWith(EXTENSION_LOCATION)
+                            && !jarEntry.isDirectory()
+                            && isYamlFile(name);
+                    })
+                    .map(ZipEntry::getName)
+                    .collect(Collectors.toSet());
+            } catch (IOException e) {
+                throw new PluginRuntimeException(e);
+            }
+        }
     }
 }
---
+++ src/test/java/run/halo/app/plugin/PluginStartedListenerTest.java
@@ -0,0 +1,16 @@
+package run.halo.app.plugin;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author guqing
+ * @since 2.0.0
+ */
+class PluginStartedListenerTest {
+    @Test
+    void test() {
+
+    }
+}
