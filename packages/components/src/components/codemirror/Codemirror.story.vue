e218818a5971c1d3a91b0fd6c00534947d7497f0
Ryan Wang <i@ryanc.cc>
2022-07-22 16:12:28 +0800

    feat: add codemirror component
    Signed-off-by: Ryan Wang <i@ryanc.cc>

---
+++ packages/components/src/components/codemirror/Codemirror.story.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import { VCodemirror } from "./index";
+
+function initState() {
+  return {
+    value: "Hello Halo",
+  };
+}
+</script>
+<template>
+  <Story :initState="initState" title="Codemirror">
+    <template #default="{ state }">
+      <VCodemirror v-model="state.value" height="500px" language="javascript" />
+    </template>
+  </Story>
+</template>
---
+++ packages/components/src/components/codemirror/Codemirror.vue
@@ -0,0 +1,97 @@
+<script lang="ts" setup>
+import type { PropType } from "vue";
+import { onBeforeUnmount, onMounted, shallowRef, watch } from "vue";
+import type { EditorStateConfig } from "@codemirror/state";
+import { EditorState } from "@codemirror/state";
+import { EditorView } from "@codemirror/view";
+import { basicSetup } from "codemirror";
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: "",
+  },
+  height: {
+    type: String,
+    default: "auto",
+  },
+  extensions: {
+    type: Array as PropType<EditorStateConfig["extensions"]>,
+    default: () => [],
+  },
+});
+
+const emit = defineEmits<{
+  (e: "update:modelValue", value: string): void;
+  (e: "change", value: string): void;
+}>();
+
+const customTheme = EditorView.theme({
+  "&": {
+    height: props.height,
+  },
+});
+
+const wrapper = shallowRef<HTMLDivElement>();
+const cmState = shallowRef<EditorState>();
+const cmView = shallowRef<EditorView>();
+
+const createCmEditor = () => {
+  let extensions = [
+    basicSetup,
+    EditorView.lineWrapping,
+    customTheme,
+    EditorView.updateListener.of((viewUpdate) => {
+      if (viewUpdate.docChanged) {
+        const doc = viewUpdate.state.doc.toString();
+        emit("update:modelValue", doc);
+        emit("change", doc);
+      }
+    }),
+  ];
+
+  if (props.extensions) {
+    extensions = extensions.concat(props.extensions);
+  }
+
+  cmState.value = EditorState.create({
+    doc: props.modelValue,
+    extensions,
+  });
+
+  cmView.value = new EditorView({
+    state: cmState.value,
+    parent: wrapper.value,
+  });
+};
+
+onMounted(() => {
+  createCmEditor();
+
+  // Update the codemirror editor doc when the model value changes.
+  watch(
+    () => props.modelValue,
+    (newValue) => {
+      if (newValue !== cmView.value?.state.doc.toString()) {
+        cmView.value?.dispatch({
+          changes: {
+            from: 0,
+            to: cmView.value?.state.doc.length,
+            insert: newValue,
+          },
+        });
+      }
+    }
+  );
+});
+
+// Destroy codemirror editor when component unmounts
+onBeforeUnmount(() => {
+  if (cmView.value) {
+    cmView.value.destroy();
+  }
+});
+</script>
+<template>
+  <div ref="wrapper" class="codemirror-wrapper contents"></div>
+</template>
