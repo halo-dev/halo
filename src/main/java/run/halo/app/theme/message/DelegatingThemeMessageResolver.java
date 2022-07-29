d1f2cb75b05eee6e77a6742e044361802fbeaaa9
guqing <i@guqing.email>
2022-07-29 17:14:44 +0800

    refactor: message resolver lock

--- src/main/java/run/halo/app/theme/message/DelegatingThemeMessageResolver.java
+++ src/main/java/run/halo/app/theme/message/DelegatingThemeMessageResolver.java
@@ -1,7 +1,8 @@
 package run.halo.app.theme.message;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.StampedLock;
 import org.thymeleaf.context.ITemplateContext;
 import org.thymeleaf.messageresolver.AbstractMessageResolver;
 import run.halo.app.theme.ThemeContext;
@@ -12,7 +13,8 @@ import run.halo.app.theme.ThemeContextHolder;
  * @since 2.0.0
  */
 public class DelegatingThemeMessageResolver extends AbstractMessageResolver {
-    private final Map<String, ThemeMessageResolver> delegateCache = new ConcurrentHashMap<>();
+    private final Map<String, ThemeMessageResolver> delegateCache = new HashMap<>();
+    private final StampedLock stampedLock = new StampedLock();
 
     @Override
     public String resolveMessage(ITemplateContext context, Class<?> origin, String key,
@@ -27,14 +29,33 @@ public class DelegatingThemeMessageResolver extends AbstractMessageResolver {
             messageParameters);
     }
 
-    private synchronized ThemeMessageResolver getMessageResolver() {
+    private ThemeMessageResolver getMessageResolver() {
         ThemeContext themeContext = ThemeContextHolder.getThemeContext();
         String themeName = themeContext.getThemeName();
+        long stamp = stampedLock.tryOptimisticRead();
         ThemeMessageResolver themeMessageResolver = delegateCache.get(themeName);
         if (themeMessageResolver == null) {
-            themeMessageResolver = new ThemeMessageResolver();
-            delegateCache.put(themeName, themeMessageResolver);
+            return createMessageResolver(themeName);
+        }
+        if (!stampedLock.validate(stamp)) {
+            stamp = stampedLock.readLock();
+            try {
+                return delegateCache.get(themeName);
+            } finally {
+                stampedLock.unlockRead(stamp);
+            }
         }
         return themeMessageResolver;
+    }
+
+    private ThemeMessageResolver createMessageResolver(String themeName) {
+        long stamp = stampedLock.writeLock();
+        try {
+            ThemeMessageResolver themeMessageResolver = new ThemeMessageResolver();
+            delegateCache.put(themeName, themeMessageResolver);
+            return themeMessageResolver;
+        } finally {
+            stampedLock.unlock(stamp);
+        }
     }
 }
