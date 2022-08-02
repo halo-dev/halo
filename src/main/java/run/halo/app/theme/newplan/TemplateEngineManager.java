1f0869e1d06decd94ea5ba9e0ef2ee405c3c7bff
johnniang <johnniang@fastmail.com>
2022-08-02 10:52:13 +0800

    Remove unused MessageResolver
    Signed-off-by: johnniang <johnniang@fastmail.com>

--- src/main/java/run/halo/app/theme/newplan/TemplateEngineManager.java
+++ src/main/java/run/halo/app/theme/newplan/TemplateEngineManager.java
@@ -4,6 +4,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 import org.thymeleaf.dialect.IDialect;
 import org.thymeleaf.spring6.ISpringWebFluxTemplateEngine;
@@ -11,7 +12,6 @@ import org.thymeleaf.templateresolver.FileTemplateResolver;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 import run.halo.app.infra.properties.HaloProperties;
 import run.halo.app.theme.engine.SpringWebFluxTemplateEngine;
-import run.halo.app.theme.message.DelegatingThemeMessageResolver;
 import run.halo.app.theme.message.ThemeMessageResolver;
 
 @Component
@@ -21,20 +21,20 @@ public class TemplateEngineManager {
 
     private final ThymeleafProperties thymeleafProperties;
 
-    private final HaloProperties haloProperties;
-
     private final ObjectProvider<ITemplateResolver> templateResolvers;
 
     private final ObjectProvider<IDialect> dialects;
+    private final MessageSource messageSource;
 
     public TemplateEngineManager(ThymeleafProperties thymeleafProperties,
         HaloProperties haloProperties,
         ObjectProvider<ITemplateResolver> templateResolvers,
-        ObjectProvider<IDialect> dialects) {
+        ObjectProvider<IDialect> dialects,
+        MessageSource messageSource) {
         this.thymeleafProperties = thymeleafProperties;
-        this.haloProperties = haloProperties;
         this.templateResolvers = templateResolvers;
         this.dialects = dialects;
+        this.messageSource = messageSource;
     }
 
     public ISpringWebFluxTemplateEngine getTemplateEngine(ThemeContext theme) {
@@ -42,6 +42,7 @@ public class TemplateEngineManager {
             var engine = new SpringWebFluxTemplateEngine();
             engine.setEnableSpringELCompiler(thymeleafProperties.isEnableSpringElCompiler());
             engine.setMessageResolver(new ThemeMessageResolver(theme));
+            engine.setMessageSource(messageSource);
             engine.addLinkBuilder(new ThemeLinkBuilder(theme));
             engine.setRenderHiddenMarkersBeforeCheckboxes(
                 thymeleafProperties.isRenderHiddenMarkersBeforeCheckboxes());
