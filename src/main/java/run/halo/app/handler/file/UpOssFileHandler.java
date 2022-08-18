9e861a28a94221e68931ad8e8d3956eac247b6d7
源 <73240868+JustinLiang522@users.noreply.github.com>
2022-08-18 15:32:31 +0800

    checkstyle

--- src/main/java/run/halo/app/handler/file/UpOssFileHandler.java
+++ src/main/java/run/halo/app/handler/file/UpOssFileHandler.java
@@ -139,8 +139,8 @@ public class UpOssFileHandler implements FileHandler {
         try {
             Response result = manager.deleteFile(key, null);
             HashMap respondBody = JsonUtils.jsonToObject(result.body().string(), HashMap.class);
-            if (!result.isSuccessful() &&
-                !(result.code() == 404 && respondBody.get("code").equals(40400001))) {
+            if (!result.isSuccessful()
+                && !(result.code() == 404 && respondBody.get("code").equals(40400001))) {
                 log.warn("附件 " + key + " 从又拍云删除失败");
                 throw new FileOperationException("附件 " + key + " 从又拍云删除失败");
             }
