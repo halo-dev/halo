0e0b52ef7022d34f7e09093bfd4e89504f54a890
guqing <1484563614@qq.com>
2022-08-25 17:25:56 +0800

    refactor: mono subscriber

--- src/main/java/run/halo/app/theme/HaloViewResolver.java
+++ src/main/java/run/halo/app/theme/HaloViewResolver.java
@@ -10,6 +10,7 @@ import org.springframework.web.server.ServerWebExchange;
 import org.thymeleaf.spring6.view.reactive.ThymeleafReactiveView;
 import org.thymeleaf.spring6.view.reactive.ThymeleafReactiveViewResolver;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 import run.halo.app.theme.finders.FinderRegistry;
 
 @Component("thymeleafReactiveViewResolver")
@@ -47,7 +48,8 @@ public class HaloViewResolver extends ThymeleafReactiveViewResolver {
             return themeResolver.getTheme(exchange.getRequest()).flatMap(theme -> {
                 // calculate the engine before rendering
                 setTemplateEngine(engineManager.getTemplateEngine(theme));
-                return super.render(model, contentType, exchange);
+                return super.render(model, contentType, exchange)
+                        .subscribeOn(Schedulers.boundedElastic());
             });
         }
     }
