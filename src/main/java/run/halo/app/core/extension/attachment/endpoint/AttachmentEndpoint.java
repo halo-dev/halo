6bbd7a6c32832e583dee499c08bd33f990093d44
johnniang <johnniang@fastmail.com>
2022-09-02 10:16:01 +0800

    Enable group set when uploading attachment
    Signed-off-by: johnniang <johnniang@fastmail.com>

--- src/main/java/run/halo/app/core/extension/attachment/endpoint/AttachmentEndpoint.java
+++ src/main/java/run/halo/app/core/extension/attachment/endpoint/AttachmentEndpoint.java
@@ -78,6 +78,9 @@ public class AttachmentEndpoint implements CustomEndpoint {
         @Schema(required = true, description = "Storage policy name")
         String getPolicyName();
 
+        @Schema(description = "The name of the group to which the attachment belongs")
+        String getGroupName();
+
     }
 
     public record UploadRequest(MultiValueMap<String, Part> formData) implements IUploadRequest {
@@ -94,6 +97,14 @@ public class AttachmentEndpoint implements CustomEndpoint {
                 return form.value();
             }
             throw new ServerWebInputException("Invalid part of policyName");
+        }
+
+        @Override
+        public String getGroupName() {
+            if (formData.getFirst("groupName") instanceof FormFieldPart form) {
+                return form.value();
+            }
+            return null;
         }
 
     }
@@ -115,23 +126,28 @@ public class AttachmentEndpoint implements CustomEndpoint {
                             .map(configMap -> new UploadOption(uploadRequest.getFile(), policy,
                                 configMap));
                     })
+                    // find the proper handler to handle the attachment
+                    .flatMap(uploadOption -> Flux.fromIterable(
+                            pluginManager.getExtensions(AttachmentHandler.class))
+                        .concatMap(uploadHandler -> uploadHandler.upload(uploadOption)
+                            .doOnNext(attachment -> {
+                                var spec = attachment.getSpec();
+                                spec.setUploadedBy(Ref.of(username));
+                                spec.setPolicyRef(Ref.of(uploadOption.policy()));
+                                var groupName = uploadRequest.getGroupName();
+                                if (groupName != null) {
+                                    // validate the group name
+                                    spec.setGroupRef(Ref.of(groupName));
+                                }
+                                // set finalizers mandatory
+                                attachment.getMetadata().setFinalizers(Set.of(FINALIZER_NAME));
+                            }))
+                        .next()
+                        .switchIfEmpty(Mono.error(
+                            () -> new ResponseStatusException(
+                                HttpStatus.INTERNAL_SERVER_ERROR,
+                                "No suitable handler found for uploading the attachment"))))
                 )
-                // find the proper handler to handle the attachment
-                .flatMap(uploadOption -> Flux.fromIterable(
-                        pluginManager.getExtensions(AttachmentHandler.class))
-                    .concatMap(uploadHandler -> uploadHandler.upload(uploadOption)
-                        .doOnNext(attachment -> {
-                            var spec = attachment.getSpec();
-                            spec.setUploadedBy(Ref.of(username));
-                            spec.setPolicyRef(Ref.of(uploadOption.policy()));
-                            // set finalizers mandatory
-                            attachment.getMetadata().setFinalizers(Set.of(FINALIZER_NAME));
-                        }))
-                    .next()
-                    .switchIfEmpty(Mono.error(
-                        () -> new ResponseStatusException(
-                            HttpStatus.INTERNAL_SERVER_ERROR,
-                            "No suitable handler found for uploading the attachment"))))
                 // create the attachment
                 .flatMap(client::create)
                 .flatMap(attachment -> ServerResponse.ok()
