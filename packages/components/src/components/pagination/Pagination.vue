2788261001e7a83dc641b8a56288132ac19300d1
Ryan Wang <i@ryanc.cc>
2022-09-13 16:25:45 +0800

    fix: display styles of pagination component
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- packages/components/src/components/pagination/Pagination.vue
+++ packages/components/src/components/pagination/Pagination.vue
@@ -57,7 +57,7 @@ watch(
     @page-size-change="onPageChange"
   >
     <div class="bg-white flex items-center justify-between">
-      <div class="flex-1 flex justify-between sm:hidden items-center">
+      <div class="flex-1 flex justify-between sm:!hidden items-center">
         <span
           class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 cursor-pointer"
           @click="prev"
