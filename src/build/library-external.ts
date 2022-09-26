9050aa01766965c0dfd0d0aaa3d781219a279577
Ryan Wang <i@ryanc.cc>
2022-09-26 13:16:40 +0800

    refactor: external static libraries
    Signed-off-by: Ryan Wang <i@ryanc.cc>

---
+++ src/build/library-external.ts
@@ -0,0 +1,98 @@
+import { viteExternalsPlugin as ViteExternals } from "vite-plugin-externals";
+import {
+  viteStaticCopy as ViteStaticCopy,
+  type Target,
+} from "vite-plugin-static-copy";
+import { createHtmlPlugin as VitePluginHtml } from "vite-plugin-html";
+import randomstring from "randomstring";
+
+/**
+ * It copies the external libraries to the `assets` folder, and injects the script tags into the HTML
+ *
+ * @param {boolean} isProduction - boolean
+ * @param {string} baseUrl - The base url of the application.
+ * @returns An array of plugins
+ */
+export const setupLibraryExternal = (
+  isProduction: boolean,
+  baseUrl: string
+) => {
+  const staticSuffix = randomstring.generate({
+    length: 8,
+    charset: "hex",
+  });
+
+  const staticTargets: Target[] = [
+    {
+      src: `./node_modules/vue/dist/vue.global${
+        isProduction ? ".prod" : ""
+      }.js`,
+      dest: "assets/vue",
+      rename: `vue.global.${staticSuffix}.js`,
+    },
+    {
+      src: `./node_modules/vue-router/dist/vue-router.global${
+        isProduction ? ".prod" : ""
+      }.js`,
+      dest: "assets/vue-router",
+      rename: `vue-router.global.${staticSuffix}.js`,
+    },
+    {
+      src: "./node_modules/@vueuse/shared/index.iife.min.js",
+      dest: "assets/vueuse",
+      rename: `vueuse.shared.iife.${staticSuffix}.js`,
+    },
+    {
+      src: "./node_modules/@vueuse/core/index.iife.min.js",
+      dest: "assets/vueuse",
+      rename: `vueuse.core.iife.${staticSuffix}.js`,
+    },
+    {
+      src: "./node_modules/@vueuse/components/index.iife.min.js",
+      dest: "assets/vueuse",
+      rename: `vueuse.components.iife.${staticSuffix}.js`,
+    },
+    {
+      src: "./node_modules/@vueuse/router/index.iife.min.js",
+      dest: "assets/vueuse",
+      rename: `vueuse.router.iife.${staticSuffix}.js`,
+    },
+    {
+      src: "./node_modules/@halo-dev/components/dist/halo-components.iife.js",
+      dest: "assets/components",
+      rename: `halo-components.iife.${staticSuffix}.js`,
+    },
+    {
+      src: "./node_modules/@halo-dev/admin-shared/dist/halo-admin-shared.iife.js",
+      dest: "assets/admin-shared",
+      rename: `halo-admin-shared.iife.${staticSuffix}.js`,
+    },
+  ];
+
+  return [
+    ViteExternals({
+      vue: "Vue",
+      "vue-router": "VueRouter",
+      "@halo-dev/shared": "HaloAdminShared",
+      "@halo-dev/components": "HaloComponents",
+      "@vueuse/core": "VueUse",
+      "@vueuse/components": "VueUse",
+      "@vueuse/router": "VueUse",
+    }),
+    ViteStaticCopy({
+      targets: staticTargets,
+    }),
+    VitePluginHtml({
+      minify: false,
+      inject: {
+        data: {
+          injectScript: staticTargets
+            .map((target) => {
+              return `<script src="${baseUrl}${target.dest}/${target.rename}"></script>`;
+            })
+            .join("\n"),
+        },
+      },
+    }),
+  ];
+};
--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,12 @@ import VueJsx from "@vitejs/plugin-vue-jsx";
 import VueSetupExtend from "vite-plugin-vue-setup-extend";
 import Compression from "vite-compression-plugin";
 import { VitePWA } from "vite-plugin-pwa";
-import { viteExternalsPlugin as ViteExternals } from "vite-plugin-externals";
-import { viteStaticCopy as ViteStaticCopy } from "vite-plugin-static-copy";
-import { createHtmlPlugin as VitePluginHtml } from "vite-plugin-html";
 import Icons from "unplugin-icons/vite";
-import randomstring from "randomstring";
+import { setupLibraryExternal } from "./src/build/library-external";
 
 export default ({ mode }: { mode: string }) => {
   const env = loadEnv(mode, process.cwd(), "");
   const isProduction = mode === "production";
-
-  const staticSuffix = randomstring.generate({
-    length: 8,
-    charset: "hex",
-  });
 
   return defineConfig({
     base: env.VITE_BASE_URL,
@@ -28,53 +20,6 @@ export default ({ mode }: { mode: string }) => {
       VueSetupExtend(),
       Compression(),
       Icons({ compiler: "vue3" }),
-      ViteExternals({
-        vue: "Vue",
-        "vue-router": "VueRouter",
-        "@halo-dev/shared": "HaloAdminShared",
-        "@halo-dev/components": "HaloComponents",
-      }),
-      ViteStaticCopy({
-        targets: [
-          {
-            src: `./node_modules/vue/dist/vue.global${
-              isProduction ? ".prod" : ""
-            }.js`,
-            dest: "assets/vue",
-            rename: `vue.global.${staticSuffix}.js`,
-          },
-          {
-            src: `./node_modules/vue-router/dist/vue-router.global${
-              isProduction ? ".prod" : ""
-            }.js`,
-            dest: "assets/vue-router",
-            rename: `vue-router.global.${staticSuffix}.js`,
-          },
-          {
-            src: "./node_modules/@halo-dev/admin-shared/dist/halo-admin-shared.iife.js",
-            dest: "assets/admin-shared",
-            rename: `halo-admin-shared.iife.${staticSuffix}.js`,
-          },
-          {
-            src: "./node_modules/@halo-dev/components/dist/halo-components.iife.js",
-            dest: "assets/components",
-            rename: `halo-components.iife.${staticSuffix}.js`,
-          },
-        ],
-      }),
-      VitePluginHtml({
-        minify: false,
-        inject: {
-          data: {
-            injectScript: [
-              `<script src="${env.VITE_BASE_URL}assets/vue/vue.global.${staticSuffix}.js"></script>`,
-              `<script src="${env.VITE_BASE_URL}assets/vue-router/vue-router.global.${staticSuffix}.js"></script>`,
-              `<script src="${env.VITE_BASE_URL}assets/components/halo-components.iife.${staticSuffix}.js"></script>`,
-              `<script src="${env.VITE_BASE_URL}assets/admin-shared/halo-admin-shared.iife.${staticSuffix}.js"></script>`,
-            ].join("\n"),
-          },
-        },
-      }),
       VitePWA({
         manifest: {
           name: "Halo",
@@ -84,6 +29,7 @@ export default ({ mode }: { mode: string }) => {
         },
         disable: true,
       }),
+      ...setupLibraryExternal(isProduction, env.VITE_BASE_URL),
     ],
     resolve: {
       alias: {
