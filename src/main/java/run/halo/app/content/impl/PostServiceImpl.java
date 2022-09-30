95bae63e8fe42824c39390460e6a7c359c1948be
guqing <i@guqing.email>
2022-09-30 17:12:51 +0800

    fix: total comment count for page and singlepage

--- src/main/java/run/halo/app/content/impl/PostServiceImpl.java
+++ src/main/java/run/halo/app/content/impl/PostServiceImpl.java
@@ -82,7 +82,7 @@ public class PostServiceImpl implements PostService {
         return Stats.builder()
             .visit(counter.getVisit())
             .upvote(counter.getUpvote())
-            .totalComment(counter.getApprovedComment())
+            .totalComment(counter.getTotalComment())
             .approvedComment(counter.getApprovedComment())
             .build();
     }
--- src/main/java/run/halo/app/content/impl/SinglePageServiceImpl.java
+++ src/main/java/run/halo/app/content/impl/SinglePageServiceImpl.java
@@ -211,7 +211,7 @@ public class SinglePageServiceImpl implements SinglePageService {
         return Stats.builder()
             .visit(counter.getVisit())
             .upvote(counter.getUpvote())
-            .totalComment(counter.getApprovedComment())
+            .totalComment(counter.getTotalComment())
             .approvedComment(counter.getApprovedComment())
             .build();
     }
