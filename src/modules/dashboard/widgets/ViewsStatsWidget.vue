9c6bda84d322dca967376318fcfc342db727f4d4
Ryan Wang <i@ryanc.cc>
2022-09-30 16:06:18 +0800

    feat: support for displaying site content statistics (halo-dev/console#624)
    #### What type of PR is this?
    
    /kind feature
    /milestone 2.0
    
    #### What this PR does / why we need it:
    
    - 支持在文章和自定义页面的管理列表显示访问数和评论数。
    - 仪表盘支持显示整站的访问数。
    
    适配 https://github.com/halo-dev/halo/pull/2476
    
    #### Which issue(s) this PR fixes:
    
    Fixes https://github.com/halo-dev/halo/issues/2430
    
    #### Special notes for your reviewer:
    
    /cc @halo-dev/sig-halo-console 
    
    测试方式：
    
    1. Halo 需要 https://github.com/halo-dev/halo/pull/2476 PR 中的分支。
    2. 需要执行 `pnpm install && pnpm build:packages`
    3. 随机访问前台文章或者自定义页面，以及添加评论。
    4. 查看后台是否正确统计文章浏览数和评论数。
    
    TODO list:
    
    - [ ] 更新 `@halo-dev/api-client`
    
    #### Does this PR introduce a user-facing change?
    
    ```release-note
    支持在文章和自定义页面的管理列表显示访问数和评论数
    ```

--- src/modules/dashboard/widgets/ViewsStatsWidget.vue
+++ src/modules/dashboard/widgets/ViewsStatsWidget.vue
@@ -1,9 +1,25 @@
 <script lang="ts" name="ViewsStatsWidget" setup>
+import { apiClient } from "@/utils/api-client";
+import type { Stats } from "@halo-dev/api-client";
 import { VCard } from "@halo-dev/components";
+import { onMounted, ref } from "vue";
+
+const stats = ref<Stats>({
+  visits: 0,
+});
+
+const handleFetchStats = async () => {
+  const { data } = await apiClient.stats.getStats();
+  stats.value = data;
+};
+
+onMounted(handleFetchStats);
 </script>
 <template>
   <VCard class="h-full">
     <dt class="truncate text-sm font-medium text-gray-500">浏览量</dt>
-    <dd class="mt-1 text-3xl font-semibold text-gray-900">123,321</dd>
+    <dd class="mt-1 text-3xl font-semibold text-gray-900">
+      {{ stats.visits || 0 }}
+    </dd>
   </VCard>
 </template>
