b092b390b7d19a4dd7296de87474753ad2014e04
johnniang <johnniang@fastmail.com>
2022-10-10 17:48:25 +0800

    Refactor form login
    Signed-off-by: johnniang <johnniang@fastmail.com>

--- src/main/java/run/halo/app/security/DefaultUserDetailService.java
+++ src/main/java/run/halo/app/security/DefaultUserDetailService.java
@@ -1,5 +1,9 @@
 package run.halo.app.security;
 
+import static run.halo.app.core.extension.User.GROUP;
+import static run.halo.app.core.extension.User.KIND;
+
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsPasswordService;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
@@ -11,6 +15,7 @@ import run.halo.app.core.extension.RoleBinding.Subject;
 import run.halo.app.core.extension.service.RoleService;
 import run.halo.app.core.extension.service.UserService;
 import run.halo.app.extension.GroupKind;
+import run.halo.app.extension.exception.ExtensionNotFoundException;
 
 public class DefaultUserDetailService
     implements ReactiveUserDetailsService, ReactiveUserDetailsPasswordService {
@@ -32,20 +37,21 @@ public class DefaultUserDetailService
 
     @Override
     public Mono<UserDetails> findByUsername(String username) {
-        return userService.getUser(username).flatMap(user -> {
-            final var userGvk =
-                new run.halo.app.core.extension.User().groupVersionKind();
-            var subject = new Subject(userGvk.kind(), username, userGvk.group());
-            return roleService.listRoleRefs(subject)
-                .filter(this::isRoleRef)
-                .map(RoleRef::getName)
-                .collectList()
-                .map(roleNames -> User.builder()
-                    .username(username)
-                    .password(user.getSpec().getPassword())
-                    .roles(roleNames.toArray(new String[0]))
-                    .build());
-        });
+        return userService.getUser(username)
+            .flatMap(user -> {
+                var subject = new Subject(KIND, username, GROUP);
+                return roleService.listRoleRefs(subject)
+                    .filter(this::isRoleRef)
+                    .map(RoleRef::getName)
+                    .collectList()
+                    .map(roleNames -> User.builder()
+                        .username(username)
+                        .password(user.getSpec().getPassword())
+                        .roles(roleNames.toArray(new String[0]))
+                        .build());
+            })
+            .onErrorMap(ExtensionNotFoundException.class,
+                e -> new BadCredentialsException("Invalid Credentials"));
     }
 
     private boolean isRoleRef(RoleRef roleRef) {
