f1ab2d9a8f804c4e985dea29b2bb06f91685469c
johnniang <johnniang@fastmail.com>
2022-10-11 16:01:12 +0800

    Support proxying console in server side
    Signed-off-by: johnniang <johnniang@fastmail.com>

--- src/main/java/run/halo/app/infra/properties/ConsoleProperties.java
+++ src/main/java/run/halo/app/infra/properties/ConsoleProperties.java
@@ -1,5 +1,7 @@
 package run.halo.app.infra.properties;
 
+import jakarta.validation.Valid;
+import java.net.URI;
 import lombok.Data;
 
 @Data
@@ -7,4 +9,20 @@ public class ConsoleProperties {
 
     private String location = "classpath:/console/";
 
+    @Valid
+    private ProxyProperties proxy = new ProxyProperties();
+
+    @Data
+    public static class ProxyProperties {
+
+        /**
+         * Console endpoint in development environment to be proxied. e.g.: http://localhost:8090/
+         */
+        private URI endpoint;
+
+        /**
+         * Indicates if the proxy behaviour is enabled. Default is false
+         */
+        private boolean enabled = false;
+    }
 }
