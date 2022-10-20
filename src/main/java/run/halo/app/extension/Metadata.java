0f9c73ac8e6eb96c6fe9cb9b971243bd7149d984
John Niang <johnniang@fastmail.com>
2022-10-20 11:48:14 +0800

    Make field metadata.name of extension required (#2608)
    #### What type of PR is this?
    
    /kind improvement
    /area core
    
    #### What this PR does / why we need it:
    
    Remove custom schema validation and make field metadata.name of extension required. So that the API client generated by `openapi-gen` will be more consistent than before.
    
    #### Does this PR introduce a user-facing change?
    
    ```release-note
    None
    ```

--- src/main/java/run/halo/app/extension/Metadata.java
+++ src/main/java/run/halo/app/extension/Metadata.java
@@ -1,8 +1,5 @@
 package run.halo.app.extension;
 
-import io.swagger.v3.oas.annotations.extensions.Extension;
-import io.swagger.v3.oas.annotations.extensions.ExtensionProperty;
-import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.Instant;
 import java.util.Map;
 import java.util.Set;
@@ -16,11 +13,6 @@ import lombok.EqualsAndHashCode;
  */
 @Data
 @EqualsAndHashCode
-@Schema(description = "Metadata information", extensions = {
-    @Extension(name = "x-validation", properties = {
-        @ExtensionProperty(name = "not-blank-at-least-one", value = "name, generateName")
-    })}
-)
 public class Metadata implements MetadataOperator {
 
     /**
