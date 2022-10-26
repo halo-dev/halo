d9cb6bf732187032a1e2df37a38453372e451c5d
John Niang <johnniang@fastmail.com>
2022-10-26 12:02:10 +0800

    Provide an endpoint to upgrade plugin (#2624)
    #### What type of PR is this?
    
    /kind feature
    /area core
    /milestone 2.0
    
    #### What this PR does / why we need it:
    
    Provide an endpoint to upgrade plugin by uploading a new jar file.
    
    #### Which issue(s) this PR fixes:
    
    Fixes https://github.com/halo-dev/halo/issues/2551
    
    #### Special notes for your reviewer:
    
    1. Install an old plugin
    2. Update the plugin and package again
    3. Upgrade the plugin and see the result
    
    #### Does this PR introduce a user-facing change?
    
    ```release-note
    提供插件更新功能
    ```

--- src/test/java/run/halo/app/infra/utils/FileUtilsTest.java
+++ src/test/java/run/halo/app/infra/utils/FileUtilsTest.java
@@ -1,8 +1,23 @@
 package run.halo.app.infra.utils;
 
+import static java.util.Objects.requireNonNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static run.halo.app.infra.utils.FileUtils.checkDirectoryTraversal;
+import static run.halo.app.infra.utils.FileUtils.deleteRecursivelyAndSilently;
+import static run.halo.app.infra.utils.FileUtils.jar;
+import static run.halo.app.infra.utils.FileUtils.unzip;
+import static run.halo.app.infra.utils.FileUtils.zip;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.zip.ZipInputStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import run.halo.app.infra.exception.AccessDeniedException;
@@ -29,5 +44,65 @@ class FileUtilsTest {
                 () -> checkDirectoryTraversal("/etc/", "/tmp"));
         }
 
+    }
+
+    @Nested
+    class ZipTest {
+
+        Path tempDirectory;
+
+        @BeforeEach
+        void setUp() throws IOException {
+            tempDirectory = Files.createTempDirectory("halo-test-fileutils-zip-");
+        }
+
+        @AfterEach
+        void cleanUp() {
+            deleteRecursivelyAndSilently(tempDirectory);
+        }
+
+        @Test
+        void zipFolderAndUnzip() throws IOException, URISyntaxException {
+            var uri = requireNonNull(getClass().getClassLoader().getResource("folder-to-zip"))
+                .toURI();
+            var zipPath = tempDirectory.resolve("example.zip");
+            zip(Paths.get(uri), zipPath);
+
+            var unzipTarget = tempDirectory.resolve("example-folder");
+            try (var zis = new ZipInputStream(Files.newInputStream(zipPath))) {
+                unzip(zis, unzipTarget);
+            }
+
+            var content = Files.readString(unzipTarget.resolve("examplefile"));
+            assertEquals("Here is an example file.\n", content);
+        }
+
+        @Test
+        void jarFolderAndUnzip() throws IOException, URISyntaxException {
+            var uri = requireNonNull(getClass().getClassLoader().getResource("folder-to-zip"))
+                .toURI();
+            var zipPath = tempDirectory.resolve("example.zip");
+            jar(Paths.get(uri), zipPath);
+
+            var unzipTarget = tempDirectory.resolve("example-folder");
+            try (var zis = new ZipInputStream(Files.newInputStream(zipPath))) {
+                unzip(zis, unzipTarget);
+            }
+
+            var content = Files.readString(unzipTarget.resolve("examplefile"));
+            assertEquals("Here is an example file.\n", content);
+        }
+
+        @Test
+        void zipFolderIfNoSuchFolder() {
+            assertThrows(NoSuchFileException.class, () ->
+                zip(Paths.get("no-such-folder"), tempDirectory.resolve("example.zip")));
+        }
+
+        @Test
+        void jarFolderIfNoSuchFolder() {
+            assertThrows(NoSuchFileException.class, () ->
+                jar(Paths.get("no-such-folder"), tempDirectory.resolve("example.zip")));
+        }
     }
 }
