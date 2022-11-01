c3a3281fcce6a6fcd1722e3add3b47121c830b32
guqing <38999863+guqing@users.noreply.github.com>
2022-11-01 21:12:16 +0800

    fix: labels update for post and singlepage (#2648)
    #### What type of PR is this?
    /kind bug
    /area core
    /milestone 2.0
    #### What this PR does / why we need it:
    修复文章和自定义页面的 labels 在文章标记为删除状态时没有更新的问题
    #### Which issue(s) this PR fixes:
    A part of  #2647
    
    #### Special notes for your reviewer:
    how to test it?
    1. 创建文章
    2. 由于没有发布，此时访问文章的 permalink 访问不到
    3. 发布文章，可以正常访问
    4. 更新文章的 `spec.deleted=true`,在访问文章期望404，并且 `metadata.labels` 中的数据正确
    5. 自定义页面亦如是
    
    /cc @halo-dev/sig-halo 
    #### Does this PR introduce a user-facing change?
    
    ```release-note
    None
    ```

--- src/main/java/run/halo/app/content/permalinks/PostPermalinkPolicy.java
+++ src/main/java/run/halo/app/content/permalinks/PostPermalinkPolicy.java
@@ -5,6 +5,7 @@ import java.text.NumberFormat;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.Objects;
 import java.util.Properties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
@@ -53,6 +54,10 @@ public class PostPermalinkPolicy implements PermalinkPolicy<Post>, PermalinkWatc
 
     @Override
     public void onPermalinkAdd(Post post) {
+        if (!post.isPublished() || Objects.equals(true, post.getSpec().getDeleted())) {
+            return;
+        }
+        // publish when post is published and not deleted
         applicationContext.publishEvent(new PermalinkIndexAddCommand(this, getLocator(post),
             post.getStatusOrDefault().getPermalink()));
     }
