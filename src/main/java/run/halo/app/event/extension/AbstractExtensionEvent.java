410c9b7cecafda1021795daf38ee4c2d0373fdeb
guqing <i@guqing.email>
2022-11-16 17:29:27 +0800

    refactor: remove SharedEvent annotation

--- src/main/java/run/halo/app/event/extension/AbstractExtensionEvent.java
+++ src/main/java/run/halo/app/event/extension/AbstractExtensionEvent.java
@@ -2,17 +2,13 @@ package run.halo.app.event.extension;
 
 import org.springframework.context.ApplicationEvent;
 import run.halo.app.extension.Extension;
-import run.halo.app.plugin.SharedEvent;
 
 /**
  * <p>Events produced during changing an {@link Extension}.</p>
- * This event is marked with {@link SharedEvent} annotation and will be propagated to the plugin
- * when the event is triggered.
  *
  * @author guqing
  * @since 2.0.0
  */
-@SharedEvent
 public abstract class AbstractExtensionEvent extends ApplicationEvent {
 
     private final Extension extension;
