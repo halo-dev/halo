6d22f0f81e6b0dba7f2c6174edef90a8e93d22c9
John Niang <johnniang@fastmail.com>
2022-11-28 16:39:26 +0800

    Manage PostEventReconciler by self

--- src/main/java/run/halo/app/search/post/PostEventReconciler.java
+++ src/main/java/run/halo/app/search/post/PostEventReconciler.java
@@ -5,12 +5,11 @@ import java.time.Instant;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CountDownLatch;
-import org.springframework.beans.factory.DisposableBean;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.SmartLifecycle;
 import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import reactor.core.Exceptions;
-import run.halo.app.event.post.PostDeletedEvent;
 import run.halo.app.event.post.PostEvent;
 import run.halo.app.event.post.PostPublishedEvent;
 import run.halo.app.event.post.PostRecycledEvent;
@@ -21,12 +20,12 @@ import run.halo.app.extension.controller.DefaultController;
 import run.halo.app.extension.controller.DefaultDelayQueue;
 import run.halo.app.extension.controller.Reconciler;
 import run.halo.app.extension.controller.RequestQueue;
-import run.halo.app.infra.SchemeInitializedEvent;
 import run.halo.app.plugin.extensionpoint.ExtensionGetter;
 import run.halo.app.theme.finders.PostFinder;
 
+@Slf4j
 @Component
-public class PostEventListener implements DisposableBean {
+public class PostEventReconciler implements Reconciler<PostEvent>, SmartLifecycle {
 
     private final ExtensionGetter extensionGetter;
 
@@ -37,73 +36,59 @@ public class PostEventListener implements DisposableBean {
 
     private final Controller postEventController;
 
-    public PostEventListener(ExtensionGetter extensionGetter,
+    private boolean running = false;
+
+    public PostEventReconciler(ExtensionGetter extensionGetter,
         PostFinder postFinder) {
         this.extensionGetter = extensionGetter;
         this.postFinder = postFinder;
 
         postEventQueue = new DefaultDelayQueue<>(Instant::now);
-        postEventController = new PostEventReconciler().setupWith(null);
-    }
-
-    class PostEventReconciler implements Reconciler<PostEvent> {
-
-        @Override
-        public Result reconcile(PostEvent postEvent) {
-            if (postEvent instanceof PostPublishedEvent) {
-                try {
-                    addPostDoc(postEvent.getName());
-                } catch (InterruptedException e) {
-                    throw Exceptions.propagate(e);
-                }
-            }
-            if (postEvent instanceof PostUnpublishedEvent
-                || postEvent instanceof PostDeletedEvent) {
-                try {
-                    deletePostDoc(postEvent.getName());
-                } catch (InterruptedException e) {
-                    throw Exceptions.propagate(e);
-                }
-            }
-            return null;
-        }
-
-        @Override
-        public Controller setupWith(ControllerBuilder builder) {
-            return new DefaultController<>(
-                this.getClass().getName(),
-                this,
-                postEventQueue,
-                null,
-                Duration.ofMillis(100),
-                Duration.ofSeconds(1000)
-            );
-        }
+        postEventController = this.setupWith(null);
     }
 
     @Override
-    public void destroy() throws Exception {
-        postEventController.dispose();
+    public Result reconcile(PostEvent postEvent) {
+        if (postEvent instanceof PostPublishedEvent) {
+            try {
+                addPostDoc(postEvent.getName());
+            } catch (InterruptedException e) {
+                throw Exceptions.propagate(e);
+            }
+        }
+        if (postEvent instanceof PostUnpublishedEvent
+            || postEvent instanceof PostRecycledEvent) {
+            try {
+                deletePostDoc(postEvent.getName());
+            } catch (InterruptedException e) {
+                throw Exceptions.propagate(e);
+            }
+        }
+        return null;
     }
 
-    @EventListener(SchemeInitializedEvent.class)
-    public void handleSchemeInitializedEvent(SchemeInitializedEvent event) {
-        postEventController.start();
+    @Override
+    public Controller setupWith(ControllerBuilder builder) {
+        return new DefaultController<>(
+            this.getClass().getName(),
+            this,
+            postEventQueue,
+            null,
+            Duration.ofMillis(100),
+            Duration.ofSeconds(1000)
+        );
     }
 
-    @Async
     @EventListener(PostPublishedEvent.class)
     public void handlePostPublished(PostPublishedEvent publishedEvent) {
         postEventQueue.addImmediately(publishedEvent);
     }
 
-    @Async
     @EventListener(PostUnpublishedEvent.class)
     public void handlePostUnpublished(PostUnpublishedEvent unpublishedEvent) {
         postEventQueue.addImmediately(unpublishedEvent);
     }
 
-    @Async
     @EventListener(PostRecycledEvent.class)
     public void handlePostRecycled(PostRecycledEvent recycledEvent) {
         postEventQueue.addImmediately(recycledEvent);
@@ -123,7 +108,10 @@ public class PostEventListener implements DisposableBean {
                 })
             )
             .doFinally(signalType -> latch.countDown())
-            .subscribe();
+            .subscribe(service -> {
+            }, throwable -> {
+                throw Exceptions.propagate(throwable);
+            });
         try {
             latch.await();
         } finally {
@@ -142,11 +130,31 @@ public class PostEventListener implements DisposableBean {
                 }
             })
             .doFinally(signalType -> latch.countDown())
-            .subscribe();
+            .subscribe(service -> {
+            }, throwable -> {
+                throw Exceptions.propagate(throwable);
+            });
         try {
             latch.await();
         } finally {
             disposable.dispose();
         }
+    }
+
+    @Override
+    public void start() {
+        postEventController.start();
+        running = true;
+    }
+
+    @Override
+    public void stop() {
+        running = false;
+        postEventController.dispose();
+    }
+
+    @Override
+    public boolean isRunning() {
+        return running;
     }
 }
