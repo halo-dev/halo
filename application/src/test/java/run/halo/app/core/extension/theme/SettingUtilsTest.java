31e5014decf3933443f60e7b133e542ee7c15724
guqing <38999863+guqing@users.noreply.github.com>
2023-03-30 16:34:14 +0800

    refactor: merge patch default values to the existing config for theme and plugin setting (#3616)
    #### What type of PR is this?
    /kind improvement
    /area core
    /milestone 2.4.x
    
    #### What this PR does / why we need it:
    修复升级主题或插件时新增加的配置默认值没有更新的问题
    
    how to test it?
    1. 安装包含设置的主题后获取主题的 ConfigMap 并记下
    2. 修改（增加/更新）主题的设置（Setting）的默认值，模拟更新主题
    3. 期望默认值改变不会影响到之前已有的配置，对于新增加的配置的默认值会被合并到已有的 ConfigMap 中
    
    #### Which issue(s) this PR fixes:
    Fixes #3446
    
    #### Does this PR introduce a user-facing change?
    ```release-note
    修复升级主题或插件时新增加的配置默认值没有更新的问题
    ```

--- application/src/test/java/run/halo/app/core/extension/theme/SettingUtilsTest.java
+++ application/src/test/java/run/halo/app/core/extension/theme/SettingUtilsTest.java
@@ -1,5 +1,8 @@
 package run.halo.app.core.extension.theme;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Map;
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
@@ -25,6 +28,73 @@ class SettingUtilsTest {
                 """,
             JsonUtils.objectToJson(map),
             true);
+    }
+
+    @Test
+    void mergePatch() throws JSONException {
+        Map<String, String> defaultValue =
+            Map.of("comment", "{\"enable\":true,\"requireReviewForNew\":true}",
+                "basic", "{\"title\":\"guqing's blog\"}",
+                "authProvider", "{\"github\":{\"clientId\":\"fake-client-id\"}}");
+        Map<String, String> modified = Map.of("comment",
+            "{\"enable\":true,\"requireReviewForNew\":true,\"systemUserOnly\":false}",
+            "basic", "{\"title\":\"guqing's blog\", \"subtitle\": \"fake-sub-title\"}");
+
+        Map<String, String> result = SettingUtils.mergePatch(modified, defaultValue);
+        Map<String, String> excepted = Map.of("comment",
+            "{\"enable\":true,\"requireReviewForNew\":true,\"systemUserOnly\":false}",
+            "basic", "{\"title\":\"guqing's blog\",\"subtitle\":\"fake-sub-title\"}",
+            "authProvider", "{\"github\":{\"clientId\":\"fake-client-id\"}}");
+        JSONAssert.assertEquals(JsonUtils.objectToJson(excepted), JsonUtils.objectToJson(result),
+            true);
+    }
+
+    @Test
+    void mergePatchWithMoreType() throws JSONException {
+        Map<String, String> defaultValue = Map.of(
+            "array", "[1,2,3]",
+            "number", "1",
+            "boolean", "false",
+            "string", "new-default-string-value",
+            "object", "{\"name\":\"guqing\"}"
+        );
+        Map<String, String> modified = Map.of(
+            "stringArray", "[\"hello\", \"world\"]",
+            "boolean", "true",
+            "string", "hello",
+            "object", "{\"name\":\"guqing\", \"age\": 18}"
+        );
+        Map<String, String> result = SettingUtils.mergePatch(modified, defaultValue);
+        Map<String, String> excepted = Map.of(
+            "array", "[1,2,3]",
+            "number", "1",
+            "boolean", "true",
+            "string", "hello",
+            "object", "{\"name\":\"guqing\",\"age\":18}",
+            "stringArray", "[\"hello\",\"world\"]"
+        );
+        JSONAssert.assertEquals(JsonUtils.objectToJson(excepted), JsonUtils.objectToJson(result),
+            true);
+    }
+
+    @Test
+    void isJson() {
+        assertThat(SettingUtils.isJson("[1,2,3]")).isTrue();
+        assertThat(SettingUtils.isJson("[\"hello\"]")).isTrue();
+        assertThat(SettingUtils.isJson("{\"name\":\"guqing\",\"age\":18}")).isTrue();
+        assertThat(SettingUtils.isJson("{ \"flag\":true }")).isTrue();
+        assertThat(SettingUtils.isJson("""
+            [
+                { "K1": "value-1", "K2":"value1-2" }
+            ]
+            """)).isTrue();
+        assertThat(SettingUtils.isJson("""
+            {
+                "sites": [{ "name":"halo" , "url":"halo.run" }]
+            }
+            """)).isTrue();
+        assertThat(SettingUtils.isJson("{\"name\":\"guqing\"")).isFalse();
+        assertThat(SettingUtils.isJson("hello")).isFalse();
     }
 
     private static Setting getFakeSetting() {
