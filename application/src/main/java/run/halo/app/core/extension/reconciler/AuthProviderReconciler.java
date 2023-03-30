ba5a38e024e90e4103935d31e34e565b2218158f
guqing <1484563614@qq.com>
2023-03-30 23:58:27 +0800

    refactor: do not disable when auth provider deleted

--- application/src/main/java/run/halo/app/core/extension/reconciler/AuthProviderReconciler.java
+++ application/src/main/java/run/halo/app/core/extension/reconciler/AuthProviderReconciler.java
@@ -1,7 +1,5 @@
 package run.halo.app.core.extension.reconciler;
 
-import java.util.HashSet;
-import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.BooleanUtils;
 import org.springframework.stereotype.Component;
@@ -22,21 +20,13 @@ import run.halo.app.security.AuthProviderService;
 @Component
 @RequiredArgsConstructor
 public class AuthProviderReconciler implements Reconciler<Reconciler.Request> {
-    private static final String FINALIZER_NAME = "auth-provider-protection";
     private final ExtensionClient client;
     private final AuthProviderService authProviderService;
 
     @Override
     public Result reconcile(Request request) {
         client.fetch(AuthProvider.class, request.name())
-            .ifPresent(authProvider -> {
-                if (authProvider.getMetadata().getDeletionTimestamp() != null) {
-                    removeFinalizer(request.name());
-                    return;
-                }
-                addFinalizerIfNecessary(authProvider);
-                handlePrivileged(authProvider);
-            });
+            .ifPresent(this::handlePrivileged);
         return Result.doNotRetry();
     }
 
@@ -51,36 +41,6 @@ public class AuthProviderReconciler implements Reconciler<Reconciler.Request> {
         if (privileged(authProvider)) {
             authProviderService.enable(authProvider.getMetadata().getName()).block();
         }
-    }
-
-    private void addFinalizerIfNecessary(AuthProvider oldAuthProvider) {
-        Set<String> finalizers = oldAuthProvider.getMetadata().getFinalizers();
-        if (finalizers != null && finalizers.contains(FINALIZER_NAME)) {
-            return;
-        }
-        client.fetch(AuthProvider.class, oldAuthProvider.getMetadata().getName())
-            .ifPresent(authProvider -> {
-                Set<String> newFinalizers = authProvider.getMetadata().getFinalizers();
-                if (newFinalizers == null) {
-                    newFinalizers = new HashSet<>();
-                    authProvider.getMetadata().setFinalizers(newFinalizers);
-                }
-                newFinalizers.add(FINALIZER_NAME);
-                client.update(authProvider);
-            });
-    }
-
-    private void removeFinalizer(String authProviderName) {
-        client.fetch(AuthProvider.class, authProviderName)
-            .ifPresent(authProvider -> {
-                // Disable auth provider
-                authProviderService.disable(authProviderName).block();
-
-                if (authProvider.getMetadata().getFinalizers() != null) {
-                    authProvider.getMetadata().getFinalizers().remove(FINALIZER_NAME);
-                }
-                client.update(authProvider);
-            });
     }
 
     private boolean privileged(AuthProvider authProvider) {
