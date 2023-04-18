c798c3d7a8d1e654cb3dc0353b8df039d45a1202
John Niang <johnniang@fastmail.com>
2023-04-18 12:23:37 +0800

    Add property use-absolute-permalink to control permalink generation

--- application/src/main/java/run/halo/app/infra/HaloPropertiesExternalUrlSupplier.java
+++ application/src/main/java/run/halo/app/infra/HaloPropertiesExternalUrlSupplier.java
@@ -1,6 +1,10 @@
 package run.halo.app.infra;
 
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Component;
 import run.halo.app.infra.properties.HaloProperties;
 
@@ -20,6 +24,27 @@ public class HaloPropertiesExternalUrlSupplier implements ExternalUrlSupplier {
 
     @Override
     public URI get() {
-        return haloProperties.getExternalUrl();
+        if (!haloProperties.isUseAbsolutePermalink()) {
+            return URI.create("/");
+        }
+
+        try {
+            return haloProperties.getExternalUrl().toURI();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public URL getURL(HttpRequest request) {
+        var externalUrl = haloProperties.getExternalUrl();
+        if (externalUrl == null) {
+            try {
+                externalUrl = request.getURI().toURL();
+            } catch (MalformedURLException e) {
+                throw new RuntimeException("Cannot parse request URI to URL.", e);
+            }
+        }
+        return externalUrl;
     }
 }
--- application/src/test/java/run/halo/app/infra/HaloPropertiesExternalUrlSupplierTest.java
+++ application/src/test/java/run/halo/app/infra/HaloPropertiesExternalUrlSupplierTest.java
@@ -1,14 +1,17 @@
 package run.halo.app.infra;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.net.MalformedURLException;
 import java.net.URI;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpRequest;
 import run.halo.app.infra.properties.HaloProperties;
 
 @ExtendWith(MockitoExtension.class)
@@ -21,9 +24,37 @@ class HaloPropertiesExternalUrlSupplierTest {
     HaloPropertiesExternalUrlSupplier externalUrl;
 
     @Test
-    void get() {
-        URI fakeUri = URI.create("fake-url");
-        Mockito.when(haloProperties.getExternalUrl()).thenReturn(fakeUri);
+    void getURIWhenUsingAbsolutePermalink() throws MalformedURLException {
+        var fakeUri = URI.create("https://halo.run/fake");
+        when(haloProperties.getExternalUrl()).thenReturn(fakeUri.toURL());
+        when(haloProperties.isUseAbsolutePermalink()).thenReturn(true);
+
         assertEquals(fakeUri, externalUrl.get());
+    }
+
+    @Test
+    void getURIWhenUsingRelativePermalink() throws MalformedURLException {
+        when(haloProperties.isUseAbsolutePermalink()).thenReturn(false);
+
+        assertEquals(URI.create("/"), externalUrl.get());
+    }
+
+    @Test
+    void getURLWhenExternalURLProvided() throws MalformedURLException {
+        var fakeUri = URI.create("https://halo.run/fake");
+        when(haloProperties.getExternalUrl()).thenReturn(fakeUri.toURL());
+        var mockRequest = mock(HttpRequest.class);
+        var url = externalUrl.getURL(mockRequest);
+        assertEquals(fakeUri.toURL(), url);
+    }
+
+    @Test
+    void getURLWhenExternalURLAbsent() throws MalformedURLException {
+        var fakeUri = URI.create("https://localhost/fake");
+        when(haloProperties.getExternalUrl()).thenReturn(null);
+        var mockRequest = mock(HttpRequest.class);
+        when(mockRequest.getURI()).thenReturn(fakeUri);
+        var url = externalUrl.getURL(mockRequest);
+        assertEquals(fakeUri.toURL(), url);
     }
 }
