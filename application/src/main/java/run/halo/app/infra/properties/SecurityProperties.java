c0f1813767a0523b900bcd83bf3bf8c70cf4f253
guqing <i@guqing.email>
2023-04-19 19:10:27 +0800

    refactor: validation for super admin username

--- application/src/main/java/run/halo/app/infra/properties/SecurityProperties.java
+++ application/src/main/java/run/halo/app/infra/properties/SecurityProperties.java
@@ -2,12 +2,12 @@ package run.halo.app.infra.properties;
 
 import static org.springframework.security.web.server.header.ReferrerPolicyServerHttpHeadersWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.springframework.lang.NonNull;
 import org.springframework.security.web.server.header.ReferrerPolicyServerHttpHeadersWriter.ReferrerPolicy;
 import org.springframework.security.web.server.header.XFrameOptionsServerHttpHeadersWriter.Mode;
+import org.springframework.validation.Errors;
+import run.halo.app.infra.ValidationUtils;
 
 @Data
 public class SecurityProperties {
@@ -38,19 +38,18 @@ public class SecurityProperties {
 
         private boolean disabled;
 
-        @NotBlank(message = "Super administrator username must not be blank")
-        @Pattern(regexp = "^[a-z0-9]([-a-z0-9]*[a-z0-9])?(\\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*$",
-            message = """
-                Super administrator username must be a valid subdomain name, the name must:
-                1. contain no more than 63 characters
-                2. contain only lowercase alphanumeric characters, '-' or '.'
-                3. start with an alphanumeric character
-                4. end with an alphanumeric character
-                """)
-        @Size(max = 63, message = "Must be less than {max} characters")
         private String superAdminUsername = "admin";
 
         private String superAdminPassword;
-    }
 
+        static void validateUsername(@NonNull Initializer initializer, @NonNull Errors errors) {
+            if (initializer.isDisabled() || ValidationUtils.validateName(
+                initializer.getSuperAdminUsername())) {
+                return;
+            }
+            errors.rejectValue("security.initializer.superAdminUsername",
+                "initializer.superAdminUsername.invalid",
+                ValidationUtils.NAME_VALIDATION_MESSAGE);
+        }
+    }
 }
