07f709ff07fc215ae7284ffab9d4210e29400e0e
Ryan Wang <i@ryanc.cc>
2023-04-19 18:32:24 +0800

    feat: add description component
    Signed-off-by: Ryan Wang <i@ryanc.cc>

---
+++ console/packages/components/src/components/description/DescriptionItem.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+withDefaults(
+  defineProps<{
+    label: string;
+    description?: string;
+  }>(),
+  {
+    description: undefined,
+  }
+);
+</script>
+
+<template>
+  <div
+    class="bg-white px-4 py-5 hover:bg-gray-50 sm:grid sm:grid-cols-6 sm:gap-4 sm:px-6"
+  >
+    <dt class="text-sm font-medium text-gray-900">{{ label }}</dt>
+    <dd class="mt-1 text-sm text-gray-900 sm:col-span-3 sm:mt-0">
+      <slot v-if="$slots.default" />
+      <template v-else>
+        {{ description }}
+      </template>
+    </dd>
+  </div>
+</template>
