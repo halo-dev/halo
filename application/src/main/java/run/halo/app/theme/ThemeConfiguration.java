7ab47e3ba6300a9f432e436f76b2fd160546ccc7
guqing <i@guqing.email>
2023-06-14 14:48:09 +0800

    refactor: theme assert route

--- application/src/main/java/run/halo/app/theme/ThemeConfiguration.java
+++ application/src/main/java/run/halo/app/theme/ThemeConfiguration.java
@@ -20,7 +20,7 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import org.thymeleaf.extras.springsecurity6.dialect.SpringSecurityDialect;
 import reactor.core.publisher.Mono;
 import run.halo.app.infra.ThemeRootGetter;
-import run.halo.app.infra.exception.NotFoundException;
+import run.halo.app.infra.utils.FileUtils;
 import run.halo.app.theme.dialect.HaloSpringSecurityDialect;
 import run.halo.app.theme.dialect.LinkExpressionObjectDialect;
 
@@ -73,9 +73,7 @@ public class ThemeConfiguration {
             .resolve("assets");
         String safeResourcePath = org.springframework.util.StringUtils.cleanPath(resource);
         Path result = basePath.resolve(safeResourcePath).normalize();
-        if (!result.startsWith(basePath)) {
-            throw new NotFoundException("Resource not found.");
-        }
+        FileUtils.checkDirectoryTraversal(basePath, result);
         return result;
     }
 
