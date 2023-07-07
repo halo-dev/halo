c0aae3a63c0d252f57919f03521636316638a742
Ryan Wang <i@ryanc.cc>
2023-07-07 12:38:11 +0800

    feat: make plugin tabs extensible (#4041)
    #### What type of PR is this?
    
    /area console
    /kind feature
    /milestone 2.7.x
    
    #### What this PR does / why we need it:
    
    插件自身的详情页面中的 Tabs 选项卡支持拓展，允许开发者自行为插件编写设置界面、一些不常用的操作页面等。
    
    示例：
    
    <img width="1358" alt="image" src="https://github.com/halo-dev/halo/assets/21301288/1e414e4e-688d-47de-907f-5f2a0afa9350">
    
    扩展方式：
    
    参考文档：https://github.com/ruibaby/halo/blob/feat/plugin-tabs-extend/console/docs/extension-points/plugin-self-tabs.md
    
    #### Which issue(s) this PR fixes:
    
    Fixes https://github.com/halo-dev/halo/issues/3987
    
    #### Special notes for your reviewer:
    
    需要测试：
    
    1. 测试任意插件的详情页面是否能够正常使用。
    2. 测试任意插件的设置表单能否正常使用。
    3. 可以尝试根据文档为某个插件添加自定义的选项卡，测试是否能够正常工作。
    
    #### Does this PR introduce a user-facing change?
    
    ```release-note
    Console 端插件详情选项卡支持通过插件扩展。
    ```

--- console/packages/shared/src/index.ts
+++ console/packages/shared/src/index.ts
@@ -4,4 +4,5 @@ export * from "./core/plugins";
 export * from "./states/pages";
 export * from "./states/attachment-selector";
 export * from "./states/editor";
+export * from "./states/plugin-tab";
 export * from "./states/comment-subject-ref";
