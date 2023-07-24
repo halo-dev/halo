a8927f9ef71ed8778b5a4179e10d8baa37b3370c
Ryan Wang <i@ryanc.cc>
2023-07-24 16:13:19 +0800

    Add restore status
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- console/src/modules/system/backup/tabs/Restore.vue
+++ console/src/modules/system/backup/tabs/Restore.vue
@@ -1,10 +1,15 @@
 <script lang="ts" setup>
 import UppyUpload from "@/components/upload/UppyUpload.vue";
-import { Dialog, Toast, VAlert } from "@halo-dev/components";
+import { Dialog, Toast, VAlert, VLoading } from "@halo-dev/components";
+import { useQuery } from "@tanstack/vue-query";
 import axios from "axios";
+import { computed } from "vue";
+import { ref } from "vue";
 import { useI18n } from "vue-i18n";
 
 const { t } = useI18n();
+
+const complete = ref(false);
 
 const onUploaded = () => {
   Dialog.success({
@@ -24,34 +29,61 @@ const onUploaded = () => {
 async function handleShutdown() {
   await axios.post(`/actuator/shutdown`);
   Toast.success(t("core.backup.operations.shutdown.toast_success"));
+
+  setTimeout(() => {
+    complete.value = true;
+  }, 1000);
 }
+
+useQuery({
+  queryKey: ["check-health"],
+  queryFn: async () => {
+    const { data } = await axios.get("/actuator/health");
+    return data;
+  },
+  onSuccess(data) {
+    if (data.status === "UP") {
+      window.location.reload();
+    }
+  },
+  retry: true,
+  retryDelay: 2000,
+  enabled: computed(() => complete.value),
+});
 </script>
 
 <template>
-  <div class="px-4 py-3">
-    <VAlert :title="$t('core.common.text.tip')" :closable="false">
-      <template #description>
-        <ul>
-          <li>{{ $t("core.backup.restore.tips.first") }}</li>
-          <li>
-            {{ $t("core.backup.restore.tips.second") }}
-          </li>
-          <li>
-            {{ $t("core.backup.restore.tips.third") }}
-          </li>
-        </ul>
-      </template>
-    </VAlert>
+  <div v-if="!complete">
+    <div class="px-4 py-3">
+      <VAlert :title="$t('core.common.text.tip')" :closable="false">
+        <template #description>
+          <ul>
+            <li>{{ $t("core.backup.restore.tips.first") }}</li>
+            <li>
+              {{ $t("core.backup.restore.tips.second") }}
+            </li>
+            <li>
+              {{ $t("core.backup.restore.tips.third") }}
+            </li>
+          </ul>
+        </template>
+      </VAlert>
+    </div>
+    <div class="flex items-center justify-center px-4 py-3">
+      <UppyUpload
+        :restrictions="{
+          maxNumberOfFiles: 1,
+          allowedFileTypes: ['.zip'],
+        }"
+        endpoint="/apis/api.console.migration.halo.run/v1alpha1/restorations"
+        width="100%"
+        @uploaded="onUploaded"
+      />
+    </div>
   </div>
-  <div class="flex items-center justify-center px-4 py-3">
-    <UppyUpload
-      :restrictions="{
-        maxNumberOfFiles: 1,
-        allowedFileTypes: ['.zip'],
-      }"
-      endpoint="/apis/api.console.migration.halo.run/v1alpha1/restorations"
-      width="100%"
-      @uploaded="onUploaded"
-    />
+
+  <div v-else class="flex h-72 flex-col items-center justify-center">
+    <VLoading />
+    <div class="text-xs text-gray-600">恢复完成，等待重启...</div>
   </div>
 </template>
