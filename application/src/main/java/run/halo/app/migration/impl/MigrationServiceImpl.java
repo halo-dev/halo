c80c5e23c63a03a53b50a547851419a4ce2129ee
John Niang <johnniang@fastmail.com>
2023-08-12 17:14:11 +0800

    Refactor the transformation between data buffers and input stream (#4391)
    #### What type of PR is this?
    
    /kind cleanup
    /area core
    /milestone 2.9.x
    
    #### What this PR does / why we need it:
    
    Before this, If we use a file with length less than 256KB for recovery, the process remains stagnant until we cancel the request.
    
    This PR refactors the transformation between data buffers and input stream and resolve the issue above. We should avoid returning InputStream directly in reactive stream.
    
    - DataBufferUtils before
    
        ```java
            public static InputStream toInputStream(Flux<DataBuffer> content) throws IOException {
                var pos = new PipedOutputStream();
                var pis = new PipedInputStream(pos);
                write(content, pos)
                    .doOnComplete(() -> {
                        try {
                            pos.close();
                        } catch (IOException ignored) {
                            // Ignore the error
                        }
                    })
                    .subscribeOn(Schedulers.boundedElastic())
                    .subscribe(releaseConsumer(), error -> {
                        if (error instanceof IOException) {
                            // Ignore the error
                            return;
                        }
                        log.error("Failed to write DataBuffer into OutputStream", error);
                    });
                return pis;
        ```
    
    - DataBufferUtils after
    
        ```java
            public static Mono<InputStream> toInputStream(Publisher<DataBuffer> content,
                Scheduler scheduler) {
                return Mono.create(sink -> {
                    try {
                        var pos = new PipedOutputStream();
                        var pis = new PipedInputStream(pos);
                        var disposable = write(content, pos)
                            .subscribeOn(scheduler)
                            .subscribe(releaseConsumer(), sink::error, () -> FileUtils.closeQuietly(pos),
                                Context.of(sink.contextView()));
                        sink.onDispose(disposable);
                        sink.success(pis);
                    } catch (IOException e) {
                        sink.error(e);
                    }
                });
        ```
    
    #### Special notes for your reviewer:
    
    Please test for plugins, themes and migrations.
    
    #### Does this PR introduce a user-facing change?
    
    ```release-note
    解决备份恢复时因文件小于 256KB 而导致接口卡住的问题。
    ```

--- application/src/main/java/run/halo/app/migration/impl/MigrationServiceImpl.java
+++ application/src/main/java/run/halo/app/migration/impl/MigrationServiceImpl.java
@@ -1,38 +1,37 @@
 package run.halo.app.migration.impl;
 
-import static org.springframework.core.io.buffer.DataBufferUtils.releaseConsumer;
-import static run.halo.app.infra.utils.FileUtils.closeQuietly;
+import static java.nio.file.Files.deleteIfExists;
+import static org.springframework.util.FileSystemUtils.copyRecursively;
+import static run.halo.app.infra.utils.FileUtils.checkDirectoryTraversal;
+import static run.halo.app.infra.utils.FileUtils.copyRecursively;
+import static run.halo.app.infra.utils.FileUtils.createTempDir;
 import static run.halo.app.infra.utils.FileUtils.deleteRecursivelyAndSilently;
+import static run.halo.app.infra.utils.FileUtils.unzip;
 
 import com.fasterxml.jackson.core.util.MinimalPrettyPrinter;
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import java.io.IOException;
-import java.io.PipedInputStream;
-import java.io.PipedOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Set;
-import java.util.zip.ZipInputStream;
 import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Publisher;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.FileSystemUtils;
 import org.springframework.web.server.ServerWebInputException;
 import reactor.core.Exceptions;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
-import reactor.util.context.Context;
 import run.halo.app.extension.store.ExtensionStore;
 import run.halo.app.extension.store.ExtensionStoreRepository;
 import run.halo.app.infra.exception.NotFoundException;
@@ -67,6 +66,8 @@ public class MigrationServiceImpl implements MigrationService {
 
     private final DateTimeFormatter dateTimeFormatter;
 
+    private final Scheduler scheduler = Schedulers.boundedElastic();
+
     public MigrationServiceImpl(ExtensionStoreRepository repository,
         HaloProperties haloProperties) {
         this.repository = repository;
@@ -94,55 +95,51 @@ public class MigrationServiceImpl implements MigrationService {
 
     @Override
     public Mono<Void> backup(Backup backup) {
-        try {
-            // create temporary folder to store all backup files into single files.
-            var tempDir = Files.createTempDirectory("halo-full-backup-");
-            return backupExtensions(tempDir)
-                .and(backupWorkDir(tempDir))
-                .and(packageBackup(tempDir, backup))
-                .doFinally(signalType -> deleteRecursivelyAndSilently(tempDir))
-                .subscribeOn(Schedulers.boundedElastic());
-        } catch (IOException e) {
-            return Mono.error(e);
-        }
+        return Mono.usingWhen(
+            createTempDir("halo-full-backup-", scheduler),
+            tempDir -> backupExtensions(tempDir)
+                .then(Mono.defer(() -> backupWorkDir(tempDir)))
+                .then(Mono.defer(() -> packageBackup(tempDir, backup))),
+            tempDir -> deleteRecursivelyAndSilently(tempDir, scheduler)
+        );
     }
 
     @Override
     public Mono<Resource> download(Backup backup) {
-        var status = backup.getStatus();
-        if (!Backup.Phase.SUCCEEDED.equals(status.getPhase()) || status.getFilename() == null) {
-            return Mono.error(new ServerWebInputException("Current backup is not downloadable."));
-        }
-
-        var backupFile = getBackupsRoot().resolve(status.getFilename());
-        var resource = new FileSystemResource(backupFile);
-        if (!resource.exists()) {
-            return Mono.error(new NotFoundException("problemDetail.migration.backup.notFound",
-                new Object[] {}, "Backup file doesn't exist or deleted."));
-        }
-        return Mono.just(resource);
+        return Mono.create(sink -> {
+            var status = backup.getStatus();
+            if (!Backup.Phase.SUCCEEDED.equals(status.getPhase()) || status.getFilename() == null) {
+                sink.error(new ServerWebInputException("Current backup is not downloadable."));
+                return;
+            }
+            var backupFile = getBackupsRoot().resolve(status.getFilename());
+            var resource = new FileSystemResource(backupFile);
+            if (!resource.exists()) {
+                sink.error(
+                    new NotFoundException("problemDetail.migration.backup.notFound",
+                        new Object[] {},
+                        "Backup file doesn't exist or deleted."));
+                return;
+            }
+            sink.success(resource);
+        });
     }
 
     @Override
     @Transactional
     public Mono<Void> restore(Publisher<DataBuffer> content) {
-        return Mono.defer(() -> {
-            try {
-                var tempDir = Files.createTempDirectory("halo-restore-");
-                return unpackBackup(content, tempDir)
-                    .and(restoreExtensions(tempDir))
-                    .and(restoreWorkdir(tempDir))
-                    .doFinally(signalType -> deleteRecursivelyAndSilently(tempDir))
-                    .subscribeOn(Schedulers.boundedElastic());
-            } catch (IOException e) {
-                return Mono.error(e);
-            }
-        });
+        return Mono.usingWhen(
+            createTempDir("halo-restore-", scheduler),
+            tempDir -> unpackBackup(content, tempDir)
+                .then(Mono.defer(() -> restoreExtensions(tempDir)))
+                .then(Mono.defer(() -> restoreWorkdir(tempDir))),
+            tempDir -> deleteRecursivelyAndSilently(tempDir, scheduler)
+        );
     }
 
     @Override
     public Mono<Void> cleanup(Backup backup) {
-        return Mono.<Void>fromRunnable(() -> {
+        return Mono.<Void>create(sink -> {
             var status = backup.getStatus();
             if (status == null || status.getFilename() == null) {
                 return;
@@ -151,110 +148,104 @@ public class MigrationServiceImpl implements MigrationService {
             var backupsRoot = getBackupsRoot();
             var backupFile = backupsRoot.resolve(filename);
             try {
-                FileUtils.checkDirectoryTraversal(backupsRoot, backupFile);
-                Files.deleteIfExists(backupFile);
-            } catch (IOException e) {
-                throw Exceptions.propagate(e);
-            }
-        }).subscribeOn(Schedulers.boundedElastic());
-    }
-
-    private Mono<Void> restoreWorkdir(Path backupRoot) {
-        return Mono.fromRunnable(() -> {
-            try {
-                var workdir = backupRoot.resolve("workdir");
-                if (Files.exists(workdir)) {
-                    FileSystemUtils.copyRecursively(workdir, haloProperties.getWorkDir());
-                }
-            } catch (IOException e) {
-                throw Exceptions.propagate(e);
-            }
-        });
-    }
-
-    private Mono<Void> restoreExtensions(Path backupRoot) {
-        var extensionsPath = backupRoot.resolve("extensions.data");
-        var reader = objectMapper.readerFor(ExtensionStore.class);
-        return Mono.<Void, MappingIterator<ExtensionStore>>using(
-            () -> reader.readValues(extensionsPath.toFile()),
-            itr -> Flux.<ExtensionStore>create(
-                    sink -> {
-                        while (itr.hasNext()) {
-                            sink.next(itr.next());
-                        }
-                        sink.complete();
-                    })
-                // reset version
-                .doOnNext(extensionStore -> extensionStore.setVersion(null))
-                .buffer(100)
-                // We might encounter OptimisticLockingFailureException when saving extension store,
-                // So we have to delete all extension stores before saving.
-                .flatMap(extensionStores -> repository.deleteAll(extensionStores)
-                    .thenMany(repository.saveAll(extensionStores)))
-                .doOnNext(extensionStore ->
-                    log.info("Restored extension store: {}", extensionStore.getName()))
-                .then(),
-            itr -> {
-                try {
-                    itr.close();
-                } catch (IOException e) {
-                    throw Exceptions.propagate(e);
-                }
-            });
-    }
-
-    private Mono<Void> unpackBackup(Publisher<DataBuffer> content, Path target) {
-        return Mono.create(sink -> {
-            try (var pipedIs = new PipedInputStream();
-                 var pipedOs = new PipedOutputStream(pipedIs);
-                 var zipIs = new ZipInputStream(pipedIs)) {
-                DataBufferUtils.write(content, pipedOs)
-                    .subscribe(
-                        releaseConsumer(),
-                        sink::error,
-                        () -> closeQuietly(pipedOs),
-                        Context.of(sink.contextView()));
-                FileUtils.unzip(zipIs, target);
+                checkDirectoryTraversal(backupsRoot, backupFile);
+                deleteIfExists(backupFile);
                 sink.success();
             } catch (IOException e) {
                 sink.error(e);
             }
-        });
+        }).subscribeOn(scheduler);
+    }
+
+    private Mono<Void> restoreWorkdir(Path backupRoot) {
+        return Mono.<Void>create(sink -> {
+            try {
+                var workdir = backupRoot.resolve("workdir");
+                if (Files.exists(workdir)) {
+                    copyRecursively(workdir, haloProperties.getWorkDir());
+                }
+                sink.success();
+            } catch (IOException e) {
+                sink.error(e);
+            }
+        }).subscribeOn(scheduler);
+    }
+
+    private Mono<Void> restoreExtensions(Path backupRoot) {
+        var extensionsPath = backupRoot.resolve("extensions.data");
+        if (Files.notExists(extensionsPath)) {
+            return Mono.empty();
+        }
+        var reader = objectMapper.readerFor(ExtensionStore.class);
+        return Mono.<Void, MappingIterator<ExtensionStore>>using(
+                () -> reader.readValues(extensionsPath.toFile()),
+                itr -> Flux.<ExtensionStore>create(
+                        sink -> {
+                            while (itr.hasNext()) {
+                                sink.next(itr.next());
+                            }
+                            sink.complete();
+                        })
+                    // reset version
+                    .doOnNext(extensionStore -> extensionStore.setVersion(null)).buffer(100)
+                    // We might encounter OptimisticLockingFailureException when saving extension
+                    // store,
+                    // So we have to delete all extension stores before saving.
+                    .flatMap(extensionStores -> repository.deleteAll(extensionStores)
+                        .thenMany(repository.saveAll(extensionStores))
+                    )
+                    .doOnNext(extensionStore -> log.info("Restored extension store: {}",
+                        extensionStore.getName()))
+                    .then(),
+                FileUtils::closeQuietly)
+            .subscribeOn(scheduler);
+    }
+
+    private Mono<Void> unpackBackup(Publisher<DataBuffer> content, Path target) {
+        return unzip(content, target, scheduler);
     }
 
     private Mono<Void> packageBackup(Path baseDir, Backup backup) {
-        return Mono.fromRunnable(() -> {
-            try {
-                var backupsFolder = getBackupsRoot();
-                Files.createDirectories(backupsFolder);
+        return Mono.fromCallable(
+                () -> {
+                    var backupsFolder = getBackupsRoot();
+                    Files.createDirectories(backupsFolder);
+                    return backupsFolder;
+                })
+            .<Void>handle((backupsFolder, sink) -> {
                 var backupName = backup.getMetadata().getName();
                 var startTimestamp = backup.getStatus().getStartTimestamp();
                 var timePart = this.dateTimeFormatter.format(startTimestamp);
                 var backupFile = backupsFolder.resolve(timePart + '-' + backupName + ".zip");
-                FileUtils.zip(baseDir, backupFile);
-                backup.getStatus().setFilename(backupFile.getFileName().toString());
-                backup.getStatus().setSize(Files.size(backupFile));
-            } catch (IOException e) {
-                throw Exceptions.propagate(e);
-            }
-        });
+                try {
+                    FileUtils.zip(baseDir, backupFile);
+                    backup.getStatus().setFilename(backupFile.getFileName().toString());
+                    backup.getStatus().setSize(Files.size(backupFile));
+                    sink.complete();
+                } catch (IOException e) {
+                    sink.error(e);
+                }
+            })
+            .subscribeOn(scheduler);
     }
 
     private Mono<Void> backupWorkDir(Path baseDir) {
-        return Mono.fromRunnable(() -> {
-            try {
-                var workdirPath = Files.createDirectory(baseDir.resolve("workdir"));
-                FileUtils.copyRecursively(haloProperties.getWorkDir(), workdirPath, excludes);
-            } catch (IOException e) {
-                throw Exceptions.propagate(e);
-            }
-        });
+        return Mono.fromCallable(() -> Files.createDirectory(baseDir.resolve("workdir")))
+            .<Void>handle((workdirPath, sink) -> {
+                try {
+                    copyRecursively(haloProperties.getWorkDir(), workdirPath, excludes);
+                    sink.complete();
+                } catch (IOException e) {
+                    sink.error(e);
+                }
+            })
+            .subscribeOn(scheduler);
     }
 
     private Mono<Void> backupExtensions(Path baseDir) {
-        try {
-            var extensionsPath = Files.createFile(baseDir.resolve("extensions.data"));
-            return Mono.using(() -> objectMapper.writerFor(ExtensionStore.class)
+        return Mono.fromCallable(() -> Files.createFile(baseDir.resolve("extensions.data")))
+            .flatMap(extensionsPath -> Mono.using(
+                () -> objectMapper.writerFor(ExtensionStore.class)
                     .writeValuesAsArray(extensionsPath.toFile()),
                 seqWriter -> repository.findAll()
                     .doOnNext(extensionStore -> {
@@ -263,17 +254,8 @@ public class MigrationServiceImpl implements MigrationService {
                         } catch (IOException e) {
                             throw Exceptions.propagate(e);
                         }
-                    })
-                    .then(),
-                seqWriter -> {
-                    try {
-                        seqWriter.close();
-                    } catch (IOException e) {
-                        throw Exceptions.propagate(e);
-                    }
-                });
-        } catch (IOException e) {
-            return Mono.error(e);
-        }
+                    }).then(),
+                FileUtils::closeQuietly))
+            .subscribeOn(scheduler);
     }
 }
