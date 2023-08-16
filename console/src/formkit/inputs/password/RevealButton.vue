521c24d0efee445e9f784f44333341f1351df48f
Ryan Wang <i@ryanc.cc>
2023-08-16 14:22:16 +0800

    feat: add reveal button for password input
    Signed-off-by: Ryan Wang <i@ryanc.cc>

---
+++ console/src/formkit/inputs/password/RevealButton.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+import type { FormKitFrameworkContext } from "@formkit/core";
+import { IconEye, IconEyeOff } from "@halo-dev/components";
+import type { PropType } from "vue";
+import { toRefs } from "vue";
+const props = defineProps({
+  context: {
+    type: Object as PropType<FormKitFrameworkContext>,
+    required: true,
+  },
+});
+
+const { context } = toRefs(props);
+
+function handleChange() {
+  context.value.node.props.type =
+    context.value.node.props.type === "password" ? "text" : "password";
+}
+</script>
+
+<template>
+  <div
+    class="group flex h-full cursor-pointer items-center px-3 transition-all"
+    @click="handleChange"
+  >
+    <IconEye
+      v-if="context.node.props.type === 'password'"
+      class="h-4 w-4 text-gray-500 group-hover:text-gray-700"
+    />
+    <IconEyeOff
+      v-else
+      class="h-4 w-4 text-gray-500 group-hover:text-gray-700"
+    />
+  </div>
+</template>
