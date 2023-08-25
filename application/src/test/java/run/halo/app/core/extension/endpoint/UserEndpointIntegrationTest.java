6e920ddc364ad3e6804c1a4692cb32472ada1e66
Li <mail@e.lixingyong.com>
2023-08-25 22:52:22 +0800

    update checkstyle

--- application/src/test/java/run/halo/app/core/extension/endpoint/UserEndpointIntegrationTest.java
+++ application/src/test/java/run/halo/app/core/extension/endpoint/UserEndpointIntegrationTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
