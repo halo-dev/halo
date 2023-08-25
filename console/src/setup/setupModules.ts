5c115563e01c65d2f22329f7987aa04ba163fa4d
guqing <38999863+guqing@users.noreply.github.com>
2023-08-25 15:28:11 +0800

    feat: add API to obtain the bundled js file for all enabled plugins (#3444)
    #### What type of PR is this?
    /kind feature
    /milestone 2.3.x
    /area core
    #### What this PR does / why we need it:
    提供 `/apis/api.console.halo.run/v1alpha1/plugins/bundle.js` 来获取已启用插件的捆绑后的 main.js 和 style.css 文件
    
    #### Which issue(s) this PR fixes:
    Fixes #3442
    
    #### Does this PR introduce a user-facing change?
    
    ```release-note
    优化已启用插件 jsbundle 文件的加载方式
    ```

---
+++ console/src/setup/setupModules.ts
@@ -0,0 +1,109 @@
+import { i18n } from "@/locales";
+import { coreModules } from "@/modules";
+import router from "@/router";
+import { usePluginModuleStore } from "@/stores/plugin";
+import type { PluginModule, RouteRecordAppend } from "@halo-dev/console-shared";
+import { useScriptTag } from "@vueuse/core";
+import { Toast } from "@halo-dev/components";
+import type { App } from "vue";
+import type { RouteRecordRaw } from "vue-router";
+import { loadStyle } from "@/utils/load-style";
+
+export function setupCoreModules(app: App) {
+  coreModules.forEach((module) => {
+    registerModule(app, module, true);
+  });
+}
+
+export async function setupPluginModules(app: App) {
+  const pluginModuleStore = usePluginModuleStore();
+  try {
+    const { load } = useScriptTag(
+      `${
+        import.meta.env.VITE_API_URL
+      }/apis/api.console.halo.run/v1alpha1/plugins/-/bundle.js`
+    );
+
+    await load();
+
+    const enabledPluginNames = window["enabledPluginNames"] as string[];
+
+    enabledPluginNames.forEach((name) => {
+      const module = window[name];
+
+      if (module) {
+        registerModule(app, module, false);
+        pluginModuleStore.registerPluginModule(name, module);
+      }
+    });
+  } catch (e) {
+    const message = i18n.global.t("core.plugin.loader.toast.entry_load_failed");
+    console.error(message, e);
+    Toast.error(message);
+  }
+
+  try {
+    await loadStyle(
+      `${
+        import.meta.env.VITE_API_URL
+      }/apis/api.console.halo.run/v1alpha1/plugins/-/bundle.css`
+    );
+  } catch (e) {
+    const message = i18n.global.t("core.plugin.loader.toast.style_load_failed");
+    console.error(message, e);
+    Toast.error(message);
+  }
+}
+
+function registerModule(app: App, pluginModule: PluginModule, core: boolean) {
+  if (pluginModule.components) {
+    Object.keys(pluginModule.components).forEach((key) => {
+      const component = pluginModule.components?.[key];
+      if (component) {
+        app.component(key, component);
+      }
+    });
+  }
+
+  if (pluginModule.routes) {
+    if (!Array.isArray(pluginModule.routes)) {
+      return;
+    }
+
+    resetRouteMeta(pluginModule.routes);
+
+    for (const route of pluginModule.routes) {
+      if ("parentName" in route) {
+        router.addRoute(route.parentName, route.route);
+      } else {
+        router.addRoute(route);
+      }
+    }
+  }
+
+  function resetRouteMeta(routes: RouteRecordRaw[] | RouteRecordAppend[]) {
+    for (const route of routes) {
+      if ("parentName" in route) {
+        if (route.route.meta?.menu) {
+          route.route.meta = {
+            ...route.route.meta,
+            core,
+          };
+        }
+        if (route.route.children) {
+          resetRouteMeta(route.route.children);
+        }
+      } else {
+        if (route.meta?.menu) {
+          route.meta = {
+            ...route.meta,
+            core,
+          };
+        }
+        if (route.children) {
+          resetRouteMeta(route.children);
+        }
+      }
+    }
+  }
+}
