637071b2605a23ffca6ba5ea16c8f3379b604a5a
guqing <38999863+guqing@users.noreply.github.com>
2023-08-25 22:12:12 +0800

    feat: support displaying private posts for owner on theme-side (#4412)
    #### What type of PR is this?
    /kind feature
    /area core
    /milestone 2.9.x
    
    #### What this PR does / why we need it:
    登录后支持在主题端展示作者的私有文章
    
    how to test it?
    1. 测试登录后是否能访问到自己创建的私有文章，退出登录后私有文章消失
    2. 不能在在主题端看到别人创建的私有文章
    3. 创建私有文章测试登录后使用主题端的上一页下一页功能是否正常
    
    #### Which issue(s) this PR fixes:
    Fixes #3016
    
    #### Does this PR introduce a user-facing change?
    ```release-note
    登录后支持在主题端展示作者的私有文章
    ```

--- application/src/main/java/run/halo/app/theme/router/factories/IndexRouteFactory.java
+++ application/src/main/java/run/halo/app/theme/router/factories/IndexRouteFactory.java
@@ -13,6 +13,7 @@ import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.server.i18n.LocaleContextResolver;
 import reactor.core.publisher.Mono;
 import run.halo.app.infra.SystemConfigurableEnvironmentFetcher;
 import run.halo.app.infra.SystemSetting;
@@ -21,6 +22,7 @@ import run.halo.app.theme.finders.PostFinder;
 import run.halo.app.theme.finders.vo.ListedPostVo;
 import run.halo.app.theme.router.ModelConst;
 import run.halo.app.theme.router.PageUrlUtils;
+import run.halo.app.theme.router.TitleVisibilityIdentifyCalculator;
 import run.halo.app.theme.router.UrlContextListResult;
 
 /**
@@ -36,6 +38,8 @@ public class IndexRouteFactory implements RouteFactory {
 
     private final PostFinder postFinder;
     private final SystemConfigurableEnvironmentFetcher environmentFetcher;
+    private final TitleVisibilityIdentifyCalculator titleVisibilityIdentifyCalculator;
+    private final LocaleContextResolver localeContextResolver;
 
     @Override
     public RouterFunction<ServerResponse> create(String pattern) {
@@ -54,8 +58,19 @@ public class IndexRouteFactory implements RouteFactory {
 
     private Mono<UrlContextListResult<ListedPostVo>> postList(ServerRequest request) {
         String path = request.path();
+
         return configuredPageSize(environmentFetcher, SystemSetting.Post::getPostPageSize)
             .flatMap(pageSize -> postFinder.list(pageNumInPathVariable(request), pageSize))
+            .doOnNext(list -> list.getItems()
+                .forEach(listedPostVo -> listedPostVo.getSpec()
+                    .setTitle(titleVisibilityIdentifyCalculator.calculateTitle(
+                        listedPostVo.getSpec().getTitle(),
+                        listedPostVo.getSpec().getVisible(),
+                        localeContextResolver.resolveLocaleContext(request.exchange())
+                            .getLocale())
+                    )
+                )
+            )
             .map(list -> new UrlContextListResult.Builder<ListedPostVo>()
                 .listResult(list)
                 .nextUrl(PageUrlUtils.nextPageUrl(path, totalPage(list)))
