ae6850a1ff4ae151c1f317ba70460149183c726d
guqing <i@guqing.email>
2023-09-06 14:58:43 +0800

    refactor: set cache-control for bundle resources of plugin

--- application/src/main/java/run/halo/app/core/extension/endpoint/PluginEndpoint.java
+++ application/src/main/java/run/halo/app/core/extension/endpoint/PluginEndpoint.java
@@ -30,6 +30,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import lombok.AllArgsConstructor;
@@ -39,6 +40,7 @@ import org.springdoc.webflux.core.fn.SpringdocRouteBuilder;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.CacheControl;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.http.codec.multipart.FormFieldPart;
@@ -247,6 +249,7 @@ public class PluginEndpoint implements CustomEndpoint {
         return pluginService.uglifyJsBundle()
             .defaultIfEmpty("")
             .flatMap(bundle -> ServerResponse.ok()
+                .cacheControl(CacheControl.maxAge(1, TimeUnit.DAYS))
                 .contentType(MediaType.valueOf("text/javascript"))
                 .bodyValue(bundle)
             );
@@ -263,6 +266,7 @@ public class PluginEndpoint implements CustomEndpoint {
         }
         return pluginService.uglifyCssBundle()
             .flatMap(bundle -> ServerResponse.ok()
+                .cacheControl(CacheControl.maxAge(1, TimeUnit.DAYS))
                 .contentType(MediaType.valueOf("text/css"))
                 .bodyValue(bundle)
             );
