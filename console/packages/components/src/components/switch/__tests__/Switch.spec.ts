5f97d48fe87a0f335b36a4f949ccf22d9f6597db
Ryan Wang <i@ryanc.cc>
2023-10-08 12:31:31 +0800

    feat: add loading state for switch component
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- console/packages/components/src/components/switch/__tests__/Switch.spec.ts
+++ console/packages/components/src/components/switch/__tests__/Switch.spec.ts
@@ -1,9 +1,25 @@
 import { describe, expect, it } from "vitest";
 import { VSwitch } from "../index";
-import { mount } from "@vue/test-utils";
+import { mount, shallowMount } from "@vue/test-utils";
 
 describe("Switch", () => {
   it("should render", () => {
     expect(mount(VSwitch)).toBeDefined();
+  });
+
+  it("emits the correct events when clicked", () => {
+    const wrapper = shallowMount(VSwitch);
+    wrapper.find("button").trigger("click");
+    expect(wrapper.emitted("update:modelValue")).toBeTruthy();
+    expect(wrapper.emitted("change")).toBeTruthy();
+  });
+
+  it("does not emit events when disabled or loading", () => {
+    const wrapper = shallowMount(VSwitch, {
+      props: { disabled: true, loading: true },
+    });
+    wrapper.find("button").trigger("click");
+    expect(wrapper.emitted("update:modelValue")).toBeFalsy();
+    expect(wrapper.emitted("change")).toBeFalsy();
   });
 });
