3905ec8b8d2184fdd716e6eef4cb42e943eb9400
Roozen <93673944+Roozenlz@users.noreply.github.com>
2023-10-27 16:54:56 +0800

    fix: change the ref parameter type in the list method of comment finder api to map (#4773)
    <!--  Thanks for sending a pull request!  Here are some tips for you:
    1. 如果这是你的第一次，请阅读我们的贡献指南：<https://github.com/halo-dev/halo/blob/master/CONTRIBUTING.md>。
    1. If this is your first time, please read our contributor guidelines: <https://github.com/halo-dev/halo/blob/master/CONTRIBUTING.md>.
    2. 请根据你解决问题的类型为 Pull Request 添加合适的标签。
    2. Please label this pull request according to what type of issue you are addressing, especially if this is a release targeted pull request.
    3. 请确保你已经添加并运行了适当的测试。
    3. Ensure you have added or ran the appropriate tests for your PR.
    -->
    
    #### What type of PR is this?
    /kind bug
    <!--
    添加其中一个类别：
    Add one of the following kinds:
    
    /kind bug
    /kind cleanup
    /kind documentation
    /kind feature
    /kind improvement
    
    适当添加其中一个或多个类别（可选）：
    Optionally add one or more of the following kinds if applicable:
    
    /kind api-change
    /kind deprecation
    /kind failing-test
    /kind flake
    /kind regression
    -->
    
    #### What this PR does / why we need it:
    在主题使用如下FinderAPI时
    ![image](https://github.com/halo-dev/halo/assets/93673944/723e9f4e-0192-48e4-be18-2300579215bd)
    将产生如下错误
    ![image](https://github.com/halo-dev/halo/assets/93673944/d36c9dd7-d754-4d41-a6d2-06027359bb4e)
    因为方法定义为
    ![image](https://github.com/halo-dev/halo/assets/93673944/a3b95df3-96f8-427e-b9c0-7e97e5d2335d)
    所以修改方法参数
    #### Which issue(s) this PR fixes:
    
    <!--
    PR 合并时自动关闭 issue。
    Automatically closes linked issue when PR is merged.
    
    用法：`Fixes #<issue 号>`，或者 `Fixes (粘贴 issue 完整链接)`
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes #
    
    #### Special notes for your reviewer:
    
    #### Does this PR introduce a user-facing change?
    
    <!--
    如果当前 Pull Request 的修改不会造成用户侧的任何变更，在 `release-note` 代码块儿中填写 `NONE`。
    否则请填写用户侧能够理解的 Release Note。如果当前 Pull Request 包含破坏性更新（Break Change），
    Release Note 需要以 `action required` 开头。
    If no, just write "NONE" in the release-note block below.
    If yes, a release note is required:
    Enter your extended release note in the block below. If the PR requires additional action from users switching to the new release, include the string "action required".
    -->
    
    ```release-note
    NONE
    ```

--- application/src/main/java/run/halo/app/theme/finders/CommentFinder.java
+++ application/src/main/java/run/halo/app/theme/finders/CommentFinder.java
@@ -1,10 +1,10 @@
 package run.halo.app.theme.finders;
 
+import java.util.Map;
 import org.springframework.lang.Nullable;
 import reactor.core.publisher.Mono;
 import run.halo.app.core.extension.content.Comment;
 import run.halo.app.extension.ListResult;
-import run.halo.app.extension.Ref;
 import run.halo.app.theme.finders.vo.CommentVo;
 import run.halo.app.theme.finders.vo.ReplyVo;
 
@@ -18,7 +18,7 @@ public interface CommentFinder {
 
     Mono<CommentVo> getByName(String name);
 
-    Mono<ListResult<CommentVo>> list(@Nullable Ref ref, @Nullable Integer page,
+    Mono<ListResult<CommentVo>> list(@Nullable Map<String, String> ref, @Nullable Integer page,
         @Nullable Integer size);
 
     Mono<ListResult<ReplyVo>> listReply(String commentName, @Nullable Integer page,
