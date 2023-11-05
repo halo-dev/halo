5ee02401a94b38c59432fb7e5af4ca68cc63f9f4
John Niang <johnniang@foxmail.com>
2023-11-05 23:54:34 +0800

    Add generator meta into head

--- application/src/main/java/run/halo/app/theme/ThemeConfiguration.java
+++ application/src/main/java/run/halo/app/theme/ThemeConfiguration.java
@@ -8,7 +8,10 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.time.Instant;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
@@ -21,8 +24,10 @@ import org.thymeleaf.extras.springsecurity6.dialect.SpringSecurityDialect;
 import reactor.core.publisher.Mono;
 import run.halo.app.infra.ThemeRootGetter;
 import run.halo.app.infra.utils.FileUtils;
+import run.halo.app.theme.dialect.GeneratorMetaProcessor;
 import run.halo.app.theme.dialect.HaloSpringSecurityDialect;
 import run.halo.app.theme.dialect.LinkExpressionObjectDialect;
+import run.halo.app.theme.dialect.TemplateHeadProcessor;
 
 /**
  * @author guqing
@@ -85,5 +90,13 @@ public class ThemeConfiguration {
     SpringSecurityDialect springSecurityDialect(
         ServerSecurityContextRepository securityContextRepository) {
         return new HaloSpringSecurityDialect(securityContextRepository);
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "halo.theme.generator-meta-disabled",
+        havingValue = "false",
+        matchIfMissing = true)
+    TemplateHeadProcessor generatorMetaProcessor(ObjectProvider<BuildProperties> buildProperties) {
+        return new GeneratorMetaProcessor(buildProperties);
     }
 }
