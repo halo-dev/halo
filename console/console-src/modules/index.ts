57e3394fc12937967c6bea3843f252ab7985e41c
Ryan Wang <i@ryanc.cc>
2024-01-10 14:35:17 +0800

    Fix imports
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- console/console-src/modules/index.ts
+++ console/console-src/modules/index.ts
@@ -1,4 +1,4 @@
-import type { PluginModule } from "packages/shared/dist";
+import type { PluginModule } from "@halo-dev/console-shared";
 
 const modules = Object.values(
   import.meta.glob("./**/module.ts", {
