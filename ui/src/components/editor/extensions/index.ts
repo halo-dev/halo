820c1038db5aec9c954fed3764c7848166f962b6
Takagi <1103069291@qq.com>
2024-03-14 13:02:07 +0800

    pref: refactor default editor audio block upload logic (#5421)
    #### What type of PR is this?
    
    /kind improvement
    /area ui
    /area editor
    
    #### What this PR does / why we need it:
    
    重构默认编辑器音频组件。使其支持上传、取消上传、从附件库选择、替换等功能。
    
    #### How to test it?
    
    直接拖动、复制或选择文件上传一个音频，查看是否显示上传进度条，取消、重试功能是否正常
    
    #### Which issue(s) this PR fixes:
    
    Fixes #5239 
    
    #### Does this PR introduce a user-facing change?
    ```release-note
    重构编辑器音频组件的上传逻辑，增加选择文件上传、上传进度条、取消、重试等机制。
    ```

--- ui/src/components/editor/extensions/index.ts
+++ ui/src/components/editor/extensions/index.ts
@@ -1,4 +1,10 @@
 import UiExtensionImage from "./image";
 import UiExtensionVideo from "./video";
 import UiExtensionUpload from "./upload";
-export { UiExtensionImage, UiExtensionUpload, UiExtensionVideo };
+import UiExtensionAudio from "./audio";
+export {
+  UiExtensionImage,
+  UiExtensionUpload,
+  UiExtensionVideo,
+  UiExtensionAudio,
+};
