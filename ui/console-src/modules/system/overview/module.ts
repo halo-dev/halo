25bb1eacba728b316c89068397463f01a5cfafb2
Ryan Wang <i@ryanc.cc>
2024-03-14 12:33:30 +0800

    chore: redefine the actuator page as the overview page
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- ui/console-src/modules/system/overview/module.ts
+++ ui/console-src/modules/system/overview/module.ts
@@ -1,22 +1,22 @@
 import { definePlugin } from "@halo-dev/console-shared";
 import { IconTerminalBoxLine } from "@halo-dev/components";
 import BasicLayout from "@console/layouts/BasicLayout.vue";
-import Actuator from "./Actuator.vue";
+import Overview from "./Overview.vue";
 import { markRaw } from "vue";
 
 export default definePlugin({
   components: {},
   routes: [
     {
-      path: "/actuator",
-      name: "OverviewRoot", // fixme: actuator will be renamed to overview in the future
+      path: "/overview",
+      name: "OverviewRoot",
       component: BasicLayout,
       meta: {
-        title: "core.actuator.title",
+        title: "core.overview.title",
         searchable: true,
         permissions: ["system:actuator:manage"],
         menu: {
-          name: "core.sidebar.menu.items.actuator",
+          name: "core.sidebar.menu.items.overview",
           group: "system",
           icon: markRaw(IconTerminalBoxLine),
           priority: 3,
@@ -25,8 +25,8 @@ export default definePlugin({
       children: [
         {
           path: "",
-          name: "Actuator",
-          component: Actuator,
+          name: "Overview",
+          component: Overview,
         },
       ],
     },
