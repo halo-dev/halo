6d57bad83fe3b0299f3153b4799cbfa3274a9cfa
LIlGG <1103069291@qq.com>
2024-03-25 15:30:51 +0800

    fix: error occurs when opening attachment library in the uc

--- ui/src/components/editor/extensions/image/index.ts
+++ ui/src/components/editor/extensions/image/index.ts
@@ -3,14 +3,20 @@ import ImageView from "./ImageView.vue";
 import type { AxiosRequestConfig } from "axios";
 import type { Attachment } from "@halo-dev/api-client";
 
-interface UiImageOptions {
+export interface ImageOptions {
+  inline: boolean;
+  allowBase64: boolean;
+  HTMLAttributes: Record<string, unknown>;
+}
+
+export interface UiImageOptions {
   uploadImage?: (
     file: File,
     options?: AxiosRequestConfig
   ) => Promise<Attachment>;
 }
 
-const Image = ExtensionImage.extend<UiImageOptions>({
+const Image = ExtensionImage.extend<ImageOptions & UiImageOptions>({
   addOptions() {
     const { parent } = this;
     return {
