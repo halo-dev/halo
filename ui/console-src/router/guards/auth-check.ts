943310bb423e1116ccc74be4a35b05592f6be3bd
LIlGG <1103069291@qq.com>
2024-03-27 18:36:56 +0800

    fix: wrong router navigation when redirect to /uc

--- ui/console-src/router/guards/auth-check.ts
+++ ui/console-src/router/guards/auth-check.ts
@@ -2,7 +2,7 @@ import { rbacAnnotations } from "@/constants/annotations";
 import { useUserStore } from "@/stores/user";
 import type { Router } from "vue-router";
 
-const whiteList = ["Setup", "Login", "Binding", "ResetPassword"];
+const whiteList = ["Setup", "Login", "Binding", "ResetPassword", "Redirect"];
 
 export function setupAuthCheckGuard(router: Router) {
   router.beforeEach((to, from, next) => {
@@ -52,7 +52,7 @@ export function setupAuthCheckGuard(router: Router) {
         return;
       }
 
-      if (to.name === "whiteList") {
+      if (whiteList.includes(to.name)) {
         next();
         return;
       }
