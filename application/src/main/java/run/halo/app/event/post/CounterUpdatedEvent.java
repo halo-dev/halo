39f5cac4e58961c4e14b77fdf84081fc3f9a9453
ZJamss <zjamss@qq.com>
2024-04-02 15:28:05 +0800

    removed unused code
    Signed-off-by: ZJamss <zjamss@qq.com>

--- application/src/main/java/run/halo/app/event/post/CounterUpdatedEvent.java
+++
@@ -1,9 +0,0 @@
-package run.halo.app.event.post;
-
-import run.halo.app.core.extension.Counter;
-
-public class CounterUpdatedEvent extends CounterEvent {
-    public CounterUpdatedEvent(Object source, Counter counter) {
-        super(source, counter);
-    }
-}
