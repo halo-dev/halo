ac6896d0f359b263ff6e4910338e404ded295641
Takagi <1103069291@qq.com>
2024-04-18 17:30:06 +0800

    pref: optimize the toolbar space and fold the same type of function (#5683)
    #### What type of PR is this?
    
    /kind improvement
    /area editor
    /milestone 2.15.x
    
    #### What this PR does / why we need it:
    
    本 PR 对富文本编辑器中工具栏和冒泡菜单进行精简。
    
    - 对默认富文本冒泡菜单及顶部工具栏相同类型的功能进行了折叠。
    - 删除在 text 的冒泡菜单中，不属于当前选中文本的功能，例如转换为代码块等，这些实际上是段落的功能，并非是文本的功能。而 textAlign 目前也仅仅用在标题和行上，因此也移除。
    
    #### How to test it?
    
    查看默认富文本编辑器工具栏中，折叠的功能是否正常。选中文本，查看文本中是否不包含段落相关的功能。
    
    #### Which issue(s) this PR fixes:
    
    Fixes #5669 
    
    #### Does this PR introduce a user-facing change?
    ```release-note
    对默认富文本编辑器中的工具栏进行折叠及精简优化。
    ```

--- ui/packages/editor/src/extensions/text-align/index.ts
+++ ui/packages/editor/src/extensions/text-align/index.ts
@@ -1,4 +1,4 @@
-import type { Editor } from "@/tiptap/vue-3";
+import { type Editor } from "@/tiptap/vue-3";
 import TiptapTextAlign from "@tiptap/extension-text-align";
 import type { TextAlignOptions } from "@tiptap/extension-text-align";
 import ToolbarItem from "@/components/toolbar/ToolbarItem.vue";
@@ -9,59 +9,90 @@ import MdiFormatAlignJustify from "~icons/mdi/format-align-justify";
 import { markRaw } from "vue";
 import { i18n } from "@/locales";
 import type { ExtensionOptions } from "@/types";
+import ToolbarSubItem from "@/components/toolbar/ToolbarSubItem.vue";
+
+const iconComponent = {
+  left: MdiFormatAlignLeft,
+  center: MdiFormatAlignCenter,
+  right: MdiFormatAlignRight,
+  justify: MdiFormatAlignJustify,
+};
+
+const getIcon = (editor: Editor) => {
+  let icon = MdiFormatAlignLeft;
+  Object.entries(iconComponent).forEach(([key, value]) => {
+    if (editor.isActive({ textAlign: key })) {
+      icon = value;
+      return;
+    }
+  });
+  return icon;
+};
 
 const TextAlign = TiptapTextAlign.extend<ExtensionOptions & TextAlignOptions>({
   addOptions() {
     return {
       ...this.parent?.(),
       getToolbarItems({ editor }: { editor: Editor }) {
-        return [
-          {
-            priority: 180,
-            component: markRaw(ToolbarItem),
-            props: {
-              editor,
-              isActive: editor.isActive({ textAlign: "left" }),
-              icon: markRaw(MdiFormatAlignLeft),
-              title: i18n.global.t("editor.common.align_left"),
-              action: () => editor.chain().focus().setTextAlign("left").run(),
-            },
+        return {
+          priority: 180,
+          component: markRaw(ToolbarItem),
+          props: {
+            editor,
+            isActive: false,
+            icon: markRaw(getIcon(editor)),
+            title: i18n.global.t("editor.common.align_method"),
           },
-          {
-            priority: 190,
-            component: markRaw(ToolbarItem),
-            props: {
-              editor,
-              isActive: editor.isActive({ textAlign: "center" }),
-              icon: markRaw(MdiFormatAlignCenter),
-              title: i18n.global.t("editor.common.align_center"),
-              action: () => editor.chain().focus().setTextAlign("center").run(),
+          children: [
+            {
+              priority: 0,
+              component: markRaw(ToolbarSubItem),
+              props: {
+                editor,
+                isActive: editor.isActive({ textAlign: "left" }),
+                icon: markRaw(MdiFormatAlignLeft),
+                title: i18n.global.t("editor.common.align_left"),
+                action: () => editor.chain().focus().setTextAlign("left").run(),
+              },
             },
-          },
-          {
-            priority: 200,
-            component: markRaw(ToolbarItem),
-            props: {
-              editor,
-              isActive: editor.isActive({ textAlign: "right" }),
-              icon: markRaw(MdiFormatAlignRight),
-              title: i18n.global.t("editor.common.align_right"),
-              action: () => editor.chain().focus().setTextAlign("right").run(),
+            {
+              priority: 10,
+              component: markRaw(ToolbarSubItem),
+              props: {
+                editor,
+                isActive: editor.isActive({ textAlign: "center" }),
+                icon: markRaw(MdiFormatAlignCenter),
+                title: i18n.global.t("editor.common.align_center"),
+                action: () =>
+                  editor.chain().focus().setTextAlign("center").run(),
+              },
             },
-          },
-          {
-            priority: 210,
-            component: markRaw(ToolbarItem),
-            props: {
-              editor,
-              isActive: editor.isActive({ textAlign: "justify" }),
-              icon: markRaw(MdiFormatAlignJustify),
-              title: i18n.global.t("editor.common.align_justify"),
-              action: () =>
-                editor.chain().focus().setTextAlign("justify").run(),
+            {
+              priority: 20,
+              component: markRaw(ToolbarSubItem),
+              props: {
+                editor,
+                isActive: editor.isActive({ textAlign: "right" }),
+                icon: markRaw(MdiFormatAlignRight),
+                title: i18n.global.t("editor.common.align_right"),
+                action: () =>
+                  editor.chain().focus().setTextAlign("right").run(),
+              },
             },
-          },
-        ];
+            {
+              priority: 30,
+              component: markRaw(ToolbarSubItem),
+              props: {
+                editor,
+                isActive: editor.isActive({ textAlign: "justify" }),
+                icon: markRaw(MdiFormatAlignJustify),
+                title: i18n.global.t("editor.common.align_justify"),
+                action: () =>
+                  editor.chain().focus().setTextAlign("justify").run(),
+              },
+            },
+          ],
+        };
       },
     };
   },
