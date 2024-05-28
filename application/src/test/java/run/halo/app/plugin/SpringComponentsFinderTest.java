0c497669a19ac591cfaf05573950ff32f2a10064
JohnNiang <johnniang@foxmail.com>
2024-05-29 00:05:28 +0800

    Refactor SpringComponentsFinder to prevent unexpected entries cleanup
    Signed-off-by: JohnNiang <johnniang@foxmail.com>

--- application/src/test/java/run/halo/app/plugin/SpringComponentsFinderTest.java
+++ application/src/test/java/run/halo/app/plugin/SpringComponentsFinderTest.java
@@ -1,18 +1,25 @@
 package run.halo.app.plugin;
 
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import org.junit.jupiter.api.BeforeEach;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.pf4j.PluginClassLoader;
+import org.pf4j.PluginManager;
+import org.pf4j.PluginState;
+import org.pf4j.PluginStateEvent;
 import org.pf4j.PluginWrapper;
 import org.springframework.util.ResourceUtils;
 
@@ -25,29 +32,64 @@ import org.springframework.util.ResourceUtils;
 @ExtendWith(MockitoExtension.class)
 class SpringComponentsFinderTest {
 
-    private File testFile;
     @Mock
-    private HaloPluginManager pluginManager;
-    @Mock
-    private PluginWrapper pluginWrapper;
-    @Mock
-    private PluginClassLoader pluginClassLoader;
+    private PluginManager pluginManager;
 
     @InjectMocks
-    private SpringComponentsFinder springComponentsFinder;
+    private SpringComponentsFinder finder;
 
-    @BeforeEach
-    void setUp() throws FileNotFoundException {
-        testFile = ResourceUtils.getFile("classpath:plugin/test-plugin-components.idx");
+    @Test
+    void shouldNotInvokeReadClasspathStorages() {
+        assertThrows(UnsupportedOperationException.class,
+            () -> finder.readClasspathStorages()
+        );
     }
 
     @Test
-    void containsPlugin() {
-        boolean exist = springComponentsFinder.containsComponentsStorage("NotExist");
-        assertThat(exist).isFalse();
-
-        assertThatThrownBy(() -> springComponentsFinder.containsComponentsStorage(null))
-            .hasMessage("The pluginId cannot be null");
+    void shouldNotInvokeReadPluginsStorages() {
+        assertThrows(UnsupportedOperationException.class,
+            () -> finder.readPluginsStorages()
+        );
     }
 
+    @Test
+    void shouldPutEntryIfPluginCreated() throws FileNotFoundException {
+        var pluginWrapper = mockPluginWrapper();
+        when(pluginWrapper.getPluginState()).thenReturn(PluginState.CREATED);
+
+        var event = new PluginStateEvent(pluginManager, pluginWrapper, null);
+        finder.pluginStateChanged(event);
+
+        var classNames = finder.findClassNames("fake-plugin");
+        assertEquals(Set.of("run.halo.fake.FakePlugin"), classNames);
+    }
+
+    @Test
+    void shouldRemoveEntryIfPluginUnloaded() throws FileNotFoundException {
+        var pluginWrapper = mockPluginWrapper();
+        when(pluginWrapper.getPluginState()).thenReturn(PluginState.CREATED);
+
+        var event = new PluginStateEvent(pluginManager, pluginWrapper, null);
+        finder.pluginStateChanged(event);
+
+        var classNames = finder.findClassNames("fake-plugin");
+        assertFalse(classNames.isEmpty());
+
+        when(pluginWrapper.getPluginState()).thenReturn(PluginState.UNLOADED);
+        event = new PluginStateEvent(pluginManager, pluginWrapper, null);
+        finder.pluginStateChanged(event);
+
+        classNames = finder.findClassNames("fake-plugin");
+        assertTrue(classNames.isEmpty());
+    }
+
+    private PluginWrapper mockPluginWrapper() throws FileNotFoundException {
+        var pluginWrapper = mock(PluginWrapper.class);
+        when(pluginWrapper.getPluginId()).thenReturn("fake-plugin");
+
+        var pluginRootUrl = ResourceUtils.getURL("classpath:plugin/plugin-for-finder/");
+        var classLoader = new URLClassLoader(new URL[] {pluginRootUrl});
+        when(pluginWrapper.getPluginClassLoader()).thenReturn(classLoader);
+        return pluginWrapper;
+    }
 }
