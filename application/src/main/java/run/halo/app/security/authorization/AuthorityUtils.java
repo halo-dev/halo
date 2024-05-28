9d3c091e505892302119fa5689fc49b2bfea9a88
JohnNiang <johnniang@foxmail.com>
2024-05-29 00:40:47 +0800

    Fix the problem the PAT could not be created or restored while logging in with remember-me
    Signed-off-by: JohnNiang <johnniang@foxmail.com>

--- application/src/main/java/run/halo/app/security/authorization/AuthorityUtils.java
+++ application/src/main/java/run/halo/app/security/authorization/AuthorityUtils.java
@@ -4,6 +4,9 @@ import java.util.Collection;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.authentication.RememberMeAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 
 /**
@@ -43,5 +46,16 @@ public enum AuthorityUtils {
 
     public static boolean containsSuperRole(Collection<String> roles) {
         return roles.contains(SUPER_ROLE_NAME);
+    }
+
+    /**
+     * Check if the authentication is a real user.
+     *
+     * @param authentication current authentication
+     * @return true if the authentication is a real user; false otherwise
+     */
+    public static boolean isRealUser(Authentication authentication) {
+        return authentication instanceof UsernamePasswordAuthenticationToken
+            || authentication instanceof RememberMeAuthenticationToken;
     }
 }
