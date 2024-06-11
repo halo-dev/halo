16eaf458b0de1e1a74e5a46d7763327b40c3494e
LIlGG <1103069291@qq.com>
2024-06-11 15:46:50 +0800

    fix: default editor focus setting failure

--- ui/src/components/editor/DefaultEditor.vue
+++ ui/src/components/editor/DefaultEditor.vue
@@ -74,7 +74,6 @@ import { OverlayScrollbarsComponent } from "overlayscrollbars-vue";
 import {
   inject,
   markRaw,
-  nextTick,
   onBeforeUnmount,
   onMounted,
   ref,
@@ -169,6 +168,8 @@ const attachmentSelectorModal = ref(false);
 const { onAttachmentSelect, attachmentResult } = useAttachmentSelect();
 
 const editor = shallowRef<Editor>();
+// Set focus
+const editorTitleRef = ref();
 
 const { pluginModules } = usePluginModuleStore();
 
@@ -408,6 +409,13 @@ onMounted(async () => {
     onUpdate: () => {
       debounceOnUpdate();
     },
+    onCreate() {
+      if (editor.value?.isEmpty && !props.title) {
+        editorTitleRef.value.focus();
+      } else {
+        editor.value?.commands.focus();
+      }
+    },
   });
 });
 
@@ -442,22 +450,6 @@ const currentLocale = i18n.global.locale.value as
 function onTitleInput(event: Event) {
   emit("update:title", (event.target as HTMLInputElement).value);
 }
-
-// Set focus
-const editorTitleRef = ref();
-onMounted(() => {
-  // if name is empty, it means the editor is in the creation mode
-  const urlParams = new URLSearchParams(window.location.search);
-  const name = urlParams.get("name");
-
-  if (!name && editorTitleRef.value) {
-    nextTick(() => {
-      editorTitleRef.value.focus();
-    });
-  } else {
-    editor.value?.commands.focus();
-  }
-});
 </script>
 
 <template>
