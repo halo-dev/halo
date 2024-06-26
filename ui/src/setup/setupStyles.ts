d52574b8feb6341231c3b8879847e73a9299f775
Ryan Wang <i@ryanc.cc>
2024-06-26 19:15:36 +0800

    fix: resolve style issues caused by changing the order of CSS imports
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- ui/src/setup/setupStyles.ts
+++ ui/src/setup/setupStyles.ts
@@ -1,5 +1,9 @@
-import "@/styles/index.css";
-import "@/styles/tailwind.css";
-import "@halo-dev/components/dist/style.css";
 import "@halo-dev/richtext-editor/dist/style.css";
+
+import "@halo-dev/components/dist/style.css";
+
+import "@/styles/tailwind.css";
+
+import "@/styles/index.css";
+
 import "overlayscrollbars/overlayscrollbars.css";
