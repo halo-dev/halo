1b5a23dfb4e76ceceeeb57f39a8d5fa99490e6e8
Rick <1450685+LinuxSuRen@users.noreply.github.com>
2024-07-02 19:07:23 +0800

    fix: the e2e script exit code handling is incorrect (#6097)
    <!--  Thanks for sending a pull request!  Here are some tips for you:
    1. 如果这是你的第一次，请阅读我们的贡献指南：<https://github.com/halo-dev/halo/blob/main/CONTRIBUTING.md>。
    1. If this is your first time, please read our contributor guidelines: <https://github.com/halo-dev/halo/blob/main/CONTRIBUTING.md>.
    2. 请根据你解决问题的类型为 Pull Request 添加合适的标签。
    2. Please label this pull request according to what type of issue you are addressing, especially if this is a release targeted pull request.
    3. 请确保你已经添加并运行了适当的测试。
    3. Ensure you have added or ran the appropriate tests for your PR.
    -->
    
    #### What type of PR is this?
    
    <!--
    添加其中一个类别：
    Add one of the following kinds:
    
    /kind bug
    /kind cleanup
    /kind documentation
    /kind feature
    /kind improvement
    
    适当添加其中一个或多个类别（可选）：
    Optionally add one or more of the following kinds if applicable:
    
    /kind api-change
    /kind deprecation
    /kind failing-test
    /kind flake
    /kind regression
    -->
    /kind bug
    
    #### What this PR does / why we need it:
    
    #### Which issue(s) this PR fixes:
    
    <!--
    PR 合并时自动关闭 issue。
    Automatically closes linked issue when PR is merged.
    
    用法：`Fixes #<issue 号>`，或者 `Fixes (粘贴 issue 完整链接)`
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    Fixes https://github.com/halo-dev/halo/issues/6096
    
    #### Special notes for your reviewer:
    
    #### Does this PR introduce a user-facing change?
    
    <!--
    如果当前 Pull Request 的修改不会造成用户侧的任何变更，在 `release-note` 代码块儿中填写 `NONE`。
    否则请填写用户侧能够理解的 Release Note。如果当前 Pull Request 包含破坏性更新（Break Change），
    Release Note 需要以 `action required` 开头。
    If no, just write "NONE" in the release-note block below.
    If yes, a release note is required:
    Enter your extended release note in the block below. If the PR requires additional action from users switching to the new release, include the string "action required".
    -->
    
    ```release-note
    None
    ```

--- e2e/start.sh
+++ e2e/start.sh
@@ -6,26 +6,5 @@ then
     file=compose.yaml
 fi
 
-docker-compose version
-docker-compose -f "$file" up --build -d
-
-while true
-do
-    docker-compose -f "$file" ps | grep testing
-    if [ $? -eq 1 ]
-    then
-        code=-1
-        docker-compose -f "$file" logs | grep e2e-testing
-        docker-compose -f "$file" logs | grep e2e-testing | grep Usage
-        if [ $? -eq 1 ]
-        then
-            code=0
-            echo "successed"
-        fi
-
-        docker-compose -f "$file" down
-        set -e
-        exit $code
-    fi
-    sleep 1
-done
+docker-compose -f "$file" down
+docker-compose -f "$file" up --build testing --exit-code-from testing --remove-orphans
