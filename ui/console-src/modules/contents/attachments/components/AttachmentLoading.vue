617a4aea57a31e7dd396a2286168d817949f689f
LonelySnowman <2438490168@qq.com>
2024-07-04 01:37:36 +0800

    feat: video attachments add cover

---
+++ ui/console-src/modules/contents/attachments/components/AttachmentLoading.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex h-full items-center justify-center object-cover">
+    <span class="text-xs text-gray-400">
+      {{ $t("core.common.status.loading") }}...
+    </span>
+  </div>
+</template>
