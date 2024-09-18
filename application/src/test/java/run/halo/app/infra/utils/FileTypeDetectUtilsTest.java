5b6c4d616c20e13e74e874dc28afd79ce700e5de
guqing <i@guqing.email>
2024-09-18 17:26:10 +0800

    refactor: optimize file type detection and support decision-making based on file name

--- application/src/test/java/run/halo/app/infra/utils/FileTypeDetectUtilsTest.java
+++ application/src/test/java/run/halo/app/infra/utils/FileTypeDetectUtilsTest.java
@@ -3,6 +3,7 @@ package run.halo.app.infra.utils;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import org.apache.tika.mime.MimeTypeException;
 import org.junit.jupiter.api.Test;
@@ -29,6 +30,60 @@ class FileTypeDetectUtilsTest {
         file = ResourceUtils.getFile("classpath:themes/test-theme.zip");
         mimeType = FileTypeDetectUtils.detectMimeType(Files.newInputStream(file.toPath()));
         assertThat(mimeType).isEqualTo("application/zip");
+    }
+
+    @Test
+    void detectMimeTypeWithNameTest() throws IOException {
+        var stream = getFileInputStream("classpath:file-type-detect/index.js");
+        String mimeType = FileTypeDetectUtils.detectMimeType(stream, "index.js");
+        assertThat(mimeType).isEqualTo("application/javascript");
+
+        stream = getFileInputStream("classpath:file-type-detect/index.html");
+        mimeType =
+            FileTypeDetectUtils.detectMimeType(stream, "index.html");
+        assertThat(mimeType).isEqualTo("text/html");
+
+        stream = getFileInputStream("classpath:file-type-detect/test.json");
+        mimeType = FileTypeDetectUtils.detectMimeType(stream, "test.json");
+        assertThat(mimeType).isEqualTo("application/json");
+
+        stream = getFileInputStream("classpath:file-type-detect/other.xlsx");
+        mimeType = FileTypeDetectUtils.detectMimeType(stream, "other.xlsx");
+        assertThat(mimeType).isEqualTo(
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+
+        // other.xlsx detect without name
+        stream = getFileInputStream("classpath:file-type-detect/other.xlsx");
+        mimeType = FileTypeDetectUtils.detectMimeType(stream);
+        assertThat(mimeType).isEqualTo("application/zip");
+
+        // other.xlsx detect with wrong name
+        stream = getFileInputStream("classpath:file-type-detect/other.xlsx");
+        mimeType = FileTypeDetectUtils.detectMimeType(stream, "other.txt");
+        assertThat(mimeType).isEqualTo("application/zip");
+
+        stream = getFileInputStream("classpath:file-type-detect/test.docx");
+        mimeType = FileTypeDetectUtils.detectMimeType(stream, "test.docx");
+        assertThat(mimeType).isEqualTo(
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+
+        // docx detect without file name
+        stream = getFileInputStream("classpath:file-type-detect/test.docx");
+        mimeType = FileTypeDetectUtils.detectMimeType(stream);
+        assertThat(mimeType).isEqualTo("application/zip");
+
+        stream = getFileInputStream("classpath:file-type-detect/test.svg");
+        mimeType = FileTypeDetectUtils.detectMimeType(stream, "test.svg");
+        assertThat(mimeType).isEqualTo("image/svg+xml");
+
+        stream = getFileInputStream("classpath:file-type-detect/test.png");
+        mimeType = FileTypeDetectUtils.detectMimeType(stream, "test.png");
+        assertThat(mimeType).isEqualTo("image/png");
+    }
+
+    private static InputStream getFileInputStream(String location) throws IOException {
+        var file = ResourceUtils.getFile(location);
+        return Files.newInputStream(file.toPath());
     }
 
     @Test
