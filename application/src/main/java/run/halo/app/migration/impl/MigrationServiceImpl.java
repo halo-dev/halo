cf7746be6db71f6dda658408409ab6aa5b5eba54
Ryan Wang <i@ryanc.cc>
2024-10-04 20:48:43 +0800

    refactor: exclude indices directory during backup (#6753)
    #### What type of PR is this?
    
    /area core
    /kind improvement
    /milestone 2.20.x
    
    #### What this PR does / why we need it:
    
    备份时排除全文索引目录 indices，防止恢复之后因为索引文件问题无法启动。
    
    #### Which issue(s) this PR fixes:
    
    Fixes https://github.com/halo-dev/halo/issues/6443
    
    #### Does this PR introduce a user-facing change?
    
    ```release-note
    备份时排除全文索引目录 indices，防止恢复之后因为索引文件问题无法启动。
    ```

--- application/src/main/java/run/halo/app/migration/impl/MigrationServiceImpl.java
+++ application/src/main/java/run/halo/app/migration/impl/MigrationServiceImpl.java
@@ -64,6 +64,7 @@ public class MigrationServiceImpl implements MigrationService, InitializingBean 
         "backups/**",
         "db/**",
         "logs/**",
+        "indices/**",
         "docker-compose.yaml",
         "docker-compose.yml",
         "mysql/**",
