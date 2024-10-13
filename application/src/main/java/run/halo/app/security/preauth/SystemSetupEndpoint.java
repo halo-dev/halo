8eb36b02fb8bdc9695ddec4af2662530224613d7
Ryan Wang <i@ryanc.cc>
2024-10-13 22:42:23 +0800

    Change status code
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- application/src/main/java/run/halo/app/security/preauth/SystemSetupEndpoint.java
+++ application/src/main/java/run/halo/app/security/preauth/SystemSetupEndpoint.java
@@ -151,7 +151,7 @@ public class SystemSetupEndpoint {
     }
 
     private static Mono<ServerResponse> redirectToConsole() {
-        return ServerResponse.seeOther(URI.create("/console")).build();
+        return ServerResponse.status(HttpStatus.FOUND).location(URI.create("/console")).build();
     }
 
     private Mono<Void> doInitialization(SetupRequest body) {
