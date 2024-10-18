22c385ac0489e9d1427b65b24962c5adc82f28ec
guqing <i@guqing.email>
2024-10-18 16:29:41 +0800

    fix: modify name column charset to make it case-sensitive for mysql

--- application/src/main/resources/schema-mysql.sql
+++ application/src/main/resources/schema-mysql.sql
@@ -1,6 +1,6 @@
 create table if not exists extensions
 (
-    name    varchar(255) not null,
+    name    varchar(255) not null COLLATE utf8mb4_bin,
     data    longblob,
     version bigint,
     primary key (name)
