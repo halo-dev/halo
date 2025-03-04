81dbc6a3716d51ced1077c8057098a55a2913e23
John Niang <johnniang@foxmail.com>
2025-03-05 00:43:20 +0800

    Reformat codestyle

--- application/src/main/java/run/halo/app/plugin/loader/HaloPluginClassLoader.java
+++ application/src/main/java/run/halo/app/plugin/loader/HaloPluginClassLoader.java
@@ -1,33 +1,34 @@
 package run.halo.app.plugin.loader;
 
-import lombok.extern.slf4j.Slf4j;
-import org.pf4j.ClassLoadingStrategy;
-import org.pf4j.PluginDescriptor;
-import org.pf4j.PluginManager;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.pf4j.ClassLoadingStrategy;
+import org.pf4j.PluginClassLoader;
+import org.pf4j.PluginDescriptor;
+import org.pf4j.PluginManager;
 
 @Slf4j
-public class HaloPluginClassLoader extends org.pf4j.PluginClassLoader {
+public class HaloPluginClassLoader extends PluginClassLoader {
+
     /**
-     * see also <a href="https://github.com/halo-dev/halo/issues/4610">gh-4610</a>
+     * see also <a href="https://github.com/halo-dev/halo/issues/4610">gh-4610</a>.
      */
     private final ClassLoadingStrategy resourceLoadingStrategy = ClassLoadingStrategy.PDA;
 
     public HaloPluginClassLoader(PluginManager pluginManager, PluginDescriptor pluginDescriptor,
-                                 ClassLoader parent) {
+        ClassLoader parent) {
         super(pluginManager, pluginDescriptor, parent, ClassLoadingStrategy.APD);
     }
 
     @Override
     public URL getResource(String name) {
         for (ClassLoadingStrategy.Source classLoadingSource :
-                resourceLoadingStrategy.getSources()) {
+            resourceLoadingStrategy.getSources()) {
             URL url = switch (classLoadingSource) {
                 case APPLICATION -> super.getResource(name);
                 case PLUGIN -> this.findResource(name);
@@ -36,12 +37,12 @@ public class HaloPluginClassLoader extends org.pf4j.PluginClassLoader {
 
             if (url != null) {
                 log.trace("Found resource '{}' in {} classpath", name,
-                        classLoadingSource);
+                    classLoadingSource);
                 return url;
             }
 
             log.trace("Couldn't find resource '{}' in {}", name,
-                    classLoadingSource);
+                classLoadingSource);
         }
 
         return null;
@@ -53,12 +54,12 @@ public class HaloPluginClassLoader extends org.pf4j.PluginClassLoader {
         log.trace("Received request to load resources '{}'", name);
 
         for (ClassLoadingStrategy.Source classLoadingSource :
-                resourceLoadingStrategy.getSources()) {
+            resourceLoadingStrategy.getSources()) {
             switch (classLoadingSource) {
                 case APPLICATION:
                     if (this.getParent() != null) {
                         resources.addAll(
-                                Collections.list(this.getParent().getResources(name)));
+                            Collections.list(this.getParent().getResources(name)));
                     }
                     break;
                 case PLUGIN:
@@ -66,6 +67,9 @@ public class HaloPluginClassLoader extends org.pf4j.PluginClassLoader {
                     break;
                 case DEPENDENCIES:
                     resources.addAll(this.findResourcesFromDependencies(name));
+                    break;
+                default:
+                    // Do nothing
             }
         }
 
