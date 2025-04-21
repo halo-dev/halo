a94b74cb38500d3e19340987bf73896c22be7429
John Niang <johnniang@foxmail.com>
2025-04-21 12:56:48 +0800

    Remove unused application startup steps buffer (#7364)
    #### What type of PR is this?
    
    /kind improvement
    /area core
    /milestone 2.20.x
    
    #### What this PR does / why we need it:
    
    This PR removes application startup steps buffer to reduce memory usage.
    
    #### Does this PR introduce a user-facing change?
    
    ```release-note
    None
    ```

--- application/src/main/java/run/halo/app/Application.java
+++ application/src/main/java/run/halo/app/Application.java
@@ -3,7 +3,6 @@ package run.halo.app;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.integration.IntegrationAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import run.halo.app.infra.properties.HaloProperties;
@@ -23,9 +22,7 @@ import run.halo.app.infra.properties.HaloProperties;
 public class Application {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(Application.class)
-            .applicationStartup(new BufferingApplicationStartup(1024))
-            .run(args);
+        new SpringApplicationBuilder(Application.class).run(args);
     }
 
 }
