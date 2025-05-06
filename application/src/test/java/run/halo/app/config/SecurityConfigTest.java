5a6f1ef6419e25780972d89aa29d14ea1a981b7c
John Niang <johnniang@foxmail.com>
2025-05-06 17:19:36 +0800

    Use Argon2 password encoder as default to remove password limit (#7407)
    #### What type of PR is this?
    
    /kind bug
    /area core
    /milestone 2.20.x
    
    #### What this PR does / why we need it:
    
    This PR makes Argon2 password encoder as default to remove password limit of 72.
    
    Please note that there is no compatibility issue for old passwords.
    
    #### Which issue(s) this PR fixes:
    
    Fixes #7405 
    
    #### Special notes for your reviewer:
    
    1. Try to login as admin
    2. Create a password having the length of 73 or more for a new user
    3. See the result
    
    #### Does this PR introduce a user-facing change?
    
    ```release-note
    修复无法设置长度超过72个字符的密码的问题
    ```

--- application/src/test/java/run/halo/app/config/SecurityConfigTest.java
+++ application/src/test/java/run/halo/app/config/SecurityConfigTest.java
@@ -1,13 +1,16 @@
 package run.halo.app.config;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.security.web.server.header.StrictTransportSecurityServerHttpHeadersWriter.STRICT_TRANSPORT_SECURITY;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest
@@ -34,6 +37,12 @@ class SecurityConfigTest {
             .expectHeader()
             .value(STRICT_TRANSPORT_SECURITY,
                 hsts -> assertFalse(hsts.contains("includeSubDomains")));
+    }
+
+    @Test
+    void shouldAllowPasswordLengthMoreThan72(@Autowired PasswordEncoder passwordEncoder) {
+        var encoded = passwordEncoder.encode(RandomStringUtils.secure().nextAlphanumeric(73));
+        assertNotNull(encoded);
     }
 
 }
