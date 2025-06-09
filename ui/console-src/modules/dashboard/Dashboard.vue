ea087e2729bfc36ff5c03449fb9371da65ae2a40
Ryan Wang <i@ryanc.cc>
2025-06-09 10:26:05 +0800

    refactor: enable dashboard widgets query cache
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- ui/console-src/modules/dashboard/Dashboard.vue
+++ ui/console-src/modules/dashboard/Dashboard.vue
@@ -6,10 +6,10 @@ import {
   VPageHeader,
 } from "@halo-dev/components";
 import type { DashboardWidgetDefinition } from "@halo-dev/console-shared";
-import { computed, type ComputedRef, provide, ref } from "vue";
+import { computed, type ComputedRef, provide, ref, toRaw } from "vue";
 import WidgetViewItem from "./components/WidgetViewItem.vue";
 import { useDashboardExtensionPoint } from "./composables/use-dashboard-extension-point";
-import { useDashboardWidgetsFetch } from "./composables/use-dashboard-widgets-fetch";
+import { useDashboardWidgetsViewFetch } from "./composables/use-dashboard-widgets-fetch";
 import "./styles/dashboard.css";
 import { internalWidgetDefinitions } from "./widgets";
 
@@ -19,7 +19,7 @@ function breakpointChangedEvent(breakpoint: string) {
   currentBreakpoint.value = breakpoint;
 }
 
-const { layouts, layout } = useDashboardWidgetsFetch(currentBreakpoint);
+const { data } = useDashboardWidgetsViewFetch(currentBreakpoint);
 
 const { widgetDefinitions } = useDashboardExtensionPoint();
 
@@ -52,8 +52,8 @@ provide<ComputedRef<DashboardWidgetDefinition[]>>(
 
   <div class="dashboard m-4">
     <grid-layout
-      v-model:layout="layout"
-      :responsive-layouts="layouts"
+      :layout="toRaw(data?.layout || [])"
+      :responsive-layouts="data?.layouts"
       :col-num="12"
       :is-draggable="false"
       :is-resizable="false"
@@ -66,7 +66,7 @@ provide<ComputedRef<DashboardWidgetDefinition[]>>(
       :cols="{ lg: 12, md: 12, sm: 6, xs: 4 }"
       @breakpoint-changed="breakpointChangedEvent"
     >
-      <WidgetViewItem v-for="item in layout" :key="item.i" :item="item" />
+      <WidgetViewItem v-for="item in data?.layout" :key="item.i" :item="item" />
     </grid-layout>
   </div>
 </template>
