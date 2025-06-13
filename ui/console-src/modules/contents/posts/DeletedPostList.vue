f59e3a290946a2ca0346d71f4ad58f87c80e5a56
Ryan Wang <i@ryanc.cc>
2025-06-13 14:50:42 +0800

    refactor: remove unused width prop for post title field (#7545)
    #### What type of PR is this?
    
    /area ui
    /kind improvement
    /milestone 2.21.x
    
    #### What this PR does / why we need it:
    
    Remove unused width prop for post title field
    
    #### Does this PR introduce a user-facing change?
    
    ```release-note
    None
    ```

--- ui/console-src/modules/contents/posts/DeletedPostList.vue
+++ ui/console-src/modules/contents/posts/DeletedPostList.vue
@@ -317,7 +317,7 @@ watch(
               />
             </template>
             <template #start>
-              <VEntityField :title="post.post.spec.title" width="27rem">
+              <VEntityField :title="post.post.spec.title">
                 <template #description>
                   <div class="flex flex-col gap-1.5">
                     <VSpace class="flex-wrap !gap-y-1">
