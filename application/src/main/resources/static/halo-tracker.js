b2c9924054e50db6e890124775005d4349916b9b
humphreyLi <3087166487@qq.com>
2025-06-21 14:09:07 +0800

    文章写完后预览也会增加文章阅读数 #1763

--- application/src/main/resources/static/halo-tracker.js
+++ application/src/main/resources/static/halo-tracker.js
@@ -1,1 +1,154 @@
-!function(){"use strict";!function(t){var e=t.screen,r=e.width,n=e.height,a=t.navigator.language,o=t.location,i=t.localStorage,c=t.document,u=t.history,l=o.hostname,s=o.pathname,p=o.search,f=c.currentScript;if(f){var h=function(t,e,r){var n=t[e];return function(){for(var e=[],a=arguments.length;a--;)e[a]=arguments[a];return r.apply(null,e),n.apply(t,e)}},d=function(){return i&&i.getItem("haloTracker.disabled")||T&&function(){var e=t.doNotTrack,r=t.navigator,n=t.external,a="msTrackingProtectionEnabled",o=e||r.doNotTrack||r.msDoNotTrack||n&&a in n&&n[a]();return"1"==o||"yes"===o}()||j&&!w.includes(l)},g="data-",v=f.getAttribute.bind(f),m=v(g+"group")||"",k=v(g+"plural"),y=v(g+"name"),S=v(g+"host-url"),b="false"!==v(g+"auto-track"),T=v(g+"do-not-track"),j=v(g+"domains")||"",w=j.split(",").map((function(t){return t.trim()})),E=(S?S.replace(/\/$/,""):f.src.split("/").slice(0,-1).join("/"))+"/apis/api.halo.run/v1alpha1/trackers/counter",N=r+"x"+n,O=""+s+p,x=c.referrer,P=function(t,e){return void 0===t&&(t=O),void 0===e&&(e=x),function(t){if(!d())return fetch(E,{method:"POST",body:JSON.stringify(Object.assign({},t)),headers:{"Content-Type":"application/json"}}).then((function(t){return t.text()})).then((function(t){console.debug("Visit count:",t)}))}((r={group:m,plural:k,name:y,hostname:l,screen:N,language:a,url:O},n={url:t,referrer:e},Object.keys(n).forEach((function(t){void 0!==n[t]&&(r[t]=n[t])})),r));var r,n},V=function(t,e,r){if(r){x=O;var n=r.toString();(O="http"===n.substring(0,4)?"/"+n.split("/").splice(3).join("/"):n)!==x&&P()}};if(!t.haloTracker){var A=function(t){return trackEvent(t)};A.trackView=P,t.haloTracker=A}if(b&&!d()){u.pushState=h(u,"pushState",V),u.replaceState=h(u,"replaceState",V);var C=function(){"complete"===c.readyState&&P()};c.addEventListener("readystatechange",C,!0),C()}}}(window)}();
+(function() {
+    'use strict';
+    
+    (function(window) {
+        var screen = window.screen;
+        var width = screen.width;
+        var height = screen.height;
+        var language = window.navigator.language;
+        var location = window.location;
+        var localStorage = window.localStorage;
+        var document = window.document;
+        var history = window.history;
+        var hostname = location.hostname;
+        var pathname = location.pathname;
+        var search = location.search;
+        var currentScript = document.currentScript;
+        
+        if (!currentScript) return;
+        
+        // 预览检测函数
+        var isPreviewMode = function() {
+            // 检测预览路径
+            if (pathname.startsWith('/preview/')) {
+                return true;
+            }
+            
+            // 检测主题预览参数
+            if (search.includes('preview-theme=')) {
+                return true;
+            }
+            
+            // 检测其他预览标识
+            if (localStorage && localStorage.getItem('halo.preview.mode') === 'true') {
+                return true;
+            }
+            
+            return false;
+        };
+        
+        // 如果是预览模式，直接返回不执行统计
+        if (isPreviewMode()) {
+            console.debug('Preview mode detected, skipping visit tracking');
+            return;
+        }
+        
+        var wrap = function(obj, method, handler) {
+            var original = obj[method];
+            return function() {
+                var args = [];
+                for (var i = arguments.length; i--;) args[i] = arguments[i];
+                handler.apply(null, args);
+                return original.apply(obj, args);
+            };
+        };
+        
+        var isDisabled = function() {
+            return (localStorage && localStorage.getItem("haloTracker.disabled")) ||
+                   (doNotTrack && (function() {
+                       var dnt = window.doNotTrack;
+                       var navigator = window.navigator;
+                       var external = window.external;
+                       var prop = "msTrackingProtectionEnabled";
+                       var value = dnt || navigator.doNotTrack || navigator.msDoNotTrack || 
+                                  (external && prop in external && external[prop]());
+                       return value == "1" || value === "yes";
+                   })()) ||
+                   (domains && !allowedDomains.includes(hostname));
+        };
+        
+        var prefix = "data-";
+        var getAttribute = currentScript.getAttribute.bind(currentScript);
+        var group = getAttribute(prefix + "group") || "";
+        var plural = getAttribute(prefix + "plural");
+        var name = getAttribute(prefix + "name");
+        var hostUrl = getAttribute(prefix + "host-url");
+        var autoTrack = getAttribute(prefix + "auto-track") !== "false";
+        var doNotTrack = getAttribute(prefix + "do-not-track");
+        var domains = getAttribute(prefix + "domains") || "";
+        var allowedDomains = domains.split(",").map(function(domain) {
+            return domain.trim();
+        });
+        
+        var endpoint = (hostUrl ? hostUrl.replace(/\/$/, "") : currentScript.src.split("/").slice(0, -1).join("/")) + 
+                       "/apis/api.halo.run/v1alpha1/trackers/counter";
+        var screenSize = width + "x" + height;
+        var url = pathname + search;
+        var referrer = document.referrer;
+        
+        var trackView = function(customUrl, customReferrer) {
+            if (customUrl === undefined) customUrl = url;
+            if (customReferrer === undefined) customReferrer = referrer;
+            
+            return function(data) {
+                if (!isDisabled()) {
+                    return fetch(endpoint, {
+                        method: "POST",
+                        body: JSON.stringify(Object.assign({}, data)),
+                        headers: {
+                            "Content-Type": "application/json"
+                        }
+                    }).then(function(response) {
+                        return response.text();
+                    }).then(function(result) {
+                        console.debug("Visit count:", result);
+                    });
+                }
+            }({
+                group: group,
+                plural: plural,
+                name: name,
+                hostname: hostname,
+                screen: screenSize,
+                language: language,
+                url: customUrl,
+                referrer: customReferrer
+            });
+        };
+        
+        var handlePushState = function(originalMethod, state, title, url) {
+            if (url) {
+                referrer = location.pathname + location.search;
+                var newUrl = url.toString();
+                var newPath = newUrl.substring(0, 4) === "http" ? 
+                             "/" + newUrl.split("/").splice(3).join("/") : newUrl;
+                if (newPath !== referrer) {
+                    trackView();
+                }
+            }
+        };
+        
+        if (!window.haloTracker) {
+            var trackEvent = function(event) {
+                return trackEvent(event);
+            };
+            trackEvent.trackView = trackView;
+            window.haloTracker = trackEvent;
+        }
+        
+        if (autoTrack && !isDisabled()) {
+            history.pushState = wrap(history, "pushState", handlePushState);
+            history.replaceState = wrap(history, "replaceState", handlePushState);
+            
+            var track = function() {
+                if (document.readyState === "complete") {
+                    trackView();
+                }
+            };
+            
+            document.addEventListener("readystatechange", track, true);
+            track();
+        }
+        
+    })(window);
+})();
