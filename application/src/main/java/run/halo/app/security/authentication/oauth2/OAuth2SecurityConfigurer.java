576dda9d74233ca2e2cbee5ee326bc4595fb7ab1
John Niang <johnniang@foxmail.com>
2025-08-08 19:06:39 +0800

    Add social login endpoint for remember-me support (#7670)
    #### What type of PR is this?
    
    /kind feature
    /kind api-change
    /area core
    /milestone 2.21.x
    
    #### What this PR does / why we need it:
    
    This PR adds a new endpoint `POST /login/social/{auth_provider_name}?remember-me=true` to make the social login support remember-me mechanism. 
    
    #### Does this PR introduce a user-facing change?
    
    ```release-note
    支持社交登录时选择是否保持登录
    ```

--- application/src/main/java/run/halo/app/security/authentication/oauth2/OAuth2SecurityConfigurer.java
+++ application/src/main/java/run/halo/app/security/authentication/oauth2/OAuth2SecurityConfigurer.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.web.server.context.ServerSecurityContextRepository;
 import org.springframework.stereotype.Component;
 import run.halo.app.core.user.service.UserConnectionService;
+import run.halo.app.security.LoginHandlerEnhancer;
 import run.halo.app.security.authentication.SecurityConfigurer;
 
 /**
@@ -25,17 +26,21 @@ class OAuth2SecurityConfigurer implements SecurityConfigurer {
 
     private final ReactiveUserDetailsService userDetailsService;
 
+    private final LoginHandlerEnhancer loginHandlerEnhancer;
+
     public OAuth2SecurityConfigurer(ServerSecurityContextRepository securityContextRepository,
-        UserConnectionService connectionService, ReactiveUserDetailsService userDetailsService) {
+        UserConnectionService connectionService, ReactiveUserDetailsService userDetailsService,
+        LoginHandlerEnhancer loginHandlerEnhancer) {
         this.securityContextRepository = securityContextRepository;
         this.connectionService = connectionService;
         this.userDetailsService = userDetailsService;
+        this.loginHandlerEnhancer = loginHandlerEnhancer;
     }
 
     @Override
     public void configure(ServerHttpSecurity http) {
         var mapOAuth2Filter = new MapOAuth2AuthenticationFilter(
-            securityContextRepository, connectionService, userDetailsService
+            securityContextRepository, connectionService, userDetailsService, loginHandlerEnhancer
         );
         http.addFilterBefore(mapOAuth2Filter, SecurityWebFiltersOrder.AUTHENTICATION);
     }
