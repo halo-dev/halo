f5af5a1550ebddf2bc4896dc1c17c3290dc98253
Ryan Wang <i@ryanc.cc>
2025-08-13 15:43:50 +0800

    Add subject-based comment list modal (#7681)

---
+++ ui/console-src/modules/contents/comments/components/SubjectQueryCommentListModal.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import { VButton, VModal } from "@halo-dev/components";
+import { useTemplateRef } from "vue";
+import SubjectQueryCommentList from "./SubjectQueryCommentList.vue";
+
+const props = defineProps<{
+  subjectRefKey: string;
+}>();
+
+const emit = defineEmits<{
+  (event: "close"): void;
+}>();
+
+const modal = useTemplateRef<InstanceType<typeof VModal> | null>("modal");
+</script>
+<template>
+  <VModal
+    ref="modal"
+    :centered="false"
+    :width="1400"
+    :title="$t('core.comment.title')"
+    :layer-closable="true"
+    mount-to-body
+    @close="emit('close')"
+  >
+    <SubjectQueryCommentList :subject-ref-key="props.subjectRefKey" />
+    <template #footer>
+      <VButton @click="modal?.close()">
+        {{ $t("core.common.buttons.close") }}
+      </VButton>
+    </template>
+  </VModal>
+</template>
