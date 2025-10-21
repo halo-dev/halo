7fd3cce29a430e66db201fe031fc7ee7368b9932
Takagi <1103069291@qq.com>
2025-10-21 17:48:14 +0800

    feat: support using Chinese symbols to invoke the suggestion menu (#7840)
    #### What type of PR is this?
    
    /kind feature
    /area editor
    
    #### What this PR does / why we need it:
    
    支持使用中文符号`、`唤起建议菜单
    
    #### Which issue(s) this PR fixes:
    
    Fixes #7770
    
    #### Does this PR introduce a user-facing change?
    ```release-note
    支持使用中文符号、唤起建议菜单
    ```

--- ui/packages/editor/src/extensions/commands-menu/commands.ts
+++ ui/packages/editor/src/extensions/commands-menu/commands.ts
@@ -1,3 +1,4 @@
+import { PluginKey } from "@/tiptap";
 import {
   Extension,
   VueRenderer,
@@ -6,7 +7,7 @@ import {
   type Range,
 } from "@/tiptap/vue-3";
 import type { CommandMenuItemType } from "@/types";
-import Suggestion from "@tiptap/suggestion";
+import Suggestion, { type SuggestionOptions } from "@tiptap/suggestion";
 import type { Instance } from "tippy.js";
 import tippy from "tippy.js";
 import CommandsView from "./CommandsView.vue";
@@ -19,86 +20,96 @@ export default Extension.create({
       this.editor as Editor
     );
 
+    const suggestionPlugin: SuggestionOptions = {
+      editor: this.editor,
+      // @ts-ignore
+      command: ({
+        editor,
+        range,
+        props,
+      }: {
+        editor: Editor;
+        range: Range;
+        props: CommandMenuItemType;
+      }) => {
+        props.command({ editor, range });
+      },
+      items: ({ query }: { query: string }) => {
+        return commandMenuItems.filter((item) =>
+          [...item.keywords, item.title].some((keyword) =>
+            keyword.includes(query)
+          )
+        );
+      },
+      render: () => {
+        let component: VueRenderer;
+        let popup: Instance[];
+
+        return {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          onStart: (props: Record<string, any>) => {
+            component = new VueRenderer(CommandsView, {
+              props,
+              editor: props.editor,
+            });
+
+            if (!props.clientRect) {
+              return;
+            }
+
+            popup = tippy("body", {
+              getReferenceClientRect: props.clientRect,
+              appendTo: () => document.body,
+              content: component.element as Element,
+              showOnCreate: true,
+              interactive: true,
+              trigger: "manual",
+              placement: "bottom-start",
+            });
+          },
+
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          onUpdate(props: Record<string, any>) {
+            component.updateProps(props);
+
+            if (!props.clientRect) {
+              return;
+            }
+
+            popup[0].setProps({
+              getReferenceClientRect: props.clientRect,
+            });
+          },
+
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          onKeyDown(props: Record<string, any>) {
+            if (props.event.key === "Escape") {
+              popup[0].hide();
+
+              return true;
+            }
+
+            return component.ref?.onKeyDown(props);
+          },
+
+          onExit() {
+            popup[0].destroy();
+            component.destroy();
+          },
+        };
+      },
+    };
+
     return [
       Suggestion({
-        editor: this.editor,
+        pluginKey: new PluginKey("commands-menu-english"),
         char: "/",
-        // @ts-ignore
-        command: ({
-          editor,
-          range,
-          props,
-        }: {
-          editor: Editor;
-          range: Range;
-          props: CommandMenuItemType;
-        }) => {
-          props.command({ editor, range });
-        },
-        items: ({ query }: { query: string }) => {
-          return commandMenuItems.filter((item) =>
-            [...item.keywords, item.title].some((keyword) =>
-              keyword.includes(query)
-            )
-          );
-        },
-        render: () => {
-          let component: VueRenderer;
-          let popup: Instance[];
-
-          return {
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            onStart: (props: Record<string, any>) => {
-              component = new VueRenderer(CommandsView, {
-                props,
-                editor: props.editor,
-              });
-
-              if (!props.clientRect) {
-                return;
-              }
-
-              popup = tippy("body", {
-                getReferenceClientRect: props.clientRect,
-                appendTo: () => document.body,
-                content: component.element as Element,
-                showOnCreate: true,
-                interactive: true,
-                trigger: "manual",
-                placement: "bottom-start",
-              });
-            },
-
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            onUpdate(props: Record<string, any>) {
-              component.updateProps(props);
-
-              if (!props.clientRect) {
-                return;
-              }
-
-              popup[0].setProps({
-                getReferenceClientRect: props.clientRect,
-              });
-            },
-
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            onKeyDown(props: Record<string, any>) {
-              if (props.event.key === "Escape") {
-                popup[0].hide();
-
-                return true;
-              }
-
-              return component.ref?.onKeyDown(props);
-            },
-
-            onExit() {
-              popup[0].destroy();
-              component.destroy();
-            },
-          };
-        },
+        ...suggestionPlugin,
+      }),
+      Suggestion({
+        pluginKey: new PluginKey("commands-menu-chinese"),
+        char: "、",
+        ...suggestionPlugin,
       }),
     ];
   },
