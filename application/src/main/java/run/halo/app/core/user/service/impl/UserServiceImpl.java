547dc5a68ce92e841d97c70b72be61e91577fbe3
liuyiwuqing <1520431201@qq.com>
2025-10-31 11:33:49 +0800

    refactor(user): 移除未使用的List导入

--- application/src/main/java/run/halo/app/core/user/service/impl/UserServiceImpl.java
+++ application/src/main/java/run/halo/app/core/user/service/impl/UserServiceImpl.java
@@ -9,7 +9,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
