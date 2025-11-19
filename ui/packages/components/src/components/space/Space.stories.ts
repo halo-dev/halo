6e6ee580ec6a05a79c9a8e6ae285794776502f84
Ryan Wang <i@ryanc.cc>
2025-11-19 15:55:47 +0800

    Bump storybook to 10 (#7946)
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- ui/packages/components/src/components/space/Space.stories.ts
+++ ui/packages/components/src/components/space/Space.stories.ts
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/vue3";
+import type { Meta, StoryObj } from "@storybook/vue3-vite";
 
 import { VSpace } from ".";
 import { VButton } from "../button";
