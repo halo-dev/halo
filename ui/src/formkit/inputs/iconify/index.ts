3d8569dda0ff0266274d202eabc93c863455e0ce
Ryan Wang <i@ryanc.cc>
2025-12-09 23:44:20 +0800

    Load IconifyInput component asynchronously

--- ui/src/formkit/inputs/iconify/index.ts
+++ ui/src/formkit/inputs/iconify/index.ts
@@ -1,12 +1,15 @@
 import { initialValue, type FormKitInputs } from "@formkit/inputs";
 import { createInput } from "@formkit/vue";
-import IconifyInput from "./IconifyInput.vue";
+import { defineAsyncComponent } from "vue";
 
-export const iconify = createInput(IconifyInput, {
-  type: "input",
-  props: ["format"],
-  features: [initialValue],
-});
+export const iconify = createInput(
+  defineAsyncComponent(() => import("./IconifyInput.vue")),
+  {
+    type: "input",
+    props: ["format"],
+    features: [initialValue],
+  }
+);
 
 declare module "@formkit/inputs" {
   export interface FormKitInputProps<Props extends FormKitInputs<Props>> {
