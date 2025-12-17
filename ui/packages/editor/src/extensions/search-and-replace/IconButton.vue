c2a240b61a702d95fb07726610f8e5515a3a518a
Ryan Wang <i@ryanc.cc>
2025-12-17 17:47:55 +0800

    Refactor search UI with reusable button components

---
+++ ui/packages/editor/src/extensions/search-and-replace/IconButton.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+defineProps<{
+  disabled?: boolean;
+}>();
+</script>
+<template>
+  <button
+    type="button"
+    class="rounded p-1"
+    :class="{
+      'text-gray-600 opacity-100 hover:bg-gray-100 hover:text-gray-900 active:bg-gray-200':
+        !disabled,
+      'opacity-50': disabled,
+    }"
+    :disabled="disabled"
+  >
+    <slot />
+  </button>
+</template>
