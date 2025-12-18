2bb4d0548b2eea0938f3336cf28f533a27e5e4d7
Ryan Wang <i@ryanc.cc>
2025-12-18 22:24:39 +0800

    Add valueOnly prop to Iconify input for string values (#8055)

--- ui/src/formkit/inputs/iconify/feature.ts
+++ ui/src/formkit/inputs/iconify/feature.ts
@@ -1,4 +1,5 @@
 import type { FormKitNode } from "@formkit/core";
+import { undefine } from "@formkit/utils";
 
 /**
  * Ensure that the content entered for iconify is correctly set, and use the default value when it is not set.
@@ -8,5 +9,6 @@ export default function iconifyFeature(node: FormKitNode): void {
   node.on("created", () => {
     node.props.format = node.props.format ?? "svg";
     node.props.popperPlacement = node.props.popperPlacement ?? "auto";
+    node.props.valueOnly = undefine(node.props.valueOnly);
   });
 }
