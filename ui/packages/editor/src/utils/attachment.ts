26e9a427d2ee7a02e2e7f737d2c75c4a257ea8f5
Ryan Wang <i@ryanc.cc>
2025-12-19 16:14:49 +0800

    Support captions for media attachments in editor

--- ui/packages/editor/src/utils/attachment.ts
+++ ui/packages/editor/src/utils/attachment.ts
@@ -1,7 +1,8 @@
-import type { Content } from "@/tiptap";
+import { DOMParser, Editor, elementFromString, type Content } from "@/tiptap";
 import { utils, type AttachmentLike } from "@halo-dev/ui-shared";
 
 export function convertToMediaContents(
+  editor: Editor,
   attachments: AttachmentLike[]
 ): Content[] {
   return attachments
@@ -21,34 +22,31 @@ export function convertToMediaContents(
         return;
       }
 
-      const { mediaType, alt, url } = attachmentSimple;
+      const { mediaType, alt, url, caption } = attachmentSimple;
 
       if (mediaType?.startsWith("image/")) {
-        return {
-          type: "image",
-          attrs: {
-            src: url,
-            alt,
-          },
-        };
+        return createFigureContent(editor, {
+          contentType: "image",
+          url,
+          alt,
+          caption,
+        });
       }
 
       if (mediaType?.startsWith("video/")) {
-        return {
-          type: "video",
-          attrs: {
-            src: url,
-          },
-        };
+        return createFigureContent(editor, {
+          contentType: "video",
+          url,
+          caption,
+        });
       }
 
       if (mediaType?.startsWith("audio/")) {
-        return {
-          type: "audio",
-          attrs: {
-            src: url,
-          },
-        };
+        return createFigureContent(editor, {
+          contentType: "audio",
+          url,
+          caption,
+        });
       }
 
       return {
@@ -65,4 +63,47 @@ export function convertToMediaContents(
       };
     })
     .filter(Boolean) as Content[];
+}
+
+function createFigureContent(
+  editor: Editor,
+  {
+    contentType,
+    url,
+    alt,
+    caption,
+  }: {
+    contentType: "image" | "video" | "audio";
+    url: string;
+    alt?: string;
+    caption?: string;
+  }
+) {
+  const baseContent: Content = {
+    type: "figure",
+    attrs: {
+      contentType,
+    },
+    content: [
+      {
+        type: contentType,
+        attrs: {
+          src: url,
+          alt,
+        },
+      },
+    ],
+  };
+
+  if (caption) {
+    const dom = elementFromString(caption);
+    const content = DOMParser.fromSchema(editor.schema).parse(dom).toJSON();
+    baseContent.content?.push({
+      type: "figureCaption",
+      // TODO: Find a better way to handle the caption content
+      content: content.content[0].content,
+    });
+  }
+
+  return baseContent;
 }
