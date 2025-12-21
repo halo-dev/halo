90541601c5d46a24e80810e47f20d7718d994d43
Ryan Wang <i@ryanc.cc>
2025-12-21 21:09:12 +0800

    Refactor iconify label rendering into IconifyLabel component (#8065)

---
+++ ui/src/formkit/inputs/array/labels/IconifyLabel.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import { computed } from "vue";
+import type { ArrayItemLabelType } from "..";
+import type { IconifyFormat, IconifyValue } from "../../iconify/types";
+
+const props = defineProps<{
+  itemLabel: {
+    type: ArrayItemLabelType;
+    value: string | IconifyValue;
+    format: IconifyFormat;
+    valueOnly?: boolean;
+  };
+}>();
+
+const value = computed(() => {
+  if (props.itemLabel.valueOnly) {
+    return props.itemLabel.value as string;
+  }
+  return (props.itemLabel.value as IconifyValue)?.value;
+});
+</script>
+<template>
+  <div class="inline-flex items-center [&>*]:size-4">
+    <img
+      v-if="['url', 'dataurl'].includes(itemLabel.format)"
+      :src="value"
+      class="max-w-none"
+    />
+    <Icon v-else-if="itemLabel.format === 'name'" :icon="value" />
+    <div
+      v-else
+      class="inline-flex items-center justify-center"
+      v-html="value"
+    ></div>
+  </div>
+</template>
