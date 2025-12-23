567f7e6538b8c8b61fc021651c07a68f9c389b69
Ryan Wang <i@ryanc.cc>
2025-12-23 21:30:59 +0800

    Emit uploaded event after URL attachment upload (#8089)

--- ui/console-src/modules/contents/attachments/components/UploadFromUrl.vue
+++ ui/console-src/modules/contents/attachments/components/UploadFromUrl.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import { setFocus } from "@/formkit/utils/focus";
 import { reset } from "@formkit/core";
-import { consoleApiClient } from "@halo-dev/api-client";
+import { consoleApiClient, type Attachment } from "@halo-dev/api-client";
 import { Toast, VButton } from "@halo-dev/components";
 import { onMounted, ref } from "vue";
 import { useI18n } from "vue-i18n";
@@ -19,13 +19,17 @@ const props = withDefaults(
   }
 );
 
+const emit = defineEmits<{
+  (event: "uploaded", attachment: Attachment): void;
+}>();
+
 onMounted(() => {
   setFocus("url");
 });
 
 const downloading = ref(false);
 
-async function onSubmit(data: { url: string }) {
+async function onSubmit({ url }: { url: string }) {
   try {
     downloading.value = true;
 
@@ -33,19 +37,21 @@ async function onSubmit(data: { url: string }) {
       throw new Error("Policy name is required");
     }
 
-    await consoleApiClient.storage.attachment.externalTransferAttachment({
-      uploadFromUrlRequest: {
-        url: data.url,
-        policyName: props.policyName,
-        groupName: props.groupName,
-      },
-    });
+    const { data } =
+      await consoleApiClient.storage.attachment.externalTransferAttachment({
+        uploadFromUrlRequest: {
+          url: url,
+          policyName: props.policyName,
+          groupName: props.groupName,
+        },
+      });
 
     Toast.success(
       t("core.attachment.upload_modal.download_form.toast.success")
     );
 
     reset("url");
+    emit("uploaded", data);
   } catch (error) {
     return error;
   } finally {
