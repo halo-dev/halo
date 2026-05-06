44bc423047a37b31090ea30246e25c7d686a82fb
John Niang <johnniang@foxmail.com>
2026-05-06 11:14:00 +0800

    refactor: remove unused email parameter from sendEmailVerificationCodeRateLimiter

--- application/src/main/java/run/halo/app/core/endpoint/console/UserEndpoint.java
+++ application/src/main/java/run/halo/app/core/endpoint/console/UserEndpoint.java
@@ -319,7 +319,7 @@ public class UserEndpoint implements CustomEndpoint {
                 var email = tuple.getT1();
                 var username = tuple.getT2();
                 return Mono.just(username)
-                    .transformDeferred(sendEmailVerificationCodeRateLimiter(username, email))
+                    .transformDeferred(sendEmailVerificationCodeRateLimiter(username))
                     .flatMap(u -> emailVerificationService.sendVerificationCode(username, email))
                     .onErrorMap(RequestNotPermitted.class, RateLimitExceededException::new);
             })
@@ -333,7 +333,7 @@ public class UserEndpoint implements CustomEndpoint {
         return RateLimiterOperator.of(rateLimiter);
     }
 
-    <T> RateLimiterOperator<T> sendEmailVerificationCodeRateLimiter(String username, String email) {
+    <T> RateLimiterOperator<T> sendEmailVerificationCodeRateLimiter(String username) {
         String rateLimiterKey = "send-email-verification-code-" + username;
         var rateLimiter =
             rateLimiterRegistry.rateLimiter(rateLimiterKey, "send-email-verification-code");
