0d5fb4f8c6b75c9cd545b09ebc5a55359942550d
Ryan Wang <i@ryanc.cc>
2026-05-20 00:17:59 +0800

    Remove deprecated ThumbnailSpec interface
    Delete the legacy ThumbnailSpec interface export from thumbnail.ts. This interface was marked deprecated and retained only for backward compatibility (see halo-dev/halo#10012); removing it cleans up the API client entry and avoids exporting an obsolete type while keeping the deprecation notice in the file.

--- ui/packages/api-client/entry/patch/thumbnail.ts
+++ ui/packages/api-client/entry/patch/thumbnail.ts
@@ -1,18 +1,6 @@
 /**
  * Note: This export is for compatibility with older versions, it will be removed in the future, please do not use it.
  * See：https://github.com/halo-dev/halo/issues/10012
- * @deprecated
- */
-export interface ThumbnailSpec {
-  imageSignature: string;
-  imageUri: string;
-  size: ThumbnailSpecSizeEnum;
-  thumbnailUri: string;
-}
-
-/**
- * Note: This export is for compatibility with older versions, it will be removed in the future, please do not use it.
- * See：https://github.com/halo-dev/halo/issues/10012
  * @deprecated use GetThumbnailByUriSizeEnum instead
  */
 export const ThumbnailSpecSizeEnum = {
