e2145885f215bd7620a0c6d2cc90a66bc406f197
Ryan Wang <i@ryanc.cc>
2026-08-01 19:49:09 +0800

    Fix static copy target typing

--- ui/src/vite/library-external.ts
+++ ui/src/vite/library-external.ts
@@ -8,7 +8,8 @@ import {
 } from "vite-plugin-static-copy";
 import type { HtmlTagDescriptor, Plugin } from "vite-plus";
 
-type LibraryTarget = Omit<Target, "rename"> & {
+type LibraryTarget = Omit<Target, "rename" | "src"> & {
+  src: string;
   rename: string;
 };
 
